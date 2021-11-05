964a71e79ee834dce7e96eee99d8acac3a0adfea
sergey-shandar <sergey-shandar@users.noreply.github.com>
2021-11-05 13:35:02 -0700

    panic_if

--- test.js
+++ test.js
@@ -1,63 +1,57 @@
-const i = require('./index')
+const lib = require('./lib')
+const i = require('./')
 
 require('./lib/test')
 
-/** @type {() => never } */
-const assert = () => { throw 'assert' }
+/** @type {() => never} */
+const assert = () => lib.panic('assert')
+
+/** @type {(_: boolean) => void} */
+const assert_if = c => lib.panic_if('assert_if')(c)
 
 {
     i.parseModuleUrl('')(
         assert,
-        e => {
-            if (e.id !== 'expected') assert()
-            if (e.params[0] !== ':') assert()
-        })
+        e => assert_if(e.id !== 'expected' || e.params[0] !== ':'))
 }
 
 {
     i.parseModuleUrl(':')(
         assert,
-        e => {
-            if (e.id !== 'unknownProtocol') assert()
-        })
+        e => assert_if(e.id !== 'unknownProtocol'))
 }
 
 {
     i.parseModuleUrl('github:r')(
         assert,
-        e => {
-            if (e.id !== 'expected') assert()
-        })
+        e => assert_if(e.id !== 'expected'))
 }
 
 {
     i.parseModuleUrl('github:functionalscript/node-example')(
-        v => {
-            if (v.repo.org !== 'functionalscript') assert()
-            if (v.repo.name !== 'node-example') assert()
-            if ('branch' in v) assert()
-        },
+        v => assert_if(
+            v.repo.org !== 'functionalscript' ||
+            v.repo.name !== 'node-example' ||
+            'branch' in v),
         assert)
 }
 
 {
     i.parseModuleUrl('github:functionalscript/node-example#main')(
-        v => {
-            if (v.repo.org !== 'functionalscript') assert()
-            if (v.repo.name !== 'node-example') assert()
-            if (!('branch' in v)) assert()
-            if (v.branch !== 'main') assert()
-        },
+        v => assert_if(
+            v.repo.org !== 'functionalscript' ||
+            v.repo.name !== 'node-example' ||
+            !('branch' in v) ||
+            v.branch !== 'main'),
         assert)
 }
 
 {
     i.parseModuleUrl('github:functionalscript/node-example#4b14a7a2b11cf53f037931eb7bef240f96dcea64')(
-        v => {
-            if (v.repo.org !== 'functionalscript') assert()
-            if (v.repo.name !== 'node-example') assert()
-            if (!('commit' in v)) assert()
-            if (v.commit !== '4b14a7a2b11cf53f037931eb7bef240f96dcea64') assert()
-        },
+        v => assert_if(
+            v.repo.org !== 'functionalscript' ||
+            v.repo.name !== 'node-example' ||
+            !('commit' in v) ||
+            v.commit !== '4b14a7a2b11cf53f037931eb7bef240f96dcea64'),
         assert)
 }
