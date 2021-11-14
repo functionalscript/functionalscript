fa1234bea3e0020009b638165ae1aecf319d57ab
sergey-shandar <sergey-shandar@users.noreply.github.com>
2021-11-13 16:38:09 -0800

    print version

--- version.js
+++ version.js
@@ -5,8 +5,12 @@ const b =cp.execSync('git log --oneline')
 
 const r = b.toString().split('\n').length
 
+const v = `0.0.${r}`
+
+console.log(`version: ${v}`)
+
 const package_json = JSON.parse(fs.readFileSync('package.json').toString())
 
-package_json.version = `0.0.${r}`
+package_json.version = v
 
 fs.writeFileSync('package.json', JSON.stringify(package_json, null, 2))
