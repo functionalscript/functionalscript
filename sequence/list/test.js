f7f5ba0be1c8bd823849faea6f01e2fd63fa7a14
sergey-shandar <sergey-shandar@users.noreply.github.com>
2021-11-22 20:39:08 -0800

    minor

--- sequence/list/test.js
+++ sequence/list/test.js
@@ -12,13 +12,14 @@ const print = a => {
 
 {
     const big = list.fromArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 42, 60])
-    print(big)
+    // print(big)
     /*
     const list0 = list.fromArray([0, 1, 2, 3])
     const list1 = list.flatMap(x => list.fromArray([x, x * 2, x * 3]))(list0)
     const list2 = list.concat(list0)(list0)
     const list3 = list.inclusiveScan(sum)(list0)
     print(list3)
-    const r = list.find(x => x === 42)(list.fromArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 42, 60]))
     */
+    const r = list.find(x => x === 42)(big)
+    
 }
