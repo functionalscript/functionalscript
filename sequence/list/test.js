2b1c98bc16fc72be0def9c15bdf26860891c54dd
sergey-shandar <sergey-shandar@users.noreply.github.com>
2021-11-22 21:10:42 -0800

    concat bug

--- sequence/list/test.js
+++ sequence/list/test.js
@@ -12,14 +12,16 @@ const print = a => {
 
 {
     const big = list.fromArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 42, 60])
-    // print(big)
-    /*
     const list0 = list.fromArray([0, 1, 2, 3])
     const list1 = list.flatMap(x => list.fromArray([x, x * 2, x * 3]))(list0)
     const list2 = list.concat(list0)(list0)
     const list3 = list.inclusiveScan(sum)(list0)
-    print(list3)
-    */
     const r = list.find(x => x === 42)(big)
-    console.log(r)
+    {
+        let x = list.concat(list.empty)(big)
+        for (let i = 0; i < 10000; ++i) {
+            x = list.concat(list.empty)(x)
+        }
+        const r = x()
+    } 
 }
