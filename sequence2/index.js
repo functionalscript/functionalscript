35e3c07ba4c90b48d6398921ae6ac81d56632622
sergey-shandar <sergey-shandar@users.noreply.github.com>
2021-11-27 12:17:24 -0800

    Sequence2

--- sequence2/index.js
+++ sequence2/index.js
@@ -32,14 +32,10 @@
 /** @type {OperatorEmpty} */
 const operatorEmpty = () => () => [operatorEmpty]
 
-/** @type {<T, C>(sequence: SubSequence<T, C>) => SubSequenceResult<T, C>}*/
-const next = sequence => {
-    let i = sequence()
-    while (true) {
-        if (typeof i !== 'function') { return i }
-        i = i()
-    }
-}
+/** 
+ * @template T
+ * @typedef {readonly[T, Sequence<T>] | undefined} Result<T> 
+ */
 
 /** @type {<T, A>(a: OperatorSequence<T, A>) => (b: Sequence<T>) => Sequence<A>} */
 const operatorConcat = a => b => () => {
@@ -53,13 +49,75 @@ const operatorConcat = a => b => () => {
 /** @type {<T, A>(operator: Operator<T, A>) => (sequence: Sequence<T>) => Sequence<A>} */
 const apply = operator => sequence => () => {
     if (operator === operatorEmpty) { return [undefined] }
-    const result = next(sequence)
+    const result = sequence()
+    if (typeof result === 'function') { return apply(operator)(sequence) }
     if (result.length === 1) { return [undefined] }
     const [first, tail] = result
     return operatorConcat(operator(first))(tail)
 }
 
+/** @type {() => readonly[undefined]} */
+const empty = () => [undefined]
+
+/** @type {<T>(sequence: Sequence<T>) => Result<T>}*/
+const next = sequence => {
+    let i = sequence()
+    while (true) {
+
+        if (typeof i !== 'function') {
+            if (i.length === 1) { return undefined }
+            return i
+        }
+        i = i()
+    }
+}
+
+/** @type {<T>(a: readonly T[]) => (index: number) => Sequence<T>} */
+const fromArrayAt = array => index => {
+    if (array.length <= index) { return empty }
+    return () => [array[index], fromArrayAt(array)(index + 1)]
+}
+
+/** @type {<T>(a: readonly T[]) => Sequence<T>} */
+const fromArray = array => fromArrayAt(array)(0)
+
+/** @type {<T>(sequence: Sequence<T>) => Iterable<T>} */
+const iterable = sequence => ({
+    *[Symbol.iterator]() {
+        let i = sequence
+        while (true) {
+            const result = next(i)
+            if (result === undefined) { return }
+            const [first, tail] = result
+            yield first
+            i = tail
+        }
+    }
+})
+
+/** @type {<T>(sequence: Sequence<T>) => readonly T[]} */
+const array = sequence => Array.from(iterable(sequence))
+
+/** @type {(count: number) => Sequence<number>} */
+const countdown = count => {
+    if (count <= 0) { return empty }    
+    const result = count - 1
+    return () => [result, countdown(result)]
+}
+
 module.exports = {
     /** @readonly */
+    next,
+    /** @readonly */
+    empty,
+    /** @readonly */
     apply,
+    /** @readonly */
+    fromArray,
+    /** @readonly */
+    iterable,
+    /** @readonly */
+    array,
+    /** @readonly */
+    countdown,
 }
