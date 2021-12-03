09685bdd39ecf12b67afc6e16815d6c9fde23ed8
sergey-shandar <sergey-shandar@users.noreply.github.com>
2021-12-02 18:36:41 -0800

    simplify commonjs

---
+++ io/index.js
@@ -0,0 +1,15 @@
+const result = require('../types/result')
+
+/** @type {<T>(f: () => T) => result.Result<T, unknown>} */
+const tryCatch = f => {
+    try {
+        return result.ok(f())
+    } catch (e) {
+        return result.error(e)
+    }
+}
+
+module.exports = {
+    /** @readonly */
+    tryCatch,
+}
