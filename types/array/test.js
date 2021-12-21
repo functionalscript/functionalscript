c683b0ee500996337d2d2779089079e1ad38772e
Trinidadec <msden.ua@gmail.com>
2021-12-21 23:57:22 +0200

    Denis/tests (#21)
    * 'tests'
    
    * add array tests

---
+++ types/array/test.js
@@ -0,0 +1,94 @@
+const _ = require('.')
+const json = require('../../json')
+const { sort } = require('../object')
+
+/** @type {(a: _.Array<json.Unknown>) => string} */
+const stringify = a => json.stringify(sort)(a)
+
+{
+    const result = stringify([1, 20, 300])
+    if (result !== '[1,20,300]') { throw result }
+}
+
+{
+    const result = _.at(2)([1, 20, 300])
+    if (result === undefined) {throw result}
+    if (result[0] !== 300) { throw result }
+}
+
+{
+    const result = _.at(3)([1, 20, 300])
+    if (result !== undefined) {throw result}
+}
+
+{
+    const result = _.first([1, 20, 300])
+    if (result !== 1) { throw result }
+}
+
+{
+    const result = _.first([])
+    if (result !== undefined) {throw result}
+}
+
+{
+    const result = _.last([1, 20, 300])
+    if (result !== 300) { throw result }
+}
+
+{
+    const result = _.last([])
+    if (result !== undefined) {throw result}
+}
+
+{
+    const result = _.head([1, 20, 300])
+    if (result === undefined) {throw result}
+    const str = stringify(result)
+    if (str !== '[1,20]') { throw str }
+}
+
+{
+    const result = _.head([])
+    if (result !== undefined) {throw result}
+}
+
+{
+    const result = _.tail([1, 20, 300])
+    if (result === undefined) {throw result}
+    const str = stringify(result)
+    if (str !== '[20,300]') { throw str }
+}
+
+{
+    const result = _.tail([])
+    if (result !== undefined) {throw result}
+}
+
+{
+    const result = _.splitFirst([1, 20, 300])
+    if (result === undefined) {throw result}
+    const str = stringify(result)
+    if (str !== '[1,[20,300]]') { throw str }
+}
+
+{
+    const result = _.splitFirst([])
+    if (result !== undefined) {throw result}
+}
+
+{
+    const result = _.splitLast([1, 20, 300])
+    if (result === undefined) {throw result}
+    const str = stringify(result)
+    if (str !== '[[1,20],300]') { throw str }
+}
+
+{
+    const result = _.splitLast([])
+    if (result !== undefined) {throw result}
+}
+
+module.exports = {
+
+}
