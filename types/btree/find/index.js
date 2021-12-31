9ad5af04ed8f7d58c71c621a46807eb5cdc5f951
sergey-shandar <sergey-shandar@users.noreply.github.com>
2021-12-31 11:42:29 -0800

    btree

--- types/btree/find/index.js
+++ types/btree/find/index.js
@@ -5,37 +5,27 @@ const cmp = require('../../function/compare')
 
 /**
  * @template T
- * @typedef {readonly[1, _.Leaf1<T>|_.Branch3<T>]} Found1
+ * @typedef {readonly[cmp.Index3, _.Leaf1<T>|_.Branch3<T>]} FirstLeaf1
  */
 
 /**
  * @template T
- * @typedef {readonly[1|3, _.Leaf2<T> | _.Branch5<T>]} Found2
+ * @typedef {readonly[1, _.Branch3<T>]} FirstBranch3
  */
 
 /**
  * @template T
- * @typedef {readonly[0|2, _.Leaf1<T>]} NotFound1
+ * @typedef {readonly[cmp.Index5, _.Leaf2<T>]} FirstLeaf2
  */
 
 /**
  * @template T
- * @typedef {readonly[0|2|4, _.Leaf2<T>]} NotFound2
+ * @typedef {readonly[1|3, _.Branch5<T>]} FirstBranch5
  */
 
 /**
  * @template T
- * @typedef {Found1<T> | Found2<T>} Found
- */
-
-/**
- * @template T
- * @typedef {NotFound1<T> | NotFound2<T>} NotFound
- */
-
-/**
- * @template T
- * @typedef {Found<T> | NotFound<T>} First
+ * @typedef {FirstLeaf1<T> | FirstBranch3<T> | FirstLeaf2<T> | FirstBranch5<T>} First
  */
 
 /**
@@ -64,4 +54,38 @@ const cmp = require('../../function/compare')
  */
 
 /** @type {<T>(c: cmp.Compare<T>) => (node: _.Node<T>) => Result<T>} */
-const find = c => node => todo()
+const find = c => {
+    const i3 = cmp.index3(c)
+    const i5 = cmp.index5(c)
+    /** @typedef {typeof c extends cmp.Compare<infer T> ? T : never} T */
+    /** @type {(node: _.Node<T>) => Result<T>} */
+    const f = node => {
+        switch (node.length) {
+            case 1: { return [[i3(node[0]), node], undefined] }
+            case 2: { return [[i5(node), node], undefined] }
+            case 3: {
+                const i = i3(node[1])
+                switch (i) {
+                    case 0: case 2: {
+                        const [first, tail] = f(node[i])
+                        return [first, { first: [i, node], tail}]
+                    }
+                    case 1: { return [[i, node], undefined] }
+                }
+                return todo()
+            }
+            case 5: {
+                const i = i5([node[1], node[3]])
+                switch (i) {
+                    case 0: case 2: case 4: {
+                        const [first, tail] = f(node[i])
+                        return [first, { first: [i, node], tail }]
+                    }
+                    case 1: case 3: { return [[i, node], undefined]}
+                }
+            }
+        }
+        return todo()
+    }
+    return f
+}
