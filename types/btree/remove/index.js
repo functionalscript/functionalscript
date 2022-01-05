4a6af49390436d32ce6932767398b75994084466
sergey-shandar <sergey-shandar@users.noreply.github.com>
2022-01-04 17:22:49 -0800

    reduce operator

--- types/btree/remove/index.js
+++ types/btree/remove/index.js
@@ -32,72 +32,49 @@ const path = tail => n => {
  * @typedef {_.Branch1<T> | _.Branch3<T> | _.Branch5<T>} Branch
  */
 
+/** @type {<T>(a: Branch<T>) => (n: _.Branch3<T>) => _.Branch1<T> | _.Branch3<T>} */
+const reduceValue0 = a => n => {
+    const [, v1, n2] = n
+    if (a.length === 1) {
+        switch (n2.length) {
+            case 3: { return [[a[0], v1, ...n2]] }
+            case 5: { return [[a[0], v1, n2[0]], n2[1], [n2[2], n2[3], n2[4]]] }
+            default: { throw 'invalid node' }
+        }
+    } else {
+        return [a, v1, n2]
+    }
+}
+
+/** @type {<T>(a: Branch<T>) => (n: _.Branch3<T>) => _.Branch1<T> | _.Branch3<T>} */
+const reduceValue2 = a => n => {
+    const [n0, v1, ] = n
+    if (a.length === 1) {
+        switch (n0.length) {
+            case 3: { return [[...n0, v1, a[0]]] }
+            case 5: { return [[n0[0], n0[1], n0[2]], n0[3], [n0[4], v1, a[0]]] }
+            default: { throw 'invalid node' }
+        }
+    } else {
+        return [n0, v1, a]
+    }
+}
+
 /** @type {<T>(a: Branch<T>) => (i: find.PathItem<T>) => Branch<T>} */
 const reduce = a => i => {
+    /** @typedef {typeof a extends Branch<infer T> ? T : never} T */
+    /** @type {(r: (a: Branch<T>) => (n: _.Branch3<T>) => _.Branch1<T> | _.Branch3<T>) => Branch<T>} */
+    const f = r => {
+        const n = i[1]
+        const ra = r(a)
+        return n.length === 3 ? ra(n) : [...ra([n[0], n[1], n[2]]), n[3], n[4]]
+    }
     switch (i[0]) {
-        case 0: {
-            const n = i[1]
-            if (n.length === 3) {
-                const [, v1, n2] = n
-                if (a.length === 1) {
-                    switch (n2.length) {
-                        case 3: { return [[a[0], v1, ...n2]] }
-                        case 5: { return [[a[0], v1, n2[0]], n2[1], [n2[2], n2[3], n2[4]]] }
-                        default: { throw 'invalid node' }
-                    }
-                } else {
-                    return [a, v1, n2]
-                }
-            } else {
-                const [, v1, n2, v3, n4] = n
-                if (a.length === 1) {
-                    switch (n2.length) {
-                        case 3: { return [[a[0], v1, ...n2], v3, n4] }
-                        case 5: { return [[a[0], v1, n2[0]], n2[1], [n2[2], n2[3], n2[4]], v3, n4] }
-                        default: { throw 'invalid node' }
-                    }
-                } else {
-                    return [a, v1, n2, v3, n4]
-                }
-            }
-        }
-        case 2: {
-            const n = i[1]
-            if (n.length === 3) {
-                const [n0 ,v1] = n
-                if (a.length === 1) {
-                    switch (n0.length) {
-                        case 3: { return [[...n0, v1, a[0]]] }
-                        case 5: { return [[n0[0], n0[1], n0[2]], n0[3], [n0[4], v1, a[0]]] }
-                        default: { throw 'invalid node' }
-                    }
-                } else {
-                    return [n0, v1, a]
-                }
-            } else {
-                const [n0, v1, , v3, n4] = n
-                if (a.length === 1) {
-                    switch (n0.length) {
-                        case 3: { return [[...n0, v1, a[0]], v3, n4] }
-                        case 5: { return [[n0[0], n0[1], n0[2]], n0[3], [n0[4], v1, a[0]], v3, n4] }
-                        default: { throw 'invalid node' }
-                    }
-                } else {
-                    return [n0, v1, a, v3, n4]
-                }
-            }
-        }
+        case 0: { return f(reduceValue0) }
+        case 2: { return f(reduceValue2) }
         case 4: {
-            const [n0, v1, n2, v3, ] = i[1]
-            if (a.length === 1) {
-                switch (n2.length) {
-                    case 3: { return [n0, v1, [...n2, v1, a[0]]] }
-                    case 5: { return [n0, v1, [n2[0], n2[1], n2[2]], n2[3], [n2[4], v1, a[0]]] }
-                    default: { throw 'invalid node' }
-                }
-            } else {
-                return [n0, v1, n2, v3, a]
-            }
+            const n = i[1]
+            return [n[0], n[1], ...reduceValue2(a)([n[2], n[3], n[4]])]
         }
     }
 }
