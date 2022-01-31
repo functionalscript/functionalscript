b384589302a7878ecae2e1accda309330dee94e1
Trinidadec <msden.ua@gmail.com>
2022-02-01 00:43:46 +0200

    tokenize symbols

--- json/tokenizer/index.js
+++ json/tokenizer/index.js
@@ -34,7 +34,8 @@ const list = require('../../types/list')
 
 /** @typedef {{readonly kind: 'null'}} NullToken */
 
-/** @typedef {{readonly kind: ''}} ErrorToken */
+/** @typedef {{readonly kind: 'error'}} ErrorToken */
+
 /** 
  * @typedef {|
  * LeftBraceToken |
@@ -53,27 +54,93 @@ const list = require('../../types/list')
  * } JsonToken
  */
 
-/** @typedef {undefined} TokenizerState */
+const leftBrace = 0x7b;
+const rightBrace = 0x7d;
+const colon = 0x3a;
+const comma = 0x2c;
+const leftBracket = 0x5b;
+const rightBracket = 0x5d;
+
+const quotationMark = 0x22;
+const digit0 = 0x30;
+const digit1 = 0x31;
+const digit2 = 0x32;
+const digit3 = 0x33;
+const digit4 = 0x34;
+const digit5 = 0x35;
+const digit6 = 0x36;
+const digit7 = 0x37;
+const digit8 = 0x38;
+const digit9 = 0x39;
+const signPlus = 0x2b;
+const signMinus = 0x2d;
+
+const letterF = 0x66;
+const letterN = 0x6e;
+const letterT = 0x74;
+
+/** @type {LeftBraceToken} */
+const leftBraceToken = {kind: '{'}
+
+/** @type {RightBraceToken} */
+const rightBraceToken = {kind: '}'}
+
+/** @type {ColonToken} */
+const colonToken = {kind: ':'}
+
+/** @type {CommaToken} */
+const commaToken = {kind: ','}
+
+/** @type {LeftBracketToken} */
+const leftBracketToken = {kind: '['}
+
+/** @type {RightBracketToken} */
+const rightBracketToken = {kind: ']'}
+
+/** @typedef {(input: JsonCharacter) => readonly[JsonToken, TokenizerState]} TokenizerState */
 
 /** @typedef {number|undefined} JsonCharacter */
 
+/** @type {TokenizerState} */
+const initialState = input => 
+{
+    switch(input)
+    {
+        case leftBrace: return [leftBraceToken, initialState]
+        case rightBrace: return [rightBraceToken, initialState]
+        case colon: return [colonToken, initialState]
+        case comma: return [commaToken, initialState]
+        case leftBracket: return [leftBracketToken, initialState]
+        case rightBracket: return [rightBracketToken, initialState]
+        case letterT: return todo()
+        case letterF: return todo()
+        case letterN: return todo()
+        case quotationMark: return todo()
+        case digit0: return todo()
+        case digit1: 
+        case digit2:
+        case digit3:
+        case digit4:
+        case digit5:
+        case digit6:
+        case digit7:
+        case digit8:
+        case digit9: return todo()
+        case signPlus:
+        case signMinus: return todo()
+        case undefined: return[undefined, eofState]
+        default: return [{kind: 'error'}, initialState]
+    }
+}
+
+/** @type {TokenizerState} */
+const eofState = input => [{kind: 'error'}, eofState]
+
 /** @type {operator.StateScan<JsonCharacter, TokenizerState, JsonToken>} */
-const tokenizeOp = state => input => input === undefined ? tokenizeEof(state) : tokenizeUtfChar(state)(input)
-
-/** @type {operator.StateScan<number, TokenizerState, JsonToken>} */
-const tokenizeUtfChar = state => input => 
-{
-    return todo()
-}
-
-/** @type {(state: TokenizerState) => readonly[JsonToken, TokenizerState]} */
-const tokenizeEof = state =>
-{
-    return todo()
-}
+const tokenizeOp = state => input => state(input)
 
 /** @type {(input: list.List<JsonCharacter>) => list.List<JsonToken>} */
-const tokenize = input => list.stateScan(tokenizeOp)(undefined)(input)
+const tokenize = input => list.stateScan(tokenizeOp)(initialState)(input)
 
 module.exports = {
     /** @readonly */
--- json/tokenizer/test.js
+++ json/tokenizer/test.js
@@ -1,1 +1,83 @@
-const json = require('.')
+const tokenizer = require('.')
+const list = require('../../types/list')
+
+/** @type {(s: string) => list.List<tokenizer.JsonCharacter>} */
+const toCharacters = s =>
+{   
+    let characters = [];
+    for(var i = 0; i < s.length; i++) {
+        var char = s.charCodeAt(i);
+        characters.push(s.charCodeAt(i))
+    }
+    /** @type {list.List<tokenizer.JsonCharacter>} */
+    const jsonCharacters = characters
+    return list.concat(jsonCharacters)([undefined])
+}
+
+/** @type {(value: tokenizer.JsonToken) => boolean} */
+const definedTokenPredicate = token =>token !== undefined 
+
+/** @type {(s: string) => readonly tokenizer.JsonToken[]} */
+const tokenizeString = s =>
+{   
+    const characters = toCharacters(s)
+    const tokens = tokenizer.tokenize(characters)
+    return list.toArray(list.filter(definedTokenPredicate)(tokenizer.tokenize(characters)))
+}
+
+{
+    const result = tokenizeString('')
+    if (result.length !== 0){ throw result }
+}
+
+{
+    const result = tokenizeString('{')
+    if (result.length !== 1){ throw result }
+    if (result[0]?.kind !== '{') { throw result }
+}
+
+{
+    const result = tokenizeString('}')
+    if (result.length !== 1){ throw result }
+    if (result[0]?.kind !== '}') { throw result }
+}
+
+{
+    const result = tokenizeString(':')
+    if (result.length !== 1){ throw result }
+    if (result[0]?.kind !== ':') { throw result }
+}
+
+{
+    const result = tokenizeString(',')
+    if (result.length !== 1){ throw result }
+    if (result[0]?.kind !== ',') { throw result }
+}
+
+{
+    const result = tokenizeString('[')
+    if (result.length !== 1){ throw result }
+    if (result[0]?.kind !== '[') { throw result }
+}
+
+{
+    const result = tokenizeString(']')
+    if (result.length !== 1){ throw result }
+    if (result[0]?.kind !== ']') { throw result }
+}
+
+{
+    const result = tokenizeString('err')
+    if (result.length !== 3){ throw result }
+    if (result[0]?.kind !== 'error') { throw result }
+    if (result[1]?.kind !== 'error') { throw result }
+    if (result[2]?.kind !== 'error') { throw result }
+}
+
+{
+    const result = tokenizeString('{e}')
+    if (result.length !== 3){ throw result }
+    if (result[0]?.kind !== '{') { throw result }
+    if (result[1]?.kind !== 'error') { throw result }
+    if (result[2]?.kind !== '}') { throw result }
+}
