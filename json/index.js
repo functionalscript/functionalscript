248919e2085a495dcd17ff13165dc95e23916ca6
sergey-shandar <sergey-shandar@users.noreply.github.com>
2022-02-10 17:13:27 -0800

    simplify

--- json/index.js
+++ json/index.js
@@ -116,7 +116,7 @@ const stringify = sort => value => {
 }
 
 /** @type {(value: string) => Unknown} */
-const parse = value => JSON.parse(value)
+const parse = JSON.parse
 
 /** @type {(value: Unknown) => value is Object} */
 const isObject = value => typeof value === 'object' && value !== null && !(value instanceof Array)
