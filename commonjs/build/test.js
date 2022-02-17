3048b8472162edc99a00a29769bc778af2e07d27
sergey-shandar <sergey-shandar@users.noreply.github.com>
2022-02-17 02:18:25 -0800

    todo

--- commonjs/build/test.js
+++ commonjs/build/test.js
@@ -1,9 +1,23 @@
 const _ = require('.')
 const { todo } = require('../../dev')
 const map = require('../../types/map')
+const module_ = require('../module')
+const function_ = require('../module/function')
+const result = require('../../types/result')
 
-const compile = todo
+/** @type {{ readonly [k in string]?: result.Result<function_.Function_, unknown> }} */
+const compileMap = {
+    'a': ['ok', todo]
+}
+
+/** @type {function_.Compile} */
+const compile = source => compileMap[source] ?? ['error', 'not found']
 
 {
-    // _.getOrBuild(compile)(todo)(map)
+    const x = _.getOrBuild
+        (compile)
+        (todo)
+        (/** @type {module_.MapInterface<map.Map<module_.State>>} */(map))
+        ({ package: '', path: [] })
+        (undefined)
 }
