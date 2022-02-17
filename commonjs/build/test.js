ed7e121b5338eb60f89dbbc52b1eab60431b4035
sergey-shandar <sergey-shandar@users.noreply.github.com>
2022-02-17 08:28:03 -0800

    working version

--- commonjs/build/test.js
+++ commonjs/build/test.js
@@ -4,19 +4,43 @@ const map = require('../../types/map')
 const module_ = require('../module')
 const function_ = require('../module/function')
 const result = require('../../types/result')
+const package_ = require('../package')
 
 /** @type {{ readonly [k in string]?: result.Result<function_.Function_, unknown> }} */
 const compileMap = {
-    'a': ['ok', todo]
+    ':a': [
+        'ok',
+        require_ => m0 => {
+            let m = m0
+            m = require_('./b')(m0)[1];
+            return [['ok', {}], m]
+        }]
 }
 
 /** @type {function_.Compile} */
-const compile = source => compileMap[source] ?? ['error', 'not found']
+const compile = source => compileMap[source] ?? ['error', 'invalid source']
+
+/** @type {{ readonly [k in string]?: { readonly [k in string]?: string } }} */
+const packageMap = {
+    '': {
+        'a': ':a'
+    }
+}
+
+/** @type {package_.Get} */
+const packageGet = packageId => {
+    const p = packageMap[packageId]
+    if (p === undefined) { return undefined }
+    return {
+        dependency: () => undefined,
+        file: file => p[file]
+    }
+}
 
 {
     const x = _.getOrBuild
         (compile)
-        (todo)
+        (packageGet)
         (/** @type {module_.MapInterface<map.Map<module_.State>>} */(map))
         ({ package: '', path: [] })
         (undefined)
