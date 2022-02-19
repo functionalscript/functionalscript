02015aa0eb43dc41c8c1d6a86c379ffe269c50d7
sergey-shandar <sergey-shandar@users.noreply.github.com>
2022-02-19 11:40:07 -0800

    wip

--- dev/test/index.js
+++ dev/test/index.js
@@ -1,8 +1,17 @@
 const { todo } = require('..')
+const list = require('../../types/list')
 const function_ = require('../../commonjs/module/function')
+const package_ = require('../../commonjs/package')
 
-/** @type {(c: function_.Compile) => never} */
-const test = compile => todo()
+/**
+ * @type {(c: function_.Compile) =>
+ *  (files: string) =>
+ *  (packageGet: package_.Get) =>
+ *  }
+ */
+const test = compile => files => packageGet => {
+    return todo()
+}
 
 module.exports = {
     /** @readonly */
