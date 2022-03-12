e49fbd9069c82ba8682424e5d11a8a662c9ce4a7
sergey-shandar <sergey-shandar@users.noreply.github.com>
2022-03-12 13:09:46 -0800

    inital

--- json/tokenizer/index.js
+++ json/tokenizer/index.js
@@ -391,8 +391,10 @@ const tokenizeEofOp = state => {
 /** @type {operator.StateScan<CharCodeOrEof, TokenizerState, list.List<JsonToken>>} */
 const tokenizeOp = state => input => input === undefined ? tokenizeEofOp(state) : tokenizeCharCodeOp(state)(input)
 
+const initial = list.stateScan(tokenizeOp)({ kind: 'initial' })
+
 /** @type {(input: list.List<CharCodeOrEof>) => list.List<JsonToken>} */
-const tokenize = input => list.flat(list.stateScan(tokenizeOp)({kind: 'initial'})(input))
+const tokenize = input => list.flat(initial(input))
 
 /** @type {(input: list.List<number>) => list.List<JsonToken>} */
 const tokenize2 = input => tokenize(list.concat(/** @type {list.List<CharCodeOrEof>} */(input))([undefined]))
