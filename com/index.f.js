dd47a19e8e48d3632f416e20cfad77892628f8e0
sergey-shandar <sergey-shandar@users.noreply.github.com>
2022-07-09 22:27:33 -0700

    COM types

---
+++ com/index.f.js
@@ -0,0 +1,26 @@
+/** @typedef {{readonly[k in string]: Definition}} Library */
+
+/** @typedef {Struct|Interface} Definition */
+
+/** @typedef {{readonly struct: FieldArray}} Struct */
+
+/** @typedef {readonly Field[]} FieldArray */
+
+/** @typedef {[string, Type]} Field */
+
+/**
+ * @typedef {{
+ *  readonly interface: MethodArray
+ *  readonly guid: string
+ * }} Interface
+ */
+
+/** @typedef {readonly Method[]} MethodArray */
+
+/** @typedef {readonly[string, FieldArray, Type]} Method */
+
+/** @typedef {string|Pointer} Type */
+
+/** @typedef {readonly['*', Type]} Pointer */
+
+module.exports = {}
---
+++ com/test.f.js
@@ -0,0 +1,20 @@
+const _ = require('./index.f.js')
+
+/** @type {_.Library} */
+const library = {
+    Slice: {
+        struct: [
+            ['Start', ['*', 'u8']],
+            ['End', 'usize'],
+        ]
+    },
+    IMy: {
+        guid: 'C66FB270-2D80-49AD-BB6E-88C1F90B805D',
+        interface: [
+            ['GetSlice', [], 'Slice'],
+            ['SetSlice', [['slice', 'Slice']], ''],
+        ],
+    }
+}
+
+module.exports = {}
