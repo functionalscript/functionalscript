4b914c08a6c999b41502d04560ff791d7dee4b90
sergey-shandar <sergey-shandar@users.noreply.github.com>
2022-08-01 02:07:12 -0700

    OS

--- com/cpp/test/build.cjs
+++ com/cpp/test/build.cjs
@@ -1,10 +1,12 @@
 const fs = require('node:fs')
 const cp = require('node:child_process')
 const cpp = require('../test.f.cjs').result
+const os = require('node:os');
 
 fs.writeFileSync('_result.hpp', cpp)
 try {
-    console.log(cp.execSync('clang -std=c++11 -lc++ main.cpp').toString())
+    const flags = os.platform() === 'win32' ? '' : '-std=c++11 -lc++ '
+    console.log(cp.execSync(`clang${flags} main.cpp`).toString())
 } catch (e) {
     // @ts-ignore
     console.error(e.output.toString())
