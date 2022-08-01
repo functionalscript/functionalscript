fc859b85e8e7c79ec3076543465e6f24e0fb4dd2
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2022-08-01 02:08:27 -0700

    C++ on mac

--- com/cpp/test/build.cjs
+++ com/cpp/test/build.cjs
@@ -5,7 +5,7 @@ const os = require('node:os');
 
 fs.writeFileSync('_result.hpp', cpp)
 try {
-    const flags = os.platform() === 'win32' ? '' : '-std=c++11 -lc++ '
+    const flags = os.platform() === 'win32' ? '' : ' -std=c++11 -lc++'
     console.log(cp.execSync(`clang${flags} main.cpp`).toString())
 } catch (e) {
     // @ts-ignore
