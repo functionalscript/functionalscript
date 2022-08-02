f9ad58b375cc4c132846abb4c0f46b65ab7db1a7
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2022-08-01 17:37:31 -0700

    writeFileSync

--- nodejs/version/module.f.cjs
+++ nodejs/version/module.f.cjs
@@ -6,6 +6,13 @@ const pack = require('../../commonjs/package/module.f.cjs')
  * }} ChildProcess
  */
 
+/**
+ * @typedef {{
+ *  readonly readFileSync: (name: string) => Buffer
+ *  readonly writeFileSync: (name: string, content: string) => void
+ * }} Fs
+ */
+
 /** @type {(p: pack.PackageJson) => (cp: ChildProcess) => string} */
 const version = p => cp =>
     JSON.stringify({ ...p, version: `0.0.${cp.execSync('git log --oneline').toString().split('\n').length - 1}` }, null, 2)
