46827e54a3ce5873d3bcd49b97eb092a9301a1fa
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2022-08-16 21:55:08 -0700

    result

--- types/result/module.cjs
+++ types/result/module.cjs
@@ -1,12 +1,13 @@
 const result = require('./module.f.cjs')
+const { ok, error } = result
 
 /** @type {<T>(f: () => T) => result.Result<T, unknown>} */
 const tryCatch = f => {
     // Side effect: `try catch` is not allowed in FunctionalScript.
     try {
-        return result.ok(f())
+        return ok(f())
     } catch (e) {
-        return result.error(e)
+        return error(e)
     }
 }
 
