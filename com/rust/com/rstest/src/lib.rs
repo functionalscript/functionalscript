171d68423280c0f06d7ffc764485f72fbdb07e55
sergey-shandar <sergey-shandar@users.noreply.github.com>
2022-08-24 12:33:44 -0700

    rust: IUnknown

--- com/rust/com/rstest/src/lib.rs
+++ com/rust/com/rstest/src/lib.rs
@@ -1,8 +1,19 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+type IID = u128;
+
+pub struct IUnknown {
+    pub vmt: &'static IUnknownVmt,
+}
+
+type HRESULT = u32;
+type ULONG = u32;
+
+#[allow(non_snake_case)]
+pub struct IUnknownVmt {
+    pub QueryInterface: extern "stdcall" fn(
+        this: &mut IUnknown,
+        riid: &IID,
+        ppvObject: &mut *mut IUnknown,
+    ) -> HRESULT,
+    pub AddRef: extern "stdcall" fn(this: &mut IUnknown) -> ULONG,
+    pub Release: extern "stdcall" fn(this: &mut IUnknown) -> ULONG,
 }
