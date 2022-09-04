71b4ab8d20d3dede41f085f0c9f79ac17efd2646
sergey-shandar <sergey-shandar@users.noreply.github.com>
2022-09-04 10:36:15 -0700

    Extension

--- com/rust/com/rstest/src/lib.rs
+++ com/rust/com/rstest/src/lib.rs
@@ -1,48 +1,77 @@
 #[cfg(test)]
 mod test {
-    use com::{Object, Ref, Interface, GUID, Vmt, Class, CObject};
+    use com::{CObject, Class, Interface, Object, Ref, Vmt, GUID};
 
     struct IMy {
-        a: unsafe extern "stdcall" fn(this: &Object<IMy>) -> Ref<IMy>,
-        b: unsafe extern "stdcall" fn(this: &Object<IMy>) -> u32,
+        pub a: unsafe extern "stdcall" fn(this: &Object<IMy>) -> Ref<IMy>,
+        pub b: unsafe extern "stdcall" fn(this: &Object<IMy>) -> u32,
     }
 
     impl Interface for IMy {
         const GUID: GUID = 0x01234567_89AB_CDEF_0123_456789ABCDEF;
     }
 
-    trait IMyClass: Class<Interface = IMy> {
-        fn a(this: &CObject<Self>) -> Ref<IMy>;
-        fn b(this: &CObject<Self>) -> u32;
-        const INTERFACE_DEF: IMy = IMy { a: a::<Self>, b: b::<Self> };
+    trait IMyEx {
+        fn a(&self) -> Ref<IMy>;
+        fn b(&self) -> u32;
     }
 
-    extern "stdcall" fn a<T: IMyClass>(this: &Object<IMy>) -> Ref<IMy> {
-        T::a(unsafe { T::to_cobject(this) })
+    impl IMyEx for Object<IMy> {
+        fn a(&self) -> Ref<IMy> {
+            unsafe { (self.vmt().interface.a)(self) }
+        }
+        fn b(&self) -> u32 {
+            unsafe { (self.vmt().interface.b)(self) }
+        }
     }
-    extern "stdcall" fn b<T: IMyClass>(this: &Object<IMy>) -> u32 {
-        T::b(unsafe { T::to_cobject(this) })
+
+    trait IMyVmt: Class<Interface = IMy>
+    where
+        CObject<Self>: IMyEx,
+    {
+        const CLASS_INTERFACE: IMy = IMy {
+            a: a::<Self>,
+            b: b::<Self>,
+        };
+    }
+
+    impl<T: Class<Interface = IMy>> IMyVmt for T
+    where CObject<T>: IMyEx
+    {}
+
+    extern "stdcall" fn a<T: IMyVmt>(this: &Object<IMy>) -> Ref<IMy>
+    where
+        CObject<T>: IMyEx,
+    {
+        unsafe { T::to_cobject(this) }.a()
+    }
+
+    extern "stdcall" fn b<T: IMyVmt>(this: &Object<IMy>) -> u32
+    where
+        CObject<T>: IMyEx,
+    {
+        unsafe { T::to_cobject(this) }.b()
     }
 
     //
 
-    struct X (u32);
+    struct X(u32);
 
     impl Class for X {
         type Interface = IMy;
-        const INTERFACE: Self::Interface = X::INTERFACE_DEF;
+        const INTERFACE: Self::Interface = Self::CLASS_INTERFACE;
         fn static_vmt() -> &'static Vmt<Self::Interface> {
             static V: Vmt<IMy> = X::VMT;
             &V
         }
     }
 
-    impl IMyClass for X {
-        fn a(this: &CObject<Self>) -> Ref<IMy> {
-            this.to_interface().into()
+    impl IMyEx for CObject<X> {
+        fn a(&self) -> Ref<IMy> {
+            self.to_interface().into()
         }
-        fn b(this: &CObject<Self>) -> u32 {
-            this.value.0
+        fn b(&self) -> u32 {
+            self.value.0
         }
     }
 }
