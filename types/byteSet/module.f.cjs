375f22e915c58694cdf06dbff80c025f409fad60
sergey-shandar <sergey-shandar@users.noreply.github.com>
2022-09-04 11:48:03 -0700

    byteSet

---
+++ types/byteSet/module.f.cjs
@@ -0,0 +1,24 @@
+/** @typedef {bigint} byteSet */
+/** @typedef {number} u8 */
+
+/** @type {(n: u8) => (s: byteSet) => boolean} */
+const has = n => s => ((s >> BigInt(n)) & 1n) === 1n
+
+/** @type {(n: u8) => (s: byteSet) => byteSet} */
+const set = n => s => s | (1n << BigInt(n))
+
+/** @type {(n: u8) => (s: byteSet) => byteSet} */
+// const unset = n => s =>
+
+/** @type {(r: readonly[number, number]) => (s: byteSet) => byteSet} */
+// const setRange = r => s =>
+
+// how to define FA???
+// const stateA = [init, set] ????
+
+module.exports = {
+    /** @readonly */
+    has,
+    /** @readonly */
+    set,
+}
