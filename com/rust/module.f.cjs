816d945833c6c8414ccba8166155877177caab07
sergey-shandar <sergey-shandar@users.noreply.github.com>
2022-09-05 20:28:59 -0700

    functions

--- com/rust/module.f.cjs
+++ com/rust/module.f.cjs
@@ -2,22 +2,45 @@ const types = require('../types/module.f.cjs')
 const text = require('../../text/module.f.cjs')
 const object = require('../../types/object/module.f.cjs')
 const list = require('../../types/list/module.f.cjs')
+const { flat, map } = list
+const { entries } = Object
 
-/** @type {(name: string) => text.Block} */
-const rustStruct = name => [`#[repr(C)]`, `pub struct ${name} {`, `}`]
+/** @type {(b: text.Block) => (name: string) => text.Block} */
+const rustStruct = b => name => [`#[repr(C)]`, `pub struct ${name} {`, b, `}`]
+
+/** @type {(t: types.BaseType) => string} */
+const baseType = t => t
+
+/** @type {(t: types.Id) => string} */
+const id = ([t]) => t
+
+/** @type {(t: types.Type) => string} */
+const type = t => typeof t === 'string' ? baseType(t) : t.length === 2 ? `*const ${type(t[1])}` : id(t)
+
+/** @type {(f: types.Field) => text.Item} */
+const field = ([name, t]) => `${name}: ${type(t)},`
+
+const mapField = map(field)
 
 /** @type {(fa: types.FieldArray) => (name: string) => text.Block} */
-const struct = fa => rustStruct
+const struct = fa => rustStruct(mapField(entries(fa)))
+
+/** @type {(m: types.Method) => string} */
+const method = ([name, m]) => `${name}: extern "system" fn(),`
+
+const mapMethod = map(method)
 
 /** @type {(i: types.Interface) => (name: string) => text.Block} */
-const interface_ = i => rustStruct
+const interface_ = ({interface: i}) => rustStruct(mapMethod(entries(i)))
 
 /** @type {(type: object.Entry<types.Definition>) => text.Block} */
 const def = ([name, type]) => (type.interface === undefined ? struct(type.struct) : interface_(type))(name)
 
-/** @type {(name: string) => (library: types.Library) => text.Block} */
-const rust = name => library => {
-    return list.flat(list.map(def)(Object.entries(library)))
+const mapDef = map(def)
+
+/** @type {(library: types.Library) => text.Block} */
+const rust = library => {
+    return flat(mapDef(entries(library)))
 }
 
 module.exports = {
--- com/rust/test.f.cjs
+++ com/rust/test.f.cjs
@@ -7,11 +7,19 @@ const library = require('../types/test.f.cjs')
     const e =
         '#[repr(C)]\n' +
         'pub struct Slice {\n' +
+        '    Start: *const u8,\n' +
+        '    Size: usize,\n' +
         '}\n' +
         '#[repr(C)]\n' +
         'pub struct IMy {\n' +
+        '    GetSlice: extern "system" fn(),\n' +
+        '    SetSlice: extern "system" fn(),\n' +
+        '    GetUnsafe: extern "system" fn(),\n' +
+        '    SetUnsafe: extern "system" fn(),\n' +
+        '    Some: extern "system" fn(),\n' +
+        '    GetIMy: extern "system" fn(),\n' +
         '}'
-    const r = join('\n')(flat('    ')(rust('My')(library)))
+    const r = join('\n')(flat('    ')(rust(library)))
     if (r !== e) { throw [e, r] }
 }
 
