feb39308024a3346ecda925e4be9b20c81c81a20
sergey-shandar <sergey-shandar@users.noreply.github.com>
2022-09-12 02:29:17 -0700

    fix error

--- types/bigint/test.f.cjs
+++ types/bigint/test.f.cjs
@@ -3,6 +3,6 @@ const { sum } = require('./module.f.cjs')
 module.exports = {
     test: () => {
         const result = sum([2n, 3n, 4n, 5n])
-        if (result !== 142n) { throw result }
+        if (result !== 14n) { throw result }
     }
 }
