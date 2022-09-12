e56d6c1fa614c2d7d4ef78d8eb2da035b311544a
sergey-shandar <sergey-shandar@users.noreply.github.com>
2022-09-12 12:08:46 -0700

    html

--- html/test.f.cjs
+++ html/test.f.cjs
@@ -1,15 +1,14 @@
 const _ = require('./module.f.cjs')
 
-{
-    const r = _.htmlToString(['html', []])
-    if (r !== '<!DOCTYPE html><html></html>') { throw r }
+module.exports = {
+    empty: () => {
+        const r = _.htmlToString(['html', []])
+        if (r !== '<!DOCTYPE html><html></html>') { throw r }
+    },
+    some: () => {
+        /** @type {_.Element} */
+        const x = ['div', {}, ['<div>&amp;</div>', ['a', { href: 'hello"' }, []]]]
+        const s = _.htmlToString(x)
+        if (s !== '<!DOCTYPE html><div>&lt;div&gt;&amp;amp;&lt;/div&gt;<a href="hello&quot;"></a></div>') { throw s }
+    }
 }
-
-{
-    /** @type {_.Element} */
-    const x = ['div', {}, ['<div>&amp;</div>', ['a', { href: 'hello"' }, []]]]
-    const s = _.htmlToString(x)
-    if (s !== '<!DOCTYPE html><div>&lt;div&gt;&amp;amp;&lt;/div&gt;<a href="hello&quot;"></a></div>') { throw s }
-}
-
-module.exports = {}
