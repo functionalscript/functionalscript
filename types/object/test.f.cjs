b4601319fe949a22a21a31dbe31bdb500f27c3dd
sergey-shandar <sergey-shandar@users.noreply.github.com>
2022-09-12 03:37:06 -0700

    test functions

--- types/object/test.f.cjs
+++ types/object/test.f.cjs
@@ -1,15 +1,14 @@
 const _ = require('./module.f.cjs')
 
-{
-    const a = {}
-    const value = _.at('constructor')(a)
-    if (value !== undefined) { throw value }
+module.exports = {
+    ctor: () => {
+        const a = {}
+        const value = _.at('constructor')(a)
+        if (value !== undefined) { throw value }
+    },
+    property: () => {
+        const a = { constructor: 42 }
+        const value = _.at('constructor')(a)
+        if (value !== 42) { throw value }
+    }
 }
-
-{
-    const a = { constructor: 42}
-    const value = _.at('constructor')(a)
-    if (value !== 42) { throw value }
-}
-
-module.exports = {}
--- types/option/test.f.cjs
+++ types/option/test.f.cjs
@@ -1,11 +1,9 @@
 const _ = require('./module.f.cjs')
 
-{
+module.exports = () => {
     const optionSq = _.map(v => v * v)
     const sq3 = optionSq(3)
     if (sq3 !== 9) { throw sq3 }
     const sqUndefined = optionSq(undefined)
     if (sqUndefined !== undefined) { throw sqUndefined }
 }
-
-module.exports = {}
