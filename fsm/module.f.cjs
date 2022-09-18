18d88043ece85885ac358164a315aa679e6272ad
sergey-shandar <sergey-shandar@users.noreply.github.com>
2022-09-18 11:35:53 -0700

    grammar

--- fsm/module.f.cjs
+++ fsm/module.f.cjs
@@ -1,5 +1,9 @@
+const list = require('../types/list/module.f.cjs')
+
 /** @typedef {number} U8 */
 
 /** @typedef {readonly[string, U8, string]} Rule */
+
+/** @typedef {list.List<Rule>} Grammar */
 
 module.exports = {}
