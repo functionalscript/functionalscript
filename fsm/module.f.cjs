9cfd911ef1a430214809b76013c39049accf4089
Shandar Denys <msden.ua@gmail.com>
2022-09-25 21:50:17 +0300

    revert

--- fsm/module.f.cjs
+++ fsm/module.f.cjs
@@ -1,9 +1,6 @@
 const { todo } = require('../dev/module.f.cjs')
 const list = require('../types/list/module.f.cjs')
 const byteSet = require('../types/byte_set/module.f.cjs')
-const object = require('../types/object/module.f.cjs')
-const operator = require('../types/function/operator/module.f.cjs')
-const map = require('../types/map/module.f.cjs')
 
 /** @typedef {readonly[string, byteSet.ByteSet, string]} Rule */
 
@@ -20,26 +17,8 @@ const map = require('../types/map/module.f.cjs')
 /** @type {(faId: string) => string} */
 const escape = faId => faId.replaceAll('\\', '\\\\').replaceAll('|', '\\|')
 
-/** @type {(rule: Rule) =>  object.Entry<Rule>} */
-const toEntry = r => [escape(r[2]), r]
-
 /** @type {(grammar: Grammar) => Dfa} */
-const dfa = grammar =>
-{
-    const sorted = object.sort(list.map(toEntry)(grammar))
-    return todo()
-}
-
-/** @type {(byteMap: ByteMap|undefined) => (byteSet: byteSet.ByteSet) => (name: string) => Dfa} */
-const merge = byteMap => byteSet => name => todo()
-
-/** @type {operator.FoldT<object.Entry<Rule>, map.Map<ByteMap>} */
-const foldOp = r => m => {
-    const fromName = escape(r[1][0])
-    const toName = r[0]
-    const byteMap = merge(map.at(fromName)(m))(r[1][1])(toName)
-    return todo()
-}
+const dfa = grammar => todo()
 
 module.exports = {
     /** @readonly */
