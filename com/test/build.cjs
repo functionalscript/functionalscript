cc1af70ed4f69c8157e0bb8bc9c70edda04c0ac0
sergey-shandar <sergey-shandar@users.noreply.github.com>
2022-10-16 11:16:13 -0700

    build

--- com/test/build.cjs
+++ com/test/build.cjs
@@ -1,37 +1,44 @@
 const fs = require('node:fs')
 const cp = require('node:child_process')
 const os = require('node:os');
-const cpp = require('../cpp/test.f.cjs').result
 const { string: { join }, list: { flat } } = require('../../types/module.f.cjs')
 
 const dirname = __dirname
 
-fs.writeFileSync(`${dirname}/cpp/_result.hpp`, cpp)
-try {
+{
+    const cpp = require('../cpp/test.f.cjs').result
     const flags = os.platform() === 'win32' ? [] : ['-std=c++11', '-lc++']
     const line = join(' ')(flat([['clang'], flags, [dirname + '/cpp/main.cpp']]))
-    console.log(cp.execSync(line).toString())
-} catch (e) {
-    // @ts-ignore
-    console.error(e.output.toString())
+
+    fs.writeFileSync(`${dirname}/cpp/_result.hpp`, cpp)
+    try {
+        console.log(cp.execSync(line).toString())
+    } catch (e) {
+        // @ts-ignore
+        console.error(e.output.toString())
+    }
 }
 
-const cs = require('../cs/test.f.cjs').result
+{
+    const cs = require('../cs/test.f.cjs').result
 
-fs.writeFileSync(`${dirname}/cs/_result.cs`, cs)
-try {
-    console.log(cp.execSync(`dotnet build ${dirname}/cs/cs.csproj`).toString())
-} catch (e) {
-    // @ts-ignore
-    console.error(e.output.toString())
+    fs.writeFileSync(`${dirname}/cs/_result.cs`, cs)
+    try {
+        console.log(cp.execSync(`dotnet build ${dirname}/cs/cs.csproj`).toString())
+    } catch (e) {
+        // @ts-ignore
+        console.error(e.output.toString())
+    }
 }
 
-const rust = require("../rust/test.f.cjs").result();
+{
+    const rust = require("../rust/test.f.cjs").result();
 
-fs.writeFileSync(`${dirname}/rust/src/_result.rs`, rust);
-try {
-    console.log(cp.execSync("cargo build").toString());
-} catch (e) {
-    // @ts-ignore
-    console.error(e.output.toString());
+    fs.writeFileSync(`${dirname}/rust/src/_result.rs`, rust);
+    try {
+        console.log(cp.execSync("cargo build").toString());
+    } catch (e) {
+        // @ts-ignore
+        console.error(e.output.toString());
+    }
 }
