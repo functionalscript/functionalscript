bb39ef26f93d8af63c3a3eaa4395b433309b1de0
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2022-10-20 01:09:40 -0700

    Rust implementation (#165)
    * Vmt
    
    * VMT

--- com/test/rust/src/lib.rs
+++ com/test/rust/src/lib.rs
@@ -1,1 +1,43 @@
+use crate::_result::IMy::ClassEx;
+
 mod _result;
+
+struct My {}
+
+impl nanocom::Class for My {
+    type Interface = _result::IMy::Interface;
+    fn static_vmt() -> &'static _result::IMy::Vmt {
+        static VMT: _result::IMy::Vmt = My::VMT;
+        &VMT
+    }
+}
+
+impl _result::IMy::Ex for nanocom::CObject<My> {
+    fn GetSlice(&self) -> _result::Slice {
+        todo!()
+    }
+
+    fn SetSlice(&self, slice: _result::Slice) {
+        todo!()
+    }
+
+    fn GetUnsafe(&self) -> *const bool {
+        todo!()
+    }
+
+    fn SetUnsafe(&self, p: *const _result::Slice, size: u32) {
+        todo!()
+    }
+
+    fn Some(&self, p: &_result::IMy::Object) -> bool {
+        todo!()
+    }
+
+    fn GetIMy(&self) -> _result::IMy::Ref {
+        todo!()
+    }
+
+    fn SetManagedStruct(&self, a: _result::ManagedStruct) {
+        todo!()
+    }
+}
