dd0c6e9548e9a5942e4bd0c853360589f2d8aa4b
Shandar Denys <msden.ua@gmail.com>
2022-10-21 21:37:24 +0300

    range map merge

--- types/range_map/module.f.cjs
+++ types/range_map/module.f.cjs
@@ -28,15 +28,34 @@ const operator = require("../function/operator/module.f.cjs")
   * }} Operators
  */
 
- /** @type {<T>(op: Operators<T>) => (a: RangeMap<T>) => (b:RangeMap<T>) => RangeMap<T>} */
+/**
+ * @template T
+ * @typedef {option.Option<Entry<T>>} RangeState
+ */
+
+/** @type {<T>(union: operator.Reduce<T>) => (equal: operator.Equal<T>) => sortedList.ReduceOp<Entry<T>, RangeState<T>>} */
+const reduceOp = union => equal => state => a => b => {
+  const sign = unsafeCmp(a[1])(b[1])
+  const min = sign === 1 ? a[1] : b[1]
+  const u = union(a[0])(b[0])
+  if (state !== undefined && equal(state[0])(u)) {
+    return [undefined, sign, state]
+  }
+  return [state, sign, [u, min]]
+}
+
+/** @type {<T>(equal: operator.Equal<T>) => sortedList.TailReduce<Entry<T>, RangeState<T>>} */
+const tailReduce = equal => state => tail => {
+  if (state === undefined) { return tail }
+  const next = list.next(tail)
+  if (next !== undefined  && equal(state[0])(next.first[0])) { return { first: state, tail: next.tail} }
+  return { first: state, tail: tail}
+}
+
+ /** @type {<T>(op: Operators<T>) => (a: list.List<Entry<T>>) => (b:list.List<Entry<T>>) => list.List<Entry<T>} */
 const merge = op => {
     const { union, equal } = op
-    /** @typedef {typeof op extends Operators<infer T> ? T : never} T*/
-    /** @type {(a: RangeMap<T>) => (b:RangeMap<T>) => RangeMap<T>} */
-    const f = a => b => () => {
-      return todo()
-    }
-    return f
+    return sortedList.genericMerge({reduceOp: reduceOp(union)(equal), tailReduce: tailReduce(equal)})(undefined)
 }
 
 module.exports = {
