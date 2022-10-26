0c427f3f6b3dfb8573761e62a46727c7440365d3
Shandar Denys <msden.ua@gmail.com>
2022-10-26 20:16:04 +0300

    remove escape function

--- fsm/test.f.cjs
+++ fsm/test.f.cjs
@@ -1,14 +1,4 @@
 const _ = require('./module.f.cjs')
 
 module.exports = {
-    escape: [
-        () => {
-            const result = _.escape('abc')
-            if (result !== 'abc') { throw result }
-        },
-        () => {
-            const result = _.escape('\\a|b|c\\')
-            if (result !== '\\\\a\\|b\\|c\\\\') { throw result }
-        }
-    ]
 }
