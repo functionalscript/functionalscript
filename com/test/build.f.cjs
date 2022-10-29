a4a1a77967eed70dcc0659e8d1572af18727bfc2
sergey-shandar <sergey-shandar@users.noreply.github.com>
2022-10-29 11:09:42 -0700

    multiple

--- com/test/build.f.cjs
+++ com/test/build.f.cjs
@@ -66,7 +66,6 @@ const cpp = ({dirname, platform}) => {
                 flags(platform),
                 [`${dirname}/cpp/main.cpp`]]
             ),
-            // ['clang', '-shared', 'main.o'],
         ],
     }
 }
