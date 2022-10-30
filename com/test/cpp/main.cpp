25430681208dee094e1b845decb337a545e8b4a2
sergey-shandar <sergey-shandar@users.noreply.github.com>
2022-10-29 17:26:25 -0700

    DLL_EXPORT

--- com/test/cpp/main.cpp
+++ com/test/cpp/main.cpp
@@ -1,7 +1,13 @@
 #include "../../cpp/com.hpp"
 #include "_result.hpp"
 
-__declspec(dllexport)
+#ifdef _WIN32
+#define DLL_EXPORT __declspec(dllexport)
+#else
+#define DLL_EXPORT
+#endif
+
+DLL_EXPORT
 extern "C" int c_get()
 {
     return 43;
