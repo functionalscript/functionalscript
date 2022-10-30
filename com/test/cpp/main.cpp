05b4f1fe3758477d8d55561ecb2080a0ae628db6
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2022-10-29 17:28:43 -0700

    C DLL (#171)
    * C DLL
    
    * DLL_EXPORT

--- com/test/cpp/main.cpp
+++ com/test/cpp/main.cpp
@@ -1,2 +1,14 @@
 #include "../../cpp/com.hpp"
 #include "_result.hpp"
+
+#ifdef _WIN32
+#define DLL_EXPORT __declspec(dllexport)
+#else
+#define DLL_EXPORT
+#endif
+
+DLL_EXPORT
+extern "C" int c_get()
+{
+    return 43;
+}
