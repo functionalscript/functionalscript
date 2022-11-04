64d590075261d40cfacc822cebc7a54f17a4b87e
Shandar Denys <msden.ua@gmail.com>
2022-11-04 22:36:12 +0200

    RangeMap instead of ByteMap

--- fsm/module.f.cjs
+++ fsm/module.f.cjs
@@ -1,16 +1,18 @@
 const { todo } = require('../dev/module.f.cjs')
 const list = require('../types/list/module.f.cjs')
 const byteSet = require('../types/byte_set/module.f.cjs')
+const sortedSet = require('../types/sorted_set/module.f.cjs')
+const rangeMap = require('../types/range_map/module.f.cjs')
 
 /** @typedef {readonly[string, byteSet.ByteSet, string]} Rule */
 
 /** @typedef {list.List<Rule>} Grammar */
 
-/** @typedef {readonly string[]} ByteMap */
+/** @typedef {rangeMap.RangeMap<sortedSet.SortedSet<string>>} RangeMap */
 
 /**
  * @typedef {{
- *  readonly[state in string]: ByteMap
+ *  readonly[state in string]: RangeMap
  * }} Dfa
  */
 
