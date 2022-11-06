b716bb5e5ab91652c664483f1b8efa73c252d689
Shandar Denys <msden.ua@gmail.com>
2022-11-06 20:03:41 +0200

    stringify

--- fsm/module.f.cjs
+++ fsm/module.f.cjs
@@ -5,8 +5,13 @@ const { toRangeMap } = byteSet
 const sortedSet = require('../types/sorted_set/module.f.cjs')
 const rangeMap = require('../types/range_map/module.f.cjs')
 const { unsafeCmp } = require('../types/function/compare/module.f.cjs')
-const op = require("../types/function/operator/module.f.cjs")
-const { strictEqual } = op
+const operator = require("../types/function/operator/module.f.cjs")
+const { strictEqual } = operator
+const json = require('../json/module.f.cjs')
+const { sort } = require('../types/object/module.f.cjs')
+
+/** @type {(a: readonly json.Unknown[]) => string} */
+const stringify = a => json.stringify(sort)(a)
 
 /** @typedef {readonly[string, byteSet.ByteSet, string]} Rule */
 
@@ -16,14 +21,14 @@ const { strictEqual } = op
 
 /**
  * @typedef {{
- *  readonly[state in string]: TransitionMap
+ *  readonly[state in string]: rangeMap.RangeMap<string>
  * }} Dfa
  */
 
 /** @type {rangeMap.Operators<sortedSet.SortedSet<string>>} */
 const mergeOp = { union: sortedSet.union(unsafeCmp), equal: list.equal(strictEqual) }
 
-/** @type {(state: string) => op.Fold<Rule, TransitionMap>} */
+/** @type {(state: string) => operator.Fold<Rule, TransitionMap>} */
 const foldOp = state => ([ruleIn, bs, ruleOut]) => tm => {
     if (state !== ruleIn) { return tm }
     return rangeMap.merge(mergeOp)(tm)(toRangeMap(bs)(ruleOut))
@@ -31,6 +36,12 @@ const foldOp = state => ([ruleIn, bs, ruleOut]) => tm => {
 
 /** @type {(state: string) => (grammar: Grammar) => TransitionMap} */
 const toTransitionMap = state => grammar => list.fold(foldOp(state))(undefined)(grammar)
+
+/** @type {operator.Scan<rangeMap.Entry<sortedSet.SortedSet<string>>, rangeMap.Entry<string>>} */
+const stringifyOp = ([sortedSet, max]) => [[stringify(sortedSet), max], stringifyOp]
+
+/** @type {(input: TransitionMap) => rangeMap.RangeMap<string>} */
+const stringifyEntries = input => list.scan(stringifyOp)(input)
 
 /** @type {(grammar: Grammar) => Dfa} */
 const dfa = grammar => todo()
