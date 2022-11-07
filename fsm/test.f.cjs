12883ec3eaa429880db27f8c46ed123b7ba5c126
Shandar Denys <msden.ua@gmail.com>
2022-11-08 00:46:00 +0200

    fsm test

--- fsm/test.f.cjs
+++ fsm/test.f.cjs
@@ -1,5 +1,18 @@
 const _ = require('./module.f.cjs')
+const byteSet = require('../types/byte_set/module.f.cjs')
+
+/** @type {(c: string) => number} */
+const toCharCode = c => c.charCodeAt(0)
 
 module.exports = {
-
+    dfa: () => {
+        const lowercaseAlpha = byteSet.range([toCharCode('a'), toCharCode('z')])
+        const uppercaseAlpha = byteSet.range([toCharCode('A'), toCharCode('Z')])
+        const alpha = byteSet.union(lowercaseAlpha)(uppercaseAlpha)
+        const idSymbol = byteSet.union(byteSet.one(toCharCode('_')))(byteSet.one(toCharCode('$')))
+        const idBegin = byteSet.union(alpha)(idSymbol)
+        const digit = byteSet.range([toCharCode('0'), toCharCode('9')])
+        const idNext = byteSet.union(idBegin)(digit)
+        const dot = byteSet.one(toCharCode('.'))
+    }
 }
