66f20b30994c2bb420564d3fff3c1c9ac4d5c9fe
sergey-shandar <sergey-shandar@users.noreply.github.com>
2022-11-27 23:26:25 -0800

    merge

--- fsc/module.f.cjs
+++ fsc/module.f.cjs
@@ -1,4 +1,24 @@
-const ascii = require('../text/ascii/module.f.cjs')
+const operator = require('../types/function/operator/module.f.cjs')
+const { merge } = require('../types/range_map/module.f.cjs')
+
+/** @typedef {string} State */
+
+/** @typedef {(a: number) => State} NextState */
+
+/** @type {NextState} */
+const unknownSymbol = a => `unknown symbol ${a}`
+
+/** @type {operator.Reduce<NextState>} */
+const union = a => b => {
+    if (a === unknownSymbol || a === b) { return b }
+    if (b === unknownSymbol) { return a }
+    throw [a, b]
+}
+
+const m = merge({
+    union,
+    equal: operator.strictEqual,
+})
 
 module.exports = {
 }
