307229dd25807021837b32ee8cfd215bed7dcbed
sergey-shandar <sergey-shandar@users.noreply.github.com>
2022-11-29 03:30:11 -0800

    dependencyJson

--- commonjs/package/dependencies/test.f.cjs
+++ commonjs/package/dependencies/test.f.cjs
@@ -1,7 +1,6 @@
 const _ = require('./module.f.cjs')
 
 module.exports = () => {
-    // if (!_.isDependenciesJson(undefined)) { throw 'error' }
     if (!_.isDependenciesJson(null)) { throw 'error' }
     if (!_.isDependenciesJson({})) { throw 'error' }
     if (!_.isDependenciesJson({'a':'b'})) { throw 'error' }
