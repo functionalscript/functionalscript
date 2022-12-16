d2606b47567b1f8cd06fddf7aa6435114069cacb
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2022-12-16 04:19:51 -0800

    measure test performance time (#198)
    * time
    
    * performance
    
    * total time
    
    * hrtime

--- dev/test.mjs
+++ dev/test.mjs
@@ -1,20 +1,26 @@
 import { loadModuleMap } from './module.mjs'
 
+/** @type {(s: string) => <T>(_: T) => T} */
+const log = s => state => {
+    console.log(s)
+    return state
+}
+
+/** @type {<T>(_: T) => readonly[number, T]} */
+const performanceNow = state => [performance.now(), state]
+
 // test runner.
 const main = async() => {
     const moduleMap = await loadModuleMap()
 
-    /** @type {(s: string) => <T>(_: T) => T} */
-    const log = s => state => {
-        console.log(s)
-        return state
-    }
+    // await import(self.Deno ? 'https://deno.land/std/node/fs/promises.ts' : 'node:fs/promises')
 
     /** @type {any} */
     const f = moduleMap['./dev/test/module.f.cjs'].exports
     f({
         moduleMap,
         log,
+        performanceNow,
     })
 }
 
