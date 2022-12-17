01344df8834aeb0b9051d1fc9548c3ed07c29fac
sergey-shandar <sergey-shandar@users.noreply.github.com>
2022-12-17 14:25:29 -0800

    bold

--- dev/test/module.f.cjs
+++ dev/test/module.f.cjs
@@ -1,6 +1,6 @@
 const list = require('../../types/list/module.f.cjs')
 const { fold } = list
-const { reset, fgGreen, fgRed } = require('../../text/sgr/module.f.cjs')
+const { reset, fgGreen, fgRed, bold } = require('../../text/sgr/module.f.cjs')
 const result = require('../../types/result/module.f.cjs')
 
 /**
@@ -46,11 +46,14 @@ const result = require('../../types/result/module.f.cjs')
 /** @type {(s: string) => boolean} */
 const isTest = s => s.endsWith('test.f.cjs')
 
-/** @type {readonly[number, number]} TestState */
+/** @typedef {readonly[number, number]} TestState */
+
+/** @type {(time: number) => (testState: TestState) => TestState} */
+const addTest = delta => ([time, counter]) => [time + delta, counter + 1]
 
 /**
  * @template T
- * @typedef {readonly[number, T]} FullState
+ * @typedef {readonly[TestState, T]} FullState
  */
 
 /** @type {<T>(input: Input<T>) => T} */
@@ -58,7 +61,7 @@ const main = input => {
     let { moduleMap, log, performanceNow, state, tryCatch } = input
     /** @typedef {input extends Input<infer T> ? T : never} T */
     /** @type {(i: string) => (v: unknown) => (fs: FullState<T>) => FullState<T>} */
-    const test = i => v => ([time, state]) => {
+    const test = i => v => ([ts, state]) => {
         const next = test(`${i}| `)
         switch (typeof v) {
             case 'function': {
@@ -69,13 +72,13 @@ const main = input => {
                     let e = 0;
                     [e, state] = performanceNow(state)
                     const delta = e - b
-                    time += delta
+                    ts = addTest(delta)(ts)
                     if (s === 'error') {
                         state = log(`${i}() ${fgRed}error${reset}, ${delta} ms`)(state);
-                        return [time, state]
+                        throw r
                     }
                     state = log(`${i}() ${fgGreen}ok${reset}, ${delta} ms`)(state);
-                    [time, state] = next(r)([time, state])
+                    [ts, state] = next(r)([ts, state])
                 }
                 break
             }
@@ -87,28 +90,29 @@ const main = input => {
                         [time, state] = next(v)([time, state])
                         return [time, state]
                     }
-                    [time, state] = fold
+                    [ts, state] = fold
                         (f)
-                        ([time, state])
+                        ([ts, state])
                         (v instanceof Array ? list.entries(v) : Object.entries(v))
                 }
                 break
             }
         }
-        return [time, state]
+        return [ts, state]
     }
     const next = test('| ')
     /** @type {(k: readonly[string, Module]) => (fs: FullState<T>) => FullState<T>} */
-    const f = ([k, v]) => ([time, state]) => {
+    const f = ([k, v]) => ([ts, state]) => {
         if (isTest(k)) {
             state = log(`testing ${k}`)(state);
-            [time, state] = next(v.exports)([time, state])
+            [ts, state] = next(v.exports)([ts, state])
         }
-        return [time, state]
+        return [ts, state]
     }
-    let time = 0;
-    [time, state] = fold(f)([time, state])(Object.entries(moduleMap))
-    state = log(`total ${time} ms`)(state);
+    /** @type {TestState} */
+    let ts = [0, 0];
+    [ts, state] = fold(f)([ts, state])(Object.entries(moduleMap))
+    state = log(`${bold}Number of tests: ${ts[1]}\nTime: ${ts[0]} ms${reset}`)(state);
     return state
 }
 
