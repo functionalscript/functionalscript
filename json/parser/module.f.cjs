8e6cc2c2b41da2d4cc63489668c46754d0e0d991
Shandar Denys <msden.ua@gmail.com>
2023-01-22 22:33:40 +0200

    parsing

--- json/parser/module.f.cjs
+++ json/parser/module.f.cjs
@@ -1,14 +1,63 @@
 const result = require('../../types/result/module.f.cjs')
 const list = require('../../types/list/module.f.cjs')
-const { fold } = list
+const { fold, isEmpty } = list
 const operator = require('../../types/function/operator/module.f.cjs')
 const tokenizer = require('../tokenizer/module.f.cjs')
 const map = require('../../types/map/module.f.cjs')
 const { todo } = require('../../dev/module.f.cjs')
 
-/** @typedef {map.Map<any>} JsonObject */
 
-/** @typedef {list.List<any>} JsonArray */
+/**
+ * @typedef {{
+* readonly kind: 'object'
+* readonly value: map.Map<any>
+* }} JsonObject
+* */
+
+/**
+ * @typedef {{
+* readonly kind: 'array'
+* readonly value: list.List<any>
+* }} JsonArray
+* */
+
+/**
+ * @typedef {{
+* readonly kind: 'number'
+* readonly value: number
+* }} JsonNumber
+* */
+
+/**
+ * @typedef {{
+* readonly kind: 'string'
+* readonly value: string
+* }} JsonString
+* */
+
+/**
+ * @typedef {{
+* readonly kind: 'string'
+* readonly value: boolean
+* }} JsonBoolean
+* */
+
+/**
+ * @typedef {{
+* readonly kind: 'null'
+* }} JsonNull
+* */
+
+/**
+ * @typedef {|
+* JsonObject |
+* JsonArray |
+* JsonNumber |
+* JsonString |
+* JsonBoolean |
+* JsonNull 
+* } JsonValue
+*/
 
 /**
  * @typedef {|
@@ -21,20 +70,49 @@ const { todo } = require('../../dev/module.f.cjs')
 
 /**
  * @typedef {{
+*  readonly kind: 'start'
 *  readonly stack: JsonStack
-*  readonly state: string
-* }} JsonState
+* }} StateStart
+*/
+
+/**
+ * @typedef {{
+*  readonly kind: 'end'
+*  readonly value: any
+* }} StateEnd
+*/
+
+/**
+ * @typedef {|
+* StateStart |
+* StateEnd
+* } JsonState
 */
 
 /** @type {operator.Fold<tokenizer.JsonToken, JsonState>} */
 const foldOp = token => state => {
-    return todo()
+    switch(state.kind) {
+        case 'start': {
+            switch(token.kind) {
+                case 'null': return { kind: 'end', value: null }
+                case 'false': return { kind: 'end', value: false }
+                case 'true': return { kind: 'end', value: true }
+                case 'number': return { kind: 'end', value: Number(token.value) }
+                case 'string': return { kind: 'end', value: token.value }
+                default: return todo()
+            }
+        }
+        default: return todo()
+    }
 }
 
 /** @type {(tokens: list.List<tokenizer.JsonToken>) => result.Result<any, string>} */
 const parse = tokens => {
-    fold(foldOp)({stack: null, state: ""})(tokens)
-    return todo()
+    const state = fold(foldOp)({ kind: "start", stack: null })(tokens)
+    switch(state.kind) {
+        case 'end': return  result.ok(state.value)
+        default: return result.error("unexpected end")
+    }
 }
 
 module.exports = {
--- json/parser/test.f.cjs
+++ json/parser/test.f.cjs
@@ -1,4 +1,57 @@
+const parser = require('./module.f.cjs')
+const tokenizer = require('../tokenizer/module.f.cjs')
+const { toArray } = require('../../types/list/module.f.cjs')
+const json = require('../module.f.cjs')
+const { sort } = require('../../types/object/module.f.cjs')
+const encoding = require('../../text/utf16/module.f.cjs');
+
+/** @type {(s: string) => readonly tokenizer.JsonToken[]} */
+const tokenizeString = s => toArray(tokenizer.tokenize(encoding.stringToList(s)))
+
+const stringify = json.stringify(sort)
+
 module.exports = {
     testing: [
+        () => {
+            const tokens = tokenizeString('')
+            const result = parser.parse(tokens)
+            if (result[0] !== 'error') { throw result }
+        },
+        () => {
+            const tokens = tokenizeString('null')
+            const result = parser.parse(tokens)
+            if (result[0] !== 'ok') { throw result }
+            if (result[1] !== null) { throw result }
+        },
+        () => {
+            const tokens = tokenizeString('true')
+            const result = parser.parse(tokens)
+            if (result[0] !== 'ok') { throw result }
+            if (result[1] !== true) { throw result }
+        },
+        () => {
+            const tokens = tokenizeString('false')
+            const result = parser.parse(tokens)
+            if (result[0] !== 'ok') { throw result }
+            if (result[1] !== false) { throw result }
+        },
+        () => {
+            const tokens = tokenizeString('0.1')
+            const result = parser.parse(tokens)
+            if (result[0] !== 'ok') { throw result }
+            if (result[1] !== 0.1) { throw result }
+        },
+        () => {
+            const tokens = tokenizeString('1.1e+2')
+            const result = parser.parse(tokens)
+            if (result[0] !== 'ok') { throw result }
+            if (result[1] !== 110) { throw result }
+        },
+        () => {
+            const tokens = tokenizeString('"abc"')
+            const result = parser.parse(tokens)
+            if (result[0] !== 'ok') { throw result }
+            if (result[1] !== 'abc') { throw result }
+        },
     ]
 }
