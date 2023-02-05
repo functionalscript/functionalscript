50904e55b570aa23d96905fec1d7b2c284370032
Shandar Denys <msden.ua@gmail.com>
2023-02-05 18:29:13 +0200

    bigint abs

--- types/bigint/module.f.cjs
+++ types/bigint/module.f.cjs
@@ -1,3 +1,4 @@
+const { unsafeCmp } = require('../function/compare/module.f.cjs')
 const { reduce } = require('../list/module.f.cjs')
 
 /** @type {(a: bigint) => (b: bigint) => bigint} */
@@ -5,9 +6,19 @@ const addition = a => b => a + b
 
 const sum = reduce(addition)(0n)
 
+/** @type {(a: bigint) => bigint} */
+const abs = a => a >= 0 ? a : -a
+
+/** @type {(a: bigint) => bigint} */
+const sign = a => BigInt(unsafeCmp(a)(0n))
+
 module.exports = {
     /** @readonly */
     addition,
     /** @readonly */
     sum,
+    /** @readonly */
+    abs,
+    /** @readonly */
+    sign,
 }
