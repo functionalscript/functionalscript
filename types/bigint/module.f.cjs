233e8f3279ab22840aa4744dfb9f76d570c5e017
Trinidadec <msden.ua@gmail.com>
2023-02-12 22:08:42 +0200

    Decimal float format to binary float format (#213)
    * decimal exp to binary exp
    
    * simplify
    
    * bigint abs
    
    * fix negative values
    
    * bigint sign
    
    * rename type
    
    * add bigfloat type
    
    * bigfloat structure
    
    * remove unused const
    
    * parse json number
    
    * simplify bigfloat
    
    * revert tokenizer

--- types/bigint/module.f.cjs
+++ types/bigint/module.f.cjs
@@ -1,3 +1,5 @@
+const compare = require('../function/compare/module.f.cjs')
+const { unsafeCmp } = compare
 const { reduce } = require('../list/module.f.cjs')
 
 /** @type {(a: bigint) => (b: bigint) => bigint} */
@@ -5,9 +7,19 @@ const addition = a => b => a + b
 
 const sum = reduce(addition)(0n)
 
+/** @type {(a: bigint) => bigint} */
+const abs = a => a >= 0 ? a : -a
+
+/** @type {(a: bigint) => compare.Sign} */
+const sign = a => unsafeCmp(a)(0n)
+
 module.exports = {
     /** @readonly */
     addition,
     /** @readonly */
     sum,
+    /** @readonly */
+    abs,
+    /** @readonly */
+    sign,
 }
