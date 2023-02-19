5572bbe9654ce47e737beaf4a50ddf38de466ba1
Shandar Denys <msden.ua@gmail.com>
2023-02-19 20:20:17 +0200

    format result

--- types/bigfloat/test.f.cjs
+++ types/bigfloat/test.f.cjs
@@ -4,57 +4,57 @@ module.exports = {
     decToBin: [
         () => {
             const result = decToBin([0n, 0])
-            if (result[0] !== 0b0_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000n) { throw result[0] }
+            if (result[0] !== 0b0_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000n) { throw result[0].toString(2) }
             if (result[1] !== 0) { throw result[1] }
         },
         () => {
             const result = decToBin([0n, 10])
-            if (result[0] !== 0b0_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000n) { throw result[0] }
+            if (result[0] !== 0b0_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000n) { throw result[0].toString(2) }
             if (result[1] !== 0) { throw result[1] }
         },
         () => {
             const result = decToBin([0n, -10])
-            if (result[0] !== 0b0_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000n) { throw result[0] }
+            if (result[0] !== 0b0_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000n) { throw result[0].toString(2) }
             if (result[1] !== 0) { throw result[1] }
         },
         () => {
             const result = decToBin([1n, 0])
-            if (result[0] !== 0b1_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000n) { throw result[0] }
+            if (result[0] !== 0b1_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000n) { throw result[0].toString(2) }
             if (result[1] !== -52) { throw result[1] }
         },
         () => {
             const result = decToBin([1n, 1])
-            if (result[0] !== 0b1_0100_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000n) { throw result[0] }
+            if (result[0] !== 0b1_0100_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000n) { throw result[0].toString(2) }
             if (result[1] !== -49) { throw result[1] }
         },
         () => {
             const result = decToBin([1000n, -2])
-            if (result[0] !== 0b1_0100_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000n) { throw result[0] }
+            if (result[0] !== 0b1_0100_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000n) { throw result[0].toString(2) }
             if (result[1] !== -49) { throw result[1] }
         },
         () => {
             const result = decToBin([1n, -1])
-            if (result[0] !== 0b1_1001_1001_1001_1001_1001_1001_1001_1001_1001_1001_1001_1001_1010n) { throw result[0] } //+1
+            if (result[0] !== 0b1_1001_1001_1001_1001_1001_1001_1001_1001_1001_1001_1001_1001_1010n) { throw result[0].toString(2) } //+1
             if (result[1] !== -56) { throw result[1] }
         },
         () => {
             const result = decToBin([-1n, 0])
-            if (result[0] !== -0b1_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000n) { throw result[0] }
+            if (result[0] !== -0b1_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000n) { throw result[0].toString(2) }
             if (result[1] !== -52) { throw result[1] }
         },
         () => {
             const result = decToBin([-1n, 1])
-            if (result[0] !== -0b1_0100_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000n) { throw result[0] }
+            if (result[0] !== -0b1_0100_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000n) { throw result[0].toString(2) }
             if (result[1] !== -49) { throw result[1] }
         },
         () => {
             const result = decToBin([-1000n, -2])
-            if (result[0] !== -0b1_0100_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000n) { throw result[0] }
+            if (result[0] !== -0b1_0100_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000n) { throw result[0].toString(2) }
             if (result[1] !== -49) { throw result[1] }
         },
         () => {
             const result = decToBin([-1n, -1])
-            if (result[0] !== -0b1_1001_1001_1001_1001_1001_1001_1001_1001_1001_1001_1001_1001_1010n) { throw result[0] } //+1
+            if (result[0] !== -0b1_1001_1001_1001_1001_1001_1001_1001_1001_1001_1001_1001_1001_1010n) { throw result[0].toString(2) } //+1
             if (result[1] !== -56) { throw result[1] }
         }
     ]
