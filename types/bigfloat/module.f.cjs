147ebe700b431bc0c01a8d7a82d02ad98e7b1ead
Trinidadec <msden.ua@gmail.com>
2023-02-26 21:28:14 +0200

    Denis/bigfloat (#216)
    * bigfloat 53 digits
    
    * renaming
    
    * divide
    
    * format result
    
    * round to even
    
    * round to even
    
    * more test cases
    
    * rounding test cases
    
    * simplify
    
    * deno veriosn
    
    * deno version
    
    * deno version

--- types/bigfloat/module.f.cjs
+++ types/bigfloat/module.f.cjs
@@ -1,14 +1,16 @@
 const compare = require('../function/compare/module.f.cjs')
 const { abs, sign } = require('../bigint/module.f.cjs')
+const { todo } = require('../../dev/module.f.cjs')
 
 /** @typedef {readonly[bigint,number]} BigFloat */
 
-const minSignificand = 0b10_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000n
+const twoPow53 = 0b0010_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000n
+const twoPow54 = 0b0100_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000n
 
 /** @type {(value: BigFloat) => (min: bigint) => BigFloat} */
-const increaseMantissa = ([m,e]) => min => {
+const increaseMantissa = ([m, e]) => min => {
     if (m === 0n) {
-        return [m,e]
+        return [m, e]
     }
     const s = sign(m)
     m = abs(m)
@@ -21,10 +23,42 @@ const increaseMantissa = ([m,e]) => min => {
     }
 }
 
+/** @type {(value: BigFloat) => (max: bigint) => BigFloat} */
+const decreaseMantissa = ([m, e]) => max => {
+    if (m === 0n) {
+        return [m, e]
+    }
+    const s = sign(m)
+    m = abs(m)
+    while (true) {
+        if (m < max) {
+            return [BigInt(s) * m, e]
+        }
+        m = m >> 1n
+        e++
+    }
+}
+
 /** @type {(base: bigint) => (exp: number) => bigint} */
 const pow = base => exp => base ** BigInt(exp)
 
 const pow5 = pow(5n)
+
+/** @type {(div: BigFloat) => (p: bigint) => BigFloat} */
+const divide = ([m, e]) => div => {
+    const mabs = abs(m)
+    const q = mabs / div
+    const s = BigInt(sign(m))
+    const [q53, e53] = decreaseMantissa([q, e])(twoPow54)
+    const r = q53 & 1n
+    const q52 = q53 >> 1n
+    const e52 = e53 + 1
+    if (r === 1n && mabs === q * div && q === q53 >> BigInt(e - e53)) {
+        const odd = q52 & 1n
+        return [s * (q52 + odd), e52]
+    }
+    return [s * (q52 + r), e52]
+}
 
 /** @type {(dec: BigFloat) => BigFloat} */
 const decToBin = dec => {
@@ -34,11 +68,11 @@ const decToBin = dec => {
     if (dec[1] >= 0) {
         /** @type {BigFloat} */
         const bin = [dec[0] * pow5(dec[1]), dec[1]]
-        return increaseMantissa(bin)(minSignificand)
+        return divide(increaseMantissa(bin)(twoPow53))(1n)
     }
     const p = pow5(-dec[1])
-    const inc = increaseMantissa(dec)(p * minSignificand)
-    return [inc[0] / p, inc[1]]
+    const [m, e] = increaseMantissa(dec)(p * twoPow53)
+    return divide([m, e])(p)
 }
 
 module.exports = {
