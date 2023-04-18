a4d62083f5b652926a1f0dc59574f4f22c4bfda8
Shandar Denys <msden.ua@gmail.com>
2023-04-19 00:11:21 +0300

    array wrap test

--- json/serializer/test.f.cjs
+++ json/serializer/test.f.cjs
@@ -1,4 +1,15 @@
-const serializer = require('./module.f.cjs')
+const _ = require('./module.f.cjs')
+const { toArray } = require('../../types/list/module.f.cjs')
 
 module.exports = {
+    arrayWrap: [
+        () => {
+            const result = JSON.stringify(toArray(_.arrayWrap([['a','b']])))
+            if (result !== '["[","a","b","]"]') { throw result }
+        },
+        () => {
+            const result = JSON.stringify(toArray(_.arrayWrap([['a'],['b']])))
+            if (result !== '["[","a",",","b","]"]') { throw result }
+        }
+    ]
 }
