ecdece949e7a7583d7d082dc08be1dbdd483f991
Trinidadec <msden.ua@gmail.com>
2023-04-23 20:59:18 +0300

    Serializer Tests (#230)
    * array wrap test
    
    * object wrap test
    
    * string serialize test
    
    * number serialize test
    
    * bool serialize test
    
    * wrap empty

--- json/serializer/test.f.cjs
+++ json/serializer/test.f.cjs
@@ -1,4 +1,63 @@
-const serializer = require('./module.f.cjs')
+const _ = require('./module.f.cjs')
+const { toArray } = require('../../types/list/module.f.cjs')
 
 module.exports = {
+    arrayWrap: [
+        () => {
+            const result = JSON.stringify(toArray(_.arrayWrap(null)))
+            if (result !== '["[","]"]') { throw result }
+        },
+        () => {
+            const result = JSON.stringify(toArray(_.arrayWrap([['a','b']])))
+            if (result !== '["[","a","b","]"]') { throw result }
+        },
+        () => {
+            const result = JSON.stringify(toArray(_.arrayWrap([['a'],['b']])))
+            if (result !== '["[","a",",","b","]"]') { throw result }
+        }
+    ],
+    objectWrap: [
+        () => {
+            const result = JSON.stringify(toArray(_.objectWrap(null)))
+            if (result !== '["{","}"]') { throw result }
+        },
+        () => {
+            const result = JSON.stringify(toArray(_.objectWrap([['a','b']])))
+            if (result !== '["{","a","b","}"]') { throw result }
+        },
+        () => {
+            const result = JSON.stringify(toArray(_.objectWrap([['a'],['b']])))
+            if (result !== '["{","a",",","b","}"]') { throw result }
+        }
+    ],
+    stringSerialize: [
+        () => {
+            const result = JSON.stringify(toArray(_.stringSerialize('abc')))
+            if (result !== '["\\"abc\\""]') { throw result }
+        },
+        () => {
+            const result = JSON.stringify(toArray(_.stringSerialize('123')))
+            if (result !== '["\\"123\\""]') { throw result }
+        }
+    ],
+    numberSerialize: [
+        () => {
+            const result = JSON.stringify(toArray(_.numberSerialize(123)))
+            if (result !== '["123"]') { throw result }
+        },
+        () => {
+            const result = JSON.stringify(toArray(_.numberSerialize(10e20)))
+            if (result !== '["1e+21"]') { throw result }
+        }
+    ],
+    boolSerialize: [
+        () => {
+            const result = JSON.stringify(toArray(_.boolSerialize(false)))
+            if (result !== '["false"]') { throw result }
+        },
+        () => {
+            const result = JSON.stringify(toArray(_.boolSerialize(true)))
+            if (result !== '["true"]') { throw result }
+        }
+    ]
 }
