08be4cca216ff1f8a0bcc277a82045f79b0f64f0
Shandar Denys <msden.ua@gmail.com>
2023-07-01 20:33:38 +0300

    move tokenizer to root

--- json/tokenizer/module.f.cjs
+++ json/tokenizer/module.f.cjs
@@ -1,5 +1,5 @@
 const list = require('../../types/list/module.f.cjs')
-const fjsonTokenizer = require('../../fjson/tokenizer/module.f.cjs')
+const fjsonTokenizer = require('../../tokenizer/module.f.cjs')
 
 /**
  * @typedef {|
