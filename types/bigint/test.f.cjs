cbb8350ab715bc0891d13e5458957245d9780d56
sergey-shandar <sergey-shandar@users.noreply.github.com>
2024-11-25 04:32:03 -0800

    1_048_600

--- types/bigint/test.f.cjs
+++ types/bigint/test.f.cjs
@@ -71,8 +71,8 @@ module.exports = {
             if (result !== 4n) { throw result }
         },
         () => {
-            const result = log2(1n << 1_048_800n)
-            if (result !== 1_048_800n) { throw result }
+            const result = log2(1n << 1_048_600n)
+            if (result !== 1_048_600n) { throw result }
         }
     ]
 }
