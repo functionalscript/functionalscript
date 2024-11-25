8af3394b8a0d64c3d36dfb023acb98a80af0679d
sergey-shandar <sergey-shandar@users.noreply.github.com>
2024-11-25 03:19:00 -0800

    size

---
+++ types/bitvec/test.f.cjs
@@ -0,0 +1,34 @@
+const { size } = require('./module.f.cjs')
+
+module.exports = {
+    0: () => {
+        const s = size(0n)
+        if (s !== 0n) { throw s }
+    },
+    m: () => {
+        let i = 0n
+        while (i < 10_000n) {
+            const s = size(1n << i)
+            if (s !== i + 1n) { throw [s, i] }
+            i += 1n
+        }
+    },
+    neg: [
+        () => {
+            const s = size(-1n)
+            if (s !== 1n) { throw s }
+        },
+        () => {
+            const s = size(-2n)
+            if (s !== 2n) { throw s }
+        },
+        () => {
+            const s = size(-3n)
+            if (s !== 2n) { throw s }
+        },
+        () => {
+            const s = size(-4n)
+            if (s !== 3n) { throw s }
+        },
+    ]
+}
