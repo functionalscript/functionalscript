4d7ac7858e4093410ca62dc637adaa2fb910cf39
sergey-shandar <sergey-shandar@users.noreply.github.com>
2024-11-29 00:47:33 -0800

    export default

---
+++ index.f.mjs
@@ -0,0 +1,73 @@
+// Generated file.
+import com from './commodule.f.cjs'
+import commonjs from './commonjsmodule.f.cjs'
+import dev from './devmodule.f.cjs'
+import djs from './djsmodule.f.cjs'
+import fsc from './fscmodule.f.cjs'
+import fsm from './fsmmodule.f.cjs'
+import html from './htmlmodule.f.cjs'
+import js$tokenizer from './js/tokenizermodule.f.cjs'
+import json from './jsonmodule.f.cjs'
+import nodejs from './nodejsmodule.f.cjs'
+import prime_field from './prime_fieldmodule.f.cjs'
+import secp from './secpmodule.f.cjs'
+import sha2 from './sha2module.f.cjs'
+import text from './textmodule.f.cjs'
+import types$array from './types/arraymodule.f.cjs'
+import types$bigfloat from './types/bigfloatmodule.f.cjs'
+import types$bigint from './types/bigintmodule.f.cjs'
+import types$btree from './types/btreemodule.f.cjs'
+import types$byte_set from './types/byte_setmodule.f.cjs'
+import types$function from './types/functionmodule.f.cjs'
+import types$list from './types/listmodule.f.cjs'
+import types$map from './types/mapmodule.f.cjs'
+import types$nibble_set from './types/nibble_setmodule.f.cjs'
+import types$nullable from './types/nullablemodule.f.cjs'
+import types$number from './types/numbermodule.f.cjs'
+import types$object from './types/objectmodule.f.cjs'
+import types$range from './types/rangemodule.f.cjs'
+import types$range_map from './types/range_mapmodule.f.cjs'
+import types$result from './types/resultmodule.f.cjs'
+import types$sorted_list from './types/sorted_listmodule.f.cjs'
+import types$sorted_set from './types/sorted_setmodule.f.cjs'
+import types$string from './types/stringmodule.f.cjs'
+import types$string_set from './types/string_setmodule.f.cjs'
+export default {
+  com: com,
+  commonjs: commonjs,
+  dev: dev,
+  djs: djs,
+  fsc: fsc,
+  fsm: fsm,
+  html: html,
+  js: {
+    tokenizer: js$tokenizer,
+  },
+  json: json,
+  nodejs: nodejs,
+  prime_field: prime_field,
+  secp: secp,
+  sha2: sha2,
+  text: text,
+  types: {
+    array: types$array,
+    bigfloat: types$bigfloat,
+    bigint: types$bigint,
+    btree: types$btree,
+    byte_set: types$byte_set,
+    function: types$function,
+    list: types$list,
+    map: types$map,
+    nibble_set: types$nibble_set,
+    nullable: types$nullable,
+    number: types$number,
+    object: types$object,
+    range: types$range,
+    range_map: types$range_map,
+    result: types$result,
+    sorted_list: types$sorted_list,
+    sorted_set: types$sorted_set,
+    string: types$string,
+    string_set: types$string_set,
+  },
+}
