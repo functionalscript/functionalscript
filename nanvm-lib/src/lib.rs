fb45c3fdaef623d95a380bf7a61e498703b7607b
sergey-shandar <sergey-shandar@users.noreply.github.com>
2024-12-04 08:51:30 -0800

    lib

--- nanvm-lib/src/lib.rs
+++ nanvm-lib/src/lib.rs
@@ -6,11 +6,13 @@ pub mod interface {
     }
 
     pub trait VarSize: Sized {
+        type Id: PartialEq;
         type Header;
         type Item;
         fn new(h: Self::Header, c: impl IntoIterator<Item = Self::Item>) -> Option<Self>;
         fn header(&self) -> &Self::Header;
         fn items(&self) -> &[Self::Item];
+        fn id(&self) -> Self::Id;
     }
 
     pub trait String: VarSize<Header = (), Item = u16> {}
@@ -29,10 +31,11 @@ pub mod interface {
 
     pub trait Any {
         type String: String;
-        type Object: Object;
-        type Array: Array;
+        type Object: Object<Id = Self::Id>;
+        type Array: Array<Id = Self::Id>;
         type BitInt: BigInt;
         type Function: Function;
+        type Id;
     }
 }
 
@@ -50,6 +53,7 @@ pub mod naive {
     impl<H, T> interface::VarSize for Rc<H, T> {
         type Header = H;
         type Item = T;
+        type Id = *const ();
         fn new(header: H, s: impl IntoIterator<Item = T>) -> Option<Self> {
             Some(Self {
                 header,
@@ -62,6 +66,9 @@ pub mod naive {
         fn items(&self) -> &[Self::Item] {
             &self.items
         }
+        fn id(&self) -> Self::Id {
+            self.items.as_ptr() as Self::Id
+        }
     }
 
     pub type String = Rc<(), u16>;
@@ -70,7 +77,7 @@ pub mod naive {
 
     type Object = Rc<(), (String, Any)>;
 
-    type Array = Rc<(), Any>;
+    pub type Array = Rc<(), Any>;
 
     type Function = Rc<u32, u8>;
 
@@ -94,5 +101,11 @@ mod test {
     fn test() {
         let s = naive::String::new((), b"Hello".map(|v| v as u16));
         let bi = naive::BigInt::new(Sign::Positive, []);
+        {
+            let a = naive::Array::new((), []).unwrap();
+            let b  = naive::Array::new((), []).unwrap();
+            // two empty arrays are not equal
+            assert_ne!(a.id(), b.id());
+        }
     }
 }
