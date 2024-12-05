f1b50570d1a71b63f351828854a80f252feef44d
sergey-shandar <sergey-shandar@users.noreply.github.com>
2024-12-04 20:45:04 -0800

    Into

--- nanvm-lib/src/lib.rs
+++ nanvm-lib/src/lib.rs
@@ -6,7 +6,7 @@ pub mod interface {
         Negative = -1,
     }
 
-    pub trait Instance<A>: Sized + PartialEq {
+    pub trait Instance: Sized + PartialEq {
         type Header;
         type Item;
         fn new(h: Self::Header, c: impl IntoIterator<Item = Self::Item>) -> Option<Self>;
@@ -14,15 +14,15 @@ pub mod interface {
         fn items(&self) -> &[Self::Item];
     }
 
-    pub trait String<A>: Instance<A, Header = (), Item = u16> {}
+    pub trait String<A>: Instance<Header = (), Item = u16> + Into<A> {}
 
-    pub trait BigInt<A>: Instance<A, Header = Sign, Item = u64> {}
+    pub trait BigInt<A>: Instance<Header = Sign, Item = u64> + Into<A> {}
 
-    pub trait Object<A: Any>: Instance<A, Header = (), Item = (A::String, A)> {}
+    pub trait Object<A: Any>: Instance<Header = (), Item = (A::String, A)> + Into<A> {}
 
-    pub trait Array<A>: Instance<A, Header = (), Item = A> {}
+    pub trait Array<A>: Instance<Header = (), Item = A> + Into<A> {}
 
-    pub trait Function<A>: Instance<A, Header = u32, Item = u8> {}
+    pub trait Function<A>: Instance<Header = u32, Item = u8> + Into<A> {}
 
     pub trait Any: PartialEq + Sized {
         type String: String<Self>;
@@ -66,7 +66,7 @@ pub mod naive {
         }
     }
 
-    impl<P: Policy> interface::Instance<Any> for Instance<P> {
+    impl<P: Policy> interface::Instance for Instance<P> {
         type Header = P::Header;
         type Item = P::Item;
         fn new(header: Self::Header, items: impl IntoIterator<Item = Self::Item>) -> Option<Self> {
@@ -103,25 +103,67 @@ pub mod naive {
         }
     }
 
+    // String
+
     pub type String = Instance<ValuePolicy<(), u16>>;
+
+    impl Into<Any> for String {
+        fn into(self) -> Any {
+            Any::String(self)
+        }
+    }
 
     impl interface::String<Any> for String {}
 
+    // BigInt
+
     pub type BigInt = Instance<ValuePolicy<Sign, u64>>;
+
+    impl Into<Any> for BigInt {
+        fn into(self) -> Any {
+            Any::BigInt(self)
+        }
+    }
 
     impl interface::BigInt<Any> for BigInt {}
 
+    // Object
+
     pub type Object = Instance<RefPolicy<(), (String, Any)>>;
+
+    impl Into<Any> for Object {
+        fn into(self) -> Any {
+            Any::Object(self)
+        }
+    }
 
     impl interface::Object<Any> for Object {}
 
+    // Array
+
     pub type Array = Instance<RefPolicy<(), Any>>;
+
+    impl Into<Any> for Array {
+        fn into(self) -> Any {
+            Any::Array(self)
+        }
+    }
 
     impl interface::Array<Any> for Array {}
 
+    // Function
+
     pub type Function = Instance<RefPolicy<u32, u8>>;
 
+    impl Into<Any> for Function {
+        fn into(self) -> Any {
+            Any::Function(self)
+        }
+    }
+
     impl interface::Function<Any> for Function {}
+
+    // Any
 
     #[derive(Debug, PartialEq)]
     pub enum Any {
