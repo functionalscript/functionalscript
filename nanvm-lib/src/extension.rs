66820cd45b0b0083e78fa2dfa46e86961b26c876
sergey-shandar <sergey-shandar@users.noreply.github.com>
2024-12-05 20:49:13 -0800

    extension

--- nanvm-lib/src/extension.rs
+++ nanvm-lib/src/extension.rs
@@ -23,14 +23,23 @@ pub trait AnyExtension: Any {
             Unpacked::Function(_) => todo!(),
         }
     }
-    fn own_property(self, _i: &Self) -> Self {
+    fn own_property(self, i: Self) -> Self {
         match self.unpack() {
             Unpacked::Nullish(_) => panic!("own_property(\"nullish\")"),
             Unpacked::Bool(_) => Nullish::Undefined.into(),
             Unpacked::Number(_) => todo!(),
             Unpacked::String(_) => todo!(),
             Unpacked::BigInt(_) => todo!(),
-            Unpacked::Array(_) => todo!(),
+            Unpacked::Array(v) => match i.unpack() {
+                Unpacked::Nullish(_) => todo!(),
+                Unpacked::Bool(_) => todo!(),
+                Unpacked::Number(_) => todo!(),
+                Unpacked::String(_) => todo!(),
+                Unpacked::BigInt(_) => todo!(),
+                Unpacked::Array(_) => todo!(),
+                Unpacked::Object(_) => todo!(),
+                Unpacked::Function(_) => todo!(),
+            },
             Unpacked::Object(_) => todo!(),
             Unpacked::Function(_) => todo!(),
         }
@@ -100,14 +109,14 @@ mod test {
         #[should_panic]
         fn test_own_property_null() {
             let x: Any = Nullish::Null.into();
-            x.own_property(&string::<Any>("hello").into());
+            x.own_property(string::<Any>("hello").into());
         }
 
         #[test]
         fn test_own_property_bool() {
             let x: Any = true.into();
             let undefined: Any = Nullish::Undefined.into();
-            assert_eq!(undefined, x.own_property(&string::<Any>("hello").into()));
+            assert_eq!(undefined, x.own_property(string::<Any>("hello").into()));
         }
     }
 }
