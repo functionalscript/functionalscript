7307f23bca436181b087f5e1a698dba1555cc9c0
sergey-shandar <sergey-shandar@users.noreply.github.com>
2024-12-06 15:38:21 -0800

    i2

--- nanvm-lib/src/interface2.rs
+++ nanvm-lib/src/interface2.rs
@@ -1,26 +1,42 @@
-use crate::nullish::Nullish;
+use crate::{nullish::Nullish, sign::Sign};
 
 trait SimpleUnknown<T> {
     fn new(value: T) -> Self;
     fn try_to(&self) -> Option<T>;
 }
 
-trait Complex<U: Unknown>: Sized {
+trait Collection: IntoIterator {}
+
+trait Complex<U: Unknown>: PartialEq + Sized {
+    type Header;
+    type Item;
+    type Collection: Collection<Item = Self::Item>;
     fn to_unknown(self) -> U;
     fn try_from_unknown(u: U) -> Option<Self>;
+    fn header(&self) -> Self::Header;
+    fn data(&self) -> Self::Collection;
+    fn new(header: Self::Header, items: impl IntoIterator<Item = Self::Item>) -> Self;
 }
 
-trait String16<U: Unknown<String16 = Self>>: Complex<U> {}
+trait List<U: Unknown>: Complex<U, Header = ()> {
+    fn new(items: impl IntoIterator<Item = Self::Item>) -> Self {
+        <Self as Complex<U>>::new((), items)
+    }
+}
 
-trait BigInt<U: Unknown<BigInt = Self>>: Complex<U> {}
+trait String16<U: Unknown<String16 = Self>>: List<U, Item = u16> {}
 
-trait Array<U: Unknown<Array = Self>>: Complex<U> {}
+trait BigInt<U: Unknown<BigInt = Self>>: Complex<U, Header = Sign, Item = u64> {}
 
-trait Object<U: Unknown<Object = Self>>: Complex<U> {}
+trait Array<U: Unknown<Array = Self>>: List<U, Item = U> {}
 
-trait Function<U: Unknown<Function = Self>>: Complex<U> {}
+trait Object<U: Unknown<Object = Self>>: List<U, Item = (U::String16, U)> {}
 
-trait Unknown: Sized + SimpleUnknown<Nullish> + SimpleUnknown<bool> + SimpleUnknown<f64> {
+trait Function<U: Unknown<Function = Self>>: Complex<U, Header = u32, Item = u8> {}
+
+trait Unknown:
+    PartialEq + Sized + SimpleUnknown<Nullish> + SimpleUnknown<bool> + SimpleUnknown<f64>
+{
     type String16: String16<Self>;
     type BigInt: BigInt<Self>;
     type Array: Array<Self>;
