1e0328ae65541dae7698f3d4373581afed6cb785
sergey-shandar <sergey-shandar@users.noreply.github.com>
2024-12-06 17:26:54 -0800

    interface2

--- nanvm-lib/src/interface2.rs
+++ nanvm-lib/src/interface2.rs
@@ -5,34 +5,34 @@ trait SimpleUnknown<T> {
     fn try_to(&self) -> Option<T>;
 }
 
-trait Collection: IntoIterator {}
-
-trait Complex<U: Unknown>: PartialEq + Sized {
-    type Header;
+trait Collection<U: Unknown>: PartialEq + Sized {
     type Item;
-    type Collection: Collection<Item = Self::Item>;
     fn to_unknown(self) -> U;
     fn try_from_unknown(u: U) -> Result<Self, U>;
+    /// For now, we use a slice.
+    /// We may change it in the future to support more sophisticated containers.
+    fn items(&self) -> &[Self::Item];
+}
+
+trait NoHeader<U: Unknown>: Collection<U> {
+    fn new(items: impl IntoIterator<Item = Self::Item>) -> Self;
+}
+
+trait WithHeader<U: Unknown>: Collection<U> {
+    type Header;
     fn header(&self) -> Self::Header;
-    fn data(&self) -> &Self::Collection;
     fn new(header: Self::Header, items: impl IntoIterator<Item = Self::Item>) -> Self;
 }
 
-trait List<U: Unknown>: Complex<U, Header = ()> {
-    fn new(items: impl IntoIterator<Item = Self::Item>) -> Self {
-        <Self as Complex<U>>::new((), items)
-    }
-}
+trait String16<U: Unknown<String16 = Self>>: NoHeader<U, Item = u16> {}
 
-trait String16<U: Unknown<String16 = Self>>: List<U, Item = u16> {}
+trait BigInt<U: Unknown<BigInt = Self>>: WithHeader<U, Header = Sign, Item = u64> {}
 
-trait BigInt<U: Unknown<BigInt = Self>>: Complex<U, Header = Sign, Item = u64> {}
+trait Array<U: Unknown<Array = Self>>: NoHeader<U, Item = U> {}
 
-trait Array<U: Unknown<Array = Self>>: List<U, Item = U> {}
+trait Object<U: Unknown<Object = Self>>: NoHeader<U, Item = (U::String16, U)> {}
 
-trait Object<U: Unknown<Object = Self>>: List<U, Item = (U::String16, U)> {}
-
-trait Function<U: Unknown<Function = Self>>: Complex<U, Header = u32, Item = u8> {}
+trait Function<U: Unknown<Function = Self>>: WithHeader<U, Header = u32, Item = u8> {}
 
 trait Unknown:
     PartialEq + Sized + SimpleUnknown<Nullish> + SimpleUnknown<bool> + SimpleUnknown<f64>
@@ -43,7 +43,15 @@ trait Unknown:
     type Object: Object<Self>;
     type Function: Function<Self>;
 
-    fn try_to<C: Complex<Self>>(self) -> Result<C, Self> {
+    fn try_to<C: Collection<Self>>(self) -> Result<C, Self> {
         C::try_from_unknown(self)
     }
 }
+
+trait ToUnknown: Sized {
+    fn to_unknown<U: SimpleUnknown<Self>>(self) -> U {
+        U::new(self)
+    }
+}
+
+impl<T> ToUnknown for T {}
