9b0d8b773f4c5ec979813b81526b5bda10ecdca5
sergey-shandar <sergey-shandar@users.noreply.github.com>
2024-12-14 01:10:49 -0800

    bit vec

--- types/bit_vec/module.f.mjs
+++ types/bit_vec/module.f.mjs
@@ -2,27 +2,50 @@
 import { log2 } from '../bigint/module.f.mjs'
 
 /**
+ * A vector of bits represented as a `bigint`.
+ *
  * @typedef {bigint} Vec
  */
 
+/**
+ * An empty vector of bits.
+ */
 export const empty = 1n
 
+/**
+ * Calculates the length of the given vector of bits.
+ */
 export const len = log2
 
 /**
  * Extract an unsigned integer of the given `uintLen` from the given vector.
  *
  * @type {(uintLen: bigint) => (v: Vec) => bigint}
+ *
+ * @example
+ *
+ * ```js
+ * const vector = 0b110101n;
+ * const extract3Bits = uint(3n);
+ * const result = extract3Bits(vector); // result is 0b101n (5n)
+ * ```
  */
-export const uint = vecLen => {
-    const mask = (1n << vecLen) - 1n
+export const uint = uintLen => {
+    const mask = (1n << uintLen) - 1n
     return data => data & mask
 }
 
 /**
- * The function creates a vector of bits of the given `vecLen` from the given unsigned integer.
+ * Creates a vector of bits of the given `vecLen` from the given unsigned integer.
  *
  * @type {(vecLen: bigint) => (ui: bigint) => Vec}
+ *
+ * @example
+ *
+ * ```js
+ * const createVector = vec(4n);
+ * const vector = createVector(5n); // vector is 0b10101n
+ * ```
  */
 export const vec = vecLen => {
     const stop = 1n << vecLen
@@ -31,7 +54,18 @@ export const vec = vecLen => {
 }
 
 /**
+ * Appends the vector `b` to the back of the vector `a`.
+ *
  * @type {(a: Vec) => (b: Vec) => Vec}
+ *
+ * @example
+ *
+ * ```js
+ * const vec8 = vec(8n)
+ * const a = vec8(0x345n)
+ * const b = vec8(0x789n)
+ * const ab = appendBack(a)(b) // 0x18945n
+ * ```
  */
 export const appendBack = a => {
     const aLen = len(a)
@@ -39,7 +73,16 @@ export const appendBack = a => {
 }
 
 /**
+ * Removes the first `len` bits from the given vector.
+ *
  * @type {(len: bigint) => (v: Vec) => Vec}
+ *
+ * @example
+ *
+ * ```js
+ * const v = vec(17n)(0x12345n) // v = 0x32345n
+ * const r = removeFront(9n)(v) // r = 0x191n
+ * ```
  */
 export const removeFront = len => v => {
     const r = v >> len
--- types/bit_vec/test.f.mjs
+++ types/bit_vec/test.f.mjs
@@ -1,6 +1,17 @@
 import { empty, len, appendBack, vec, uint, removeFront } from './module.f.mjs'
 
 export default {
+    uintExample: () => {
+        const vector = 0b110101n;
+        const extract3Bits = uint(3n);
+        const result = extract3Bits(vector); // result is 0b101n (5n)
+        if (result !== 0b101n) { throw result }
+    },
+    vecExample: () => {
+        const createVector = vec(4n);
+        const vector = createVector(5n); // vector is 0b10101n
+        if (vector !== 0b10101n) { throw vector }
+    },
     length: () => {
         const i = len(empty)
         if (i !== 0n) { throw i}
