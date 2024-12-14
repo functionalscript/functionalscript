b137c5a5a04f12e72aad97a5e1811f1b5a3d2d14
sergey-shandar <sergey-shandar@users.noreply.github.com>
2024-12-14 00:57:52 -0800

    bit-vec

--- types/bit_vec/module.f.mjs
+++ types/bit_vec/module.f.mjs
@@ -1,34 +1,47 @@
-import { log2 } from '../bigint/module.f.d.mts'
+// @ts-self-types="./module.f.d.mts"
+import { log2 } from '../bigint/module.f.mjs'
 
 /**
- * @typedef {bigint} BitSet
+ * @typedef {bigint} Vec
  */
 
 export const empty = 1n
 
-export const length = log2
+export const len = log2
 
 /**
- * @type {(len: bigint) => (data: bigint) => bigint}
+ * Extract an unsigned integer of the given `uintLen` from the given vector.
+ *
+ * @type {(uintLen: bigint) => (v: Vec) => bigint}
  */
-export const uint = len => {
-    const mask = (1n << len) - 1n
+export const uint = vecLen => {
+    const mask = (1n << vecLen) - 1n
     return data => data & mask
 }
 
 /**
- * @type {(len: bigint) => (data: bigint) => bigint}
+ * The function creates a vector of bits of the given `vecLen` from the given unsigned integer.
+ *
+ * @type {(vecLen: bigint) => (ui: bigint) => Vec}
  */
-export const vec = len => {
-    const stop = 1n << len
+export const vec = vecLen => {
+    const stop = 1n << vecLen
     const mask = stop - 1n
     return data => (data & mask) | stop
 }
 
 /**
- * @type {(a: BitSet) => (b: BitSet) => BitSet}
+ * @type {(a: Vec) => (b: Vec) => Vec}
  */
-export const append = a => {
-    const aLength = length(a)
-    return b => a | (b >> aLength)
+export const appendBack = a => {
+    const aLen = len(a)
+    return b => a | (b << aLen)
+}
+
+/**
+ * @type {(len: bigint) => (v: Vec) => Vec}
+ */
+export const removeFront = len => v => {
+    const r = v >> len
+    return r === 0n ? empty : r
 }
