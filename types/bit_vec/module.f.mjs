e0ae0805d91d51ee8d6f802bd06e9927586ca3ad
sergey-shandar <sergey-shandar@users.noreply.github.com>
2024-12-19 14:38:14 -0800

    LSB/MSB

--- types/bit_vec/module.f.mjs
+++ types/bit_vec/module.f.mjs
@@ -1,5 +1,6 @@
 // @ts-self-types="./module.f.d.mts"
 import { log2 } from '../bigint/module.f.mjs'
+import { flip } from '../function/module.f.mjs'
 
 /**
  * A vector of bits represented as a `bigint`.
@@ -25,12 +26,130 @@ export const lenght = log2
  * @example
  *
  * ```js
- * const vec4 = vec(4n);
- * const vector = vec4(5n); // vector is 0b10101n
+ * const u4 = vec(4n);
+ * const vector = u4(5n); // vector is 0x15n
+ * ```
+ *
+ * ```js
+ * const u4 = vec(4n);
+ * const vector = u4(0x5FEn); // vector is 0x1En
  * ```
  */
 export const vec = vecLen => {
+    if (vecLen <= 0n) { return () => empty }
     const stop = 1n << vecLen
     const mask = stop - 1n
-    return data => (data & mask) | stop
+    return data => stop | (data & mask)
 }
+
+/** @type {(len: bigint) => bigint} */
+const mask = len => (1n << len) - 1n
+
+/**
+ * Extract the least significant unsigned integer of the given `len` from the given vector.
+ *
+ * @type {(uintLen: bigint) => (v: Vec) => bigint}
+ *
+ * @example
+ *
+ * ```js
+ * const vector = vec(8n)(0xF5) // 0x1F5n
+ * const result = uintLsb(4n)(vector); // result is 5n
+ * const result2 = uintLsb(16n)(vector); // result2 is 0xF5n
+ * ```
+ */
+export const uintLsb = len => {
+    const m = mask(len)
+    return v => {
+        const result = v & m
+        return result === v ? result ^ (1n << lenght(v)) : result
+    }
+}
+
+/**
+ * Extract the most significant unsigned integer of the given `len` from the given vector.
+ *
+ * @type {(uintLen: bigint) => (v: Vec) => bigint}
+ *
+ * @example
+ *
+ * ```js
+ * const vector = vec(8n)(0xF5) // 0x1F5n
+ * const result = uintMsb(4n)(vector); // result is 0xFn
+ * const result2 = uintMsb(16n)(vector); // result2 is 0xF500n
+ */
+export const uintMsb = uintLen => {
+    const m = mask(uintLen)
+    return v => {
+        const vLen = lenght(v)
+        const d = vLen - uintLen
+        return (d >= 0n ? v >> d : v << -d) & m
+    }
+}
+
+/**
+ * Concat the given vectors of bits. The first vector is the least significant.
+ *
+ * @type {(a: Vec) => (b: Vec) => Vec}
+ *
+ * @example
+ *
+ * ```js
+ * const u8 = vec(8n)
+ * const a = u8(0x45n) // 0x145n
+ * const b = u8(0x89n) // 0x189n
+ * const ab = concatLsb(a)(b) // 0x18945n
+ * ```
+ */
+export const concatLsb = a => {
+    const aLen = lenght(a)
+    return b => (b << aLen) | a
+}
+
+/**
+ * Concat the given vectors of bits. The first vector is the most significant.
+ *
+ * @type {(b: Vec) => (a: Vec) => Vec}
+ *
+ * @example
+ *
+ * ```js
+ * const u8 = vec(8n)
+ * const a = u8(0x45n) // 0x145n
+ * const b = u8(0x89n) // 0x189n
+ * const ab = concatMsb(a)(b) // 0x14589n
+ * ```
+ */
+export const concatMsb = flip(concatLsb)
+
+/**
+ * Removes the first `len` least significant bits from the given vector.
+ *
+ * @type {(len: bigint) => (v: Vec) => Vec}
+ *
+ * @example
+ *
+ * ```js
+ * const v = vec(16n)(0x3456n) // 0x13456n
+ * const r = removeLsb(4n)(v) // 0x1345n
+ * const r2 = removeLsb(24n)(v) // 0x1n
+ * ```
+ */
+export const removeLsb = len => v => {
+    const r = v >> len
+    return r === 0n ? empty : r
+}
+
+/**
+ * Removes the first `len` most significant bits from the given vector.
+ *
+ * @type {(len: bigint) => (v: Vec) => Vec}
+ *
+ * @example
+ *
+ * ```js
+ * const v = vec(16n)(0x3456n) // 0x13456n
+ * const r = removeMsb(4n)(v) // 0x1456n
+ * const r2 = removeMsb(24n)(v) // 0x1n
+ */
+export const removeMsb = len => v => vec(lenght(v) - len)(v)
--- types/bit_vec/test.f.mjs
+++ types/bit_vec/test.f.mjs
@@ -1,16 +1,20 @@
-import { empty, vec, lenght } from './module.f.mjs'
-import { appendBack, uint, removeFront } from './le/module.f.mjs'
+import { empty, vec, lenght, concatLsb, uintLsb, removeLsb } from './module.f.mjs'
 
 export default {
-    uintExample: () => {
-        const vector = 0b1110101n;
-        const extract3Bits = uint(3n);
-        const result = extract3Bits(vector); // result is 0b101n (5n)
+    uintLsb: () => {
+        const vector = 0b1110101n
+        const extract3Bits = uintLsb(3n)
+        const result = extract3Bits(vector) // result is 0b101n (5n)
         if (result !== 0b101n) { throw result }
     },
+    uintSmall: () => {
+        const vector = 0b11n
+        const extract3Bits = uintLsb(3n)(vector)
+        if (extract3Bits !== 0b1n) { throw extract3Bits }
+    },
     vecExample: () => {
-        const createVector = vec(4n);
-        const vector = createVector(5n); // vector is 0b10101n
+        const createVector = vec(4n)
+        const vector = createVector(5n) // vector is 0b10101n
         if (vector !== 0b10101n) { throw vector }
     },
     length: () => {
@@ -21,14 +25,14 @@ export default {
         const v = vec(8n)(0x5FEn)
         if (v !== 0x1FEn) { throw v }
         if (lenght(v) !== 8n) { throw 'len' }
-        const u = uint(8n)(v)
+        const u = uintLsb(8n)(v)
         if (u !== 0xFEn) { throw v }
     },
     appendBack: () => {
         const vec8 = vec(8n)
         const a = vec8(0x345n)
         const b = vec8(0x789n)
-        const ab = appendBack(a)(b)
+        const ab = concatLsb(a)(b)
         if (ab !== 0x18945n) { throw ab }
         const s = lenght(ab)
         if (s !== 16n) { throw `appendBack: ${s}` }
@@ -36,7 +40,7 @@ export default {
     removeBack: () => {
         const v = vec(17n)(0x12345n)
         if (v !== 0x32345n) { throw v.toString(16) }
-        const r = removeFront(9n)(v)
+        const r = removeLsb(9n)(v)
         if (r !== 0x191n) { throw r.toString(16) }
     }
 }
