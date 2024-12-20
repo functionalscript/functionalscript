e361a7268d820f1247993d2611c67ea2ab58b545
sergey-shandar <sergey-shandar@users.noreply.github.com>
2024-12-20 13:14:34 -0800

    test

--- djs/parser/test.f.ts
+++ djs/parser/test.f.ts
@@ -7,8 +7,9 @@ import * as o from '../../types/object/module.f.mjs'
 const { sort } = o
 import * as encoding from '../../text/utf16/module.f.mjs'
 
-/** @type {(s: string) => readonly tokenizer.DjsToken[]} */
-const tokenizeString = s => toArray(tokenizer.tokenize(encoding.stringToList(s)))
+const tokenizeString
+    : (s: string) => readonly tokenizer.DjsToken[]
+    = s => toArray(tokenizer.tokenize(encoding.stringToList(s)))
 
 const stringify = djs.stringify(sort)
 
