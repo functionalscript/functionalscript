3999f83096a1e23cc06b7bfeb1a947df5a4ab71c
sergey-shandar <sergey-shandar@users.noreply.github.com>
2024-12-20 15:59:25 -0800

    ok

--- types/nullable/test.f.ts
+++ types/nullable/test.f.ts
@@ -1,7 +1,7 @@
 import * as _ from './module.f.mjs'
 
 export default () => {
-    const optionSq = _.map(v => v * v)
+    const optionSq = _.map((v: number) => v * v)
     const sq3 = optionSq(3)
     if (sq3 !== 9) { throw sq3 }
     const sqNull = optionSq(null)
