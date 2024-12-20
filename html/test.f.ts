992284d0840beccf78c348562b65682ca0491f0a
sergey-shandar <sergey-shandar@users.noreply.github.com>
2024-12-20 13:20:39 -0800

    f

--- html/test.f.ts
+++ html/test.f.ts
@@ -14,20 +14,23 @@ export default {
         if (r !== '<!DOCTYPE html><area>') { throw r }
     },
     some: () => {
-        /** @type {_.Element} */
-        const x = ['div', {}, '<div>&amp;</div>', ['a', { href: 'hello"' }]]
+        const x
+            : _.Element
+            = ['div', {}, '<div>&amp;</div>', ['a', { href: 'hello"' }]]
         const s = _.htmlToString(x)
         if (s !== '<!DOCTYPE html><div>&lt;div&gt;&amp;amp;&lt;/div&gt;<a href="hello&quot;"></a></div>') { throw s }
     },
     some2: () => {
-        /** @type {_.Element} */
-        const x = ['div', '<div>&amp;</div>', ['a', { href: 'hello"' }]]
+        const x
+            : _.Element
+            = ['div', '<div>&amp;</div>', ['a', { href: 'hello"' }]]
         const s = _.htmlToString(x)
         if (s !== '<!DOCTYPE html><div>&lt;div&gt;&amp;amp;&lt;/div&gt;<a href="hello&quot;"></a></div>') { throw s }
     },
     someVoid: () => {
-        /** @type {_.Element} */
-        const x = ['div', ['br', {id: '5'}], '<div>&amp;</div>', ['a', { href: 'hello"' }]]
+        const x
+            : _.Element
+            = ['div', ['br', {id: '5'}], '<div>&amp;</div>', ['a', { href: 'hello"' }]]
         const s = _.htmlToString(x)
         if (s !== '<!DOCTYPE html><div><br id="5">&lt;div&gt;&amp;amp;&lt;/div&gt;<a href="hello&quot;"></a></div>') { throw s }
     }
