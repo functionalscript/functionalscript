fd190558f086721b40217a009d4046e8141e24fe
sergey-shandar <sergey-shandar@users.noreply.github.com>
2024-12-21 11:24:35 -0800

    x

--- types/nibble_set/test.f.ts
+++ types/nibble_set/test.f.ts
@@ -1,6 +1,6 @@
 import * as list from '../list/module.f.mjs'
 const { every, map, countdown } = list
-import * as _ from './module.f.mjs'
+import * as _ from './module.f.ts'
 
 export default {
     has: () => {
