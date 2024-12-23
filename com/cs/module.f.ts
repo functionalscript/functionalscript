bb7b70cc01a2016e66d2da520a1754e42c98aa10
sergey-shandar <sergey-shandar@users.noreply.github.com>
2024-12-23 09:35:14 -0800

    style

--- com/cs/module.f.ts
+++ com/cs/module.f.ts
@@ -5,12 +5,9 @@
  * with appropriate attributes for COM interop, such as `[StructLayout]`, `[Guid]`, and `[InterfaceType]`.
  */
 import { result, paramList, type BaseType, type Type, type Field, type FieldArray, type Definition, type Library } from '../types/module.f.ts'
-import * as text from '../../text/module.f.ts'
-const { curly } = text
-import * as list from '../../types/list/module.f.ts'
-const { flat, map, some, flatMap } = list
-import * as string from '../../types/string/module.f.ts'
-const { join } = string
+import { curly, type Block, type Item } from '../../text/module.f.ts'
+import { flat, map, some, flatMap, type List } from '../../types/list/module.f.ts'
+import { join } from '../../types/string/module.f.ts'
 import * as O from '../../types/object/module.f.ts'
 const { entries } = Object
 
@@ -19,16 +16,16 @@ const using
     = v => `using ${v};`
 
 const typeDef
-    :   (attributes: list.List<string>) =>
+    : (attributes: List<string>) =>
         (type: string) =>
         (name: string) =>
-        (body: text.Block) =>
-        list.List<text.Item>
+        (body: Block) =>
+        List<Item>
     = attributes => type => name => body =>
-    flat([
-        map(v => `[${v}]`)(attributes),
-        curly(`public ${type}`)(name)(body)
-    ])
+        flat([
+            map(v => `[${v}]`)(attributes),
+            curly(`public ${type}`)(name)(body)
+        ])
 
 const baseTypeMap = {
     bool: 'byte',
@@ -47,38 +44,38 @@ const baseTypeMap = {
 }
 
 const baseType
-: (t: BaseType) => string
-= t => baseTypeMap[t]
+    : (t: BaseType) => string
+    = t => baseTypeMap[t]
 
 const unsafe
-: (isUnsafe: boolean) => string
-= isUnsafe => isUnsafe ? 'unsafe ' : ''
+    : (isUnsafe: boolean) => string
+    = isUnsafe => isUnsafe ? 'unsafe ' : ''
 
 const fullType
-: (t: Type) => readonly[boolean, string]
-= t =>
-    typeof (t) === 'string' ?
-        [false, baseType(t)] :
-    t.length === 1 ?
-        [false, t[0]] :
-        [true, `${type(t[1])}*`]
+    : (t: Type) => readonly [boolean, string]
+    = t =>
+        typeof (t) === 'string' ?
+            [false, baseType(t)] :
+            t.length === 1 ?
+                [false, t[0]] :
+                [true, `${type(t[1])}*`]
 
 const type
-: (m: Type) => string
-= t => fullType(t)[1]
+    : (m: Type) => string
+    = t => fullType(t)[1]
 
 const param
-: (f: Field) => string
-= ([name, t]) => `${type(t)} ${name}`
+    : (f: Field) => string
+    = ([name, t]) => `${type(t)} ${name}`
 
 const mapParam = map(param)
 
 const field
-: (f: Field) => string
-= ([name, comType]) => {
-    const [isUnsafe, t] = fullType(comType)
-    return `public ${unsafe(isUnsafe)}${t} ${name};`
-}
+    : (f: Field) => string
+    = ([name, comType]) => {
+        const [isUnsafe, t] = fullType(comType)
+        return `public ${unsafe(isUnsafe)}${t} ${name};`
+    }
 
 const isUnsafeField
     : (field: Field) => boolean
@@ -91,17 +88,16 @@ const resultVoid = result('void')
 const joinComma = join(', ')
 
 const method
-: (e: O.Entry<FieldArray>) => readonly string[]
-= ([name, m]) => {
-
-    const paramAndResultList = entries(m)
-    const pl = paramList(m)
-    const isUnsafe = some(mapIsUnsafeField(paramAndResultList))
-    return [
-        '[PreserveSig]',
-        `${unsafe(isUnsafe)}${resultVoid(type)(m)} ${name}(${joinComma(mapParam(pl))});`
-    ]
-}
+    : (e: O.Entry<FieldArray>) => readonly string[]
+    = ([name, m]) => {
+        const paramAndResultList = entries(m)
+        const pl = paramList(m)
+        const isUnsafe = some(mapIsUnsafeField(paramAndResultList))
+        return [
+            '[PreserveSig]',
+            `${unsafe(isUnsafe)}${resultVoid(type)(m)} ${name}(${joinComma(mapParam(pl))});`
+        ]
+    }
 
 const struct = typeDef
     (['StructLayout(LayoutKind.Sequential)'])
@@ -112,22 +108,21 @@ const mapField = map(field)
 const flatMapMethod = flatMap(method)
 
 const def
-: (e: O.Entry<Definition>) => list.List<text.Item>
-= ([n, d]) =>
-    !('interface' in d) ?
-        struct(n)(mapField(entries(d.struct))) :
-        typeDef
-            ([`Guid("${d.guid}")`, 'InterfaceType(ComInterfaceType.InterfaceIsIUnknown)'])
-            ('interface')
-            (n)
-            (flatMapMethod(entries(d.interface)))
+    : (e: O.Entry<Definition>) => List<Item>
+    = ([n, d]) =>
+        !('interface' in d) ?
+            struct(n)(mapField(entries(d.struct))) :
+            typeDef
+                ([`Guid("${d.guid}")`, 'InterfaceType(ComInterfaceType.InterfaceIsIUnknown)'])
+                ('interface')
+                (n)
+                (flatMapMethod(entries(d.interface)))
 
 const flatMapDef = flatMap(def)
 
 const namespace = curly('namespace')
 
-/** @type {text.Block} */
-const header = [
+const header: Block = [
     using('System'),
     using('System.Runtime.InteropServices'),
     ''
@@ -137,9 +132,9 @@ const header = [
  * Generates the C# code for a library.
  */
 export const cs
-: (name: string) => (library: Library) => text.Block
-= name => library => {
-    const v = flatMapDef(entries(library))
-    const ns = namespace(name)(v)
-    return flat([header, ns])
-}
+    : (name: string) => (library: Library) => Block
+    = name => library => {
+        const v = flatMapDef(entries(library))
+        const ns = namespace(name)(v)
+        return flat([header, ns])
+    }
