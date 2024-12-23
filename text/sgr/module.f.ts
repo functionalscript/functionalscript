4cb9a6bd9b715ffb9fee0ede3917b3a3c2b18936
sergey-shandar <sergey-shandar@users.noreply.github.com>
2024-12-22 19:17:56 -0800

    0.3.4

--- text/sgr/module.f.ts
+++ text/sgr/module.f.ts
@@ -5,7 +5,7 @@ export const sgr
 : (c: number) => string
 = c => `\x1b[${c.toString()}m`
 
-export const reset = sgr(0)
-export const bold = sgr(1)
-export const fgRed = sgr(31)
-export const fgGreen = sgr(32)
+export const reset: string = sgr(0)
+export const bold: string = sgr(1)
+export const fgRed: string = sgr(31)
+export const fgGreen: string = sgr(32)
