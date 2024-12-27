a5aacc59581f6a3bbfcfc493c0ddf1671729c94b
sergey-shandar <sergey-shandar@users.noreply.github.com>
2024-12-27 10:50:44 -0800

    x

--- types/sorted_list/test.f.ts
+++ types/sorted_list/test.f.ts
@@ -1,4 +1,4 @@
-import * as _ from './module.f.ts'
+import { find, merge } from './module.f.ts'
 import { type Sign, unsafeCmp } from '../function/compare/module.f.ts'
 import * as json from '../../json/module.f.ts'
 import { sort } from '../object/module.f.ts'
@@ -14,36 +14,36 @@ const reverseCmp: <T>(a: T) => (b: T) => Sign
 export default {
     sortedMergre: [
         () => {
-            const result = stringify(toArray(_.merge(unsafeCmp)([2, 3, 4])([1, 3, 5])))
+            const result = stringify(toArray(merge(unsafeCmp)([2, 3, 4])([1, 3, 5])))
             if (result !== '[1,2,3,4,5]') { throw result }
         },
         () => {
-            const result = stringify(toArray(_.merge(unsafeCmp)([1, 2, 3])([])))
+            const result = stringify(toArray(merge(unsafeCmp)([1, 2, 3])([])))
             if (result !== '[1,2,3]') { throw result }
         },
         () => {
             const n = 10_000
             const list = countdown(n)
-            const result = _.merge(reverseCmp)(list)(list)
+            const result = merge(reverseCmp)(list)(list)
             const len = length(result)
             if (len != n) { throw result }
         }
     ],
     find: [
         () => {
-            const result = _.find(unsafeCmp)(0)([0, 10, 20, 30, 40, 50, 60, 70, 80, 90])
+            const result = find(unsafeCmp)(0)([0, 10, 20, 30, 40, 50, 60, 70, 80, 90])
             if (result !== 0) { throw result }
         },
         () => {
-            const result = _.find(unsafeCmp)(3)([0, 10, 20, 30, 40, 50, 60, 70, 80, 90])
+            const result = find(unsafeCmp)(3)([0, 10, 20, 30, 40, 50, 60, 70, 80, 90])
             if (result !== null) { throw result }
         },
         () => {
-            const result = _.find(unsafeCmp)(77)([0, 10, 20, 30, 40, 50, 60, 70, 80, 90])
+            const result = find(unsafeCmp)(77)([0, 10, 20, 30, 40, 50, 60, 70, 80, 90])
             if (result !== null) { throw result }
         },
         () => {
-            const result = _.find(unsafeCmp)(80)([0, 10, 20, 30, 40, 50, 60, 70, 80, 90])
+            const result = find(unsafeCmp)(80)([0, 10, 20, 30, 40, 50, 60, 70, 80, 90])
             if (result !== 80) { throw result }
         }
     ]
