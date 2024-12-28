3b301a66987a25e326483f7cf2a80c8a6ca3e861
sergey-shandar <sergey-shandar@users.noreply.github.com>
2024-12-27 16:20:09 -0800

    mjs

---
+++ types/bigint/benchmark.mjs
@@ -0,0 +1,86 @@
+const log2 = v => {
+    if (v <= 0n) { return -1n }
+    let result = 31n
+    let i = 32n
+    while (true) {
+        const n = v >> i
+        if (n === 0n) {
+            // overshot
+            break
+        }
+        v = n
+        result += i
+        i <<= 1n
+    }
+    // We know that `v` is not 0 so it doesn't make sense to check `n` when `i` is 0.
+    // Because of this, We check if `i` is greater than 32 before we divide it by 2.
+    while (i !== 32n) {
+        i >>= 1n
+        const n = v >> i
+        if (n !== 0n) {
+            result += i
+            v = n
+        }
+    }
+    return result - BigInt(Math.clz32(Number(v)))
+}
+
+const oldLog2 = v => {
+    if (v <= 0n) { return -1n }
+    let result = 0n
+    let i = 1n
+    while (true) {
+        const n = v >> i
+        if (n === 0n) {
+            // overshot
+            break
+        }
+        v = n
+        result += i
+        i <<= 1n // multiple by two
+    }
+    // We know that `v` is not 0 so it doesn't make sense to check `n` when `i` is 0.
+    // Because of this, We check if `i` is greater than 1 before we divide it by 2.
+    while (i !== 1n) {
+        i >>= 1n
+        const n = v >> i
+        if (n !== 0n) {
+            result += i
+            v = n
+        }
+    }
+    return result
+}
+
+const stringLog2 = v => BigInt(v.toString(2).length) - 1n
+
+const stringHexLog2 = v => {
+    const len = (BigInt(v.toString(16).length) - 1n) << 2n
+    const x = v >> len
+    return len + 31n - BigInt(Math.clz32(Number(x)))
+}
+
+const log = document.getElementById('log')
+
+const benchmark = (s, f) => {
+    const start = performance.now()
+    //
+    let e = 1_048_575n
+    let c = 1n << e
+    for (let i = 0n; i < 1_000; ++i) {
+        const x = f(c)
+        if (x !== e) { throw x }
+        c >>= 1n
+        --e
+    }
+    //
+    const end = performance.now()
+    const dif = end - start
+    //
+    log.innerText += `${s}: ${dif}\n`
+}
+
+benchmark('stringLog2', stringLog2)
+benchmark('stringHexLog2', stringHexLog2)
+benchmark('oldLog2', oldLog2)
+benchmark('log2', log2)
