9e7aea8a2caa06fff7d99bfd1247366bd98f89a5
Shandar Denys <msden.ua@gmail.com>
2024-12-29 23:21:41 +0200

    concat tests

--- path/test.f.ts
+++ path/test.f.ts
@@ -1,4 +1,4 @@
-import { normalize } from "./module.f.ts"
+import { concat, normalize } from "./module.f.ts"
 
 export default {
     normalize: [
@@ -18,5 +18,19 @@ export default {
             const norm = normalize("../../dir/../file.json")
             if (norm !== "../../file.json") { throw norm }
         },        
+    ],
+    concat: [
+        () => {
+            const c = concat("a")("b")
+            if (c !== "a/b") { throw c }
+        }, 
+        () => {
+            const c = concat("a///b/")("c")
+            if (c !== "a/b/c") { throw c }
+        }, 
+        () => {
+            const c = concat("a/../b/..")("c")
+            if (c !== "c") { throw c }
+        },   
     ]
 }
