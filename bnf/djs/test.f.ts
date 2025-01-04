dc2786b9951e3e0284e569fe25afea1b9e33e331
sergey-shandar <sergey-shandar@users.noreply.github.com>
2025-01-04 10:44:21 -0800

    d

--- bnf/djs/test.f.ts
+++ bnf/djs/test.f.ts
@@ -115,3 +115,113 @@ const classic = () => {
     } as const
     const _map: RuleMap<keyof typeof map> = map
 }
+
+const deterministic = () => {
+    const map = {
+        json: [
+            ['element']
+        ],
+        value: [
+            ['object'],
+            ['array'],
+            ['string'],
+            ['number'],
+            s('true'),
+            s('false'),
+            s('null'),
+        ],
+        object: [
+            [c('{'), 'ws', c('}')],
+            [c('{'), 'members', c('}')],
+        ],
+        members: [
+            ['member'],
+            ['member', c(','), 'members'],
+        ],
+        member: [
+            ['ws', 'string', 'ws', c(':'), 'element'],
+        ],
+        array: [
+            [c('['), 'ws', c(']')],
+            [c('['), 'elements', c(']')],
+        ],
+        elements: [
+            ['element'],
+            ['element', c(','), 'elements'],
+        ],
+        element: [
+            ['ws', 'value', 'ws'],
+        ],
+        string: [
+            [c('"'), 'characters', c('"')],
+        ],
+        characters: [
+            ['character'],
+            ['character', 'characters'],
+        ],
+        character: [
+            [[0x20, 0x21]],      // exclude '"' 0x22
+            [[0x23, 0x5B]],      // exclude '\' 0x5C
+            [[0x5D ,0x10FFFF]],  // 93-1114111
+            [c('\\'), 'escape'], // 92
+        ],
+        escape: [
+            [c('"')],
+            [c('\\')],
+            [c('/')],
+            [c('b')],
+            [c('f')],
+            [c('n')],
+            [c('r')],
+            [c('t')],
+            [c('u'), 'hex', 'hex', 'hex', 'hex'],
+        ],
+        hex: [
+            ['digit'],
+            [[0x41, 0x46]], // A-F
+            [[0x61, 0x66]], // a-f
+        ],
+        number: [
+            ['integer', 'fraction', 'exponent'],
+        ],
+        integer: [
+            ['digit'],
+            ['onenine', 'digits'],
+            [c('-'), 'digit'],
+            [c('-'), 'onenine', 'digits'],
+        ],
+        digits: [
+            ['digit'],
+            ['digit', 'digits'],
+        ],
+        digit: [
+            [c('0')],
+            ['onenine'],
+        ],
+        onenine: [
+            [[0x31, 0x39]], // 1-9
+        ],
+        fraction: [
+            [],
+            [c('.'), 'digits'],
+        ],
+        exponent: [
+            [],
+            [c('E'), 'sign', 'digits'],
+            [c('e'), 'sign', 'digits'],
+        ],
+        sign: [
+            [],
+            [c('+')],
+            [c('-')],
+        ],
+        ws: [
+            [],
+            [c(' '), 'ws'],
+            [c('\n'), 'ws'],
+            [c('\r'), 'ws'],
+            [c('\t'), 'ws'],
+        ],
+    } as const
+    const _map: RuleMap<keyof typeof map> = map
+}
