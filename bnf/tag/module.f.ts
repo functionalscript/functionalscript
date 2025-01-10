cbd833cc63cfdbdd65868a0f387cb67a20482524
sergey-shandar <sergey-shandar@users.noreply.github.com>
2025-01-10 00:15:20 -0800

    ok

--- bnf/tag/module.f.ts
+++ bnf/tag/module.f.ts
@@ -2,13 +2,15 @@ import { stringToCodePointList } from '../../text/utf16/module.f.ts'
 import { toArray } from '../../types/list/module.f.ts'
 import type { TerminalRange } from '../func/module.f.ts'
 
-export type Sequence = readonly (TerminalRange | Rule)[]
+export type Sequence = readonly (Or | LazyRule | TerminalRange)[]
 
 export type Or = {
-    readonly[k in string]: Sequence|Rule|number
+    readonly[k in string]: Sequence | LazyRule
 }
 
-export type Rule = () => Or|Sequence
+export type LazyRule = () => Or | Sequence
+
+export type Rule = Sequence | Or | LazyRule
 
 export const set = (s: string): Or =>
     Object.fromEntries(toArray(stringToCodePointList(s)).map(v => [String.fromCodePoint(v), [[v, v]]] as const))
--- bnf/tag/test.f.ts
+++ bnf/tag/test.f.ts
@@ -1,7 +1,24 @@
-import { todo } from '../../dev/module.f.ts'
 import { range } from '../../text/ascii/module.f.ts'
 import { cp, str } from '../func/module.f.ts'
 import { set, type Rule } from './module.f.ts'
+
+/*
+const none: Rule = () => ({
+    '': []
+})
+
+const option = (v: Rule): Rule => () => ({
+    none,
+    some: v
+})
+
+const repeat0 = (v: Rule): Rule => {
+    const f = () => ({
+        none,
+        some: [v, f]
+    })
+    return f
+}
 
 const _classic = () => {
 
@@ -14,9 +31,9 @@ const _classic = () => {
         array,
         string,
         number,
-        true: 1,
-        false: 1,
-        null: 1,
+        true: str('true'),
+        false: str('false'),
+        null: str('null'),
     })
 
     const object: Rule = () => ({
@@ -48,17 +65,14 @@ const _classic = () => {
     const string: Rule = () =>
         [cp('"'), characters, cp('"')]
 
-    const characters: Rule = () => ({
-        none,
-        characters: [character, characters],
-    })
-
     const character: Rule = () => ({
         0: [[0x20, 0x21]],
         1: [[0x23, 0x5B]],
         2: [[0x5D, 0x10FFFF]],
         escape: [cp('\\'), escape],
     })
+
+    const characters: Rule = repeat0(character)
 
     const escape: Rule = () => ({
         ...set('"\\/bfnrt'),
@@ -115,8 +129,128 @@ const _classic = () => {
         '\r': [cp('\r'), ws],
         '\t': [cp('\t'), ws],
     })
+}
 
-    const none: Rule = () => ({
-        '': []
+const _deterministic = () => {
+
+    const json: Rule = () => ({
+        element
+    })
+
+    const value: Rule = () => ({
+        object,
+        array,
+        string,
+        number,
+        true: str('true'),
+        false: str('false'),
+        null: str('null'),
+    })
+
+    const object: Rule = () => [cp('{'), () => ({
+        ws,
+        members,
+    }), cp('}')]
+
+    const members: Rule = () => [member, () => ({
+        none,
+        members: [cp(','), members],
+    })]
+
+    const member: Rule = () =>
+        [ws, string, ws, cp(':'), element]
+
+    const array: Rule = () => [cp('['), ws, array2, cp(']')]
+
+    const array2: Rule = () => ({
+        none,
+        elements: [element1, elementsTail],
+    })
+
+    const elements: Rule = () => [ws, element1, elementsTail]
+
+    const elementsTail: Rule = () => ({
+        none,
+        elements: [cp(','), elements],
+    })
+
+    const element: Rule = () =>
+        [ws, element1]
+
+    const element1: Rule = () => [value, ws]
+
+    const string: Rule = () =>
+        [cp('"'), characters, cp('"')]
+
+    const characters: Rule = () => ({
+        none,
+        characters: [character, characters],
+    })
+
+    const character: Rule = () => ({
+        0: [[0x20, 0x21]],
+        1: [[0x23, 0x5B]],
+        2: [[0x5D, 0x10FFFF]],
+        escape: [cp('\\'), escape],
+    })
+
+    const escape: Rule = () => ({
+        ...set('"\\/bfnrt'),
+        u: [cp('u'), hex, hex, hex, hex],
+    })
+
+    const hex: Rule = () => ({
+        digit,
+        AF: [range('AF')],
+        af: [range('af')],
+    })
+
+    const number: Rule = () => [integer, fraction, exponent]
+
+    const integer: Rule = () => [numberSign, unsignedInteger]
+
+    const unsignedInteger: Rule = () => ({
+        ...set('0'),
+        onenine: [onenine, digits0],
+    })
+
+    const digits0 = () => ({
+        none,
+        digits,
+    })
+
+    const numberSign: Rule = () => ({
+        none,
+        ...set('-'),
+    })
+
+    const digits: Rule = () => [digit, digits0]
+
+    const digit: Rule = () => ({
+        ...set('0'),
+        onenine,
+    })
+
+    const onenine: Rule = () => [range('19')]
+
+    const fraction: Rule = () => ({
+        none,
+        digits: [cp('.'), digits]
+    })
+
+    const exponent: Rule = () => ({
+        none,
+        e: [() => set('Ee'), sign, digits],
+    })
+
+    const sign: Rule = () => ({
+        none,
+        ...set('+-'),
+    })
+
+    const ws: Rule = () => ({
+        none,
+        cp: [() => set(' \n\r\t'), ws],
     })
 }
+*/
