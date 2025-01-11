c813dd10cd21b8add8a09347ecfe4428dc9c483d
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2025-01-11 11:16:14 -0800

    remove com (#367)

--- com/cpp/testlib.f.ts
+++
@@ -1,6 +0,0 @@
-import { join } from '../../types/string/module.f.ts'
-import { flat } from '../../text/module.f.ts'
-import library from '../types/testlib.f.ts'
-import { cpp } from './module.f.ts'
-
-export default () => join('\n')(flat('    ')(cpp('My')(library)))
