92d8e9514bdd7dd1eae3cfb4edde019565c6f8a4
sergey-shandar <sergey-shandar@users.noreply.github.com>
2025-01-10 20:28:30 -0800

    Two new algorithms

--- types/bigint/benchmark.mjs
+++ types/bigint/benchmark.mjs
@@ -60,6 +60,44 @@ const stringHexLog2 = v => {
     return len + 31n - BigInt(Math.clz32(Number(x)))
 }
 
+const string32Log2 = v => {
+    const len = (BigInt(v.toString(32).length) - 1n) * 5n
+    const x = v >> len
+    return len + 31n - BigInt(Math.clz32(Number(x)))
+}
+
+const m = 1023n
+
+const mathLog2 = v => {
+    if (v <= 0n) { return -1n }
+    let result = -1n
+    let i = 1023n
+    while (true) {
+        const n = v >> i
+        if (n === 0n) {
+            // overshot
+            break
+        }
+        v = n
+        result += i
+        i <<= 1n
+    }
+    // We know that `v` is not 0 so it doesn't make sense to check `n` when `i` is 0.
+    // Because of this, We check if `i` is greater than 32 before we divide it by 2.
+    while (i !== 1023n) {
+        i >>= 1n
+        const n = v >> i
+        if (n !== 0n) {
+            result += i
+            v = n
+        }
+    }
+    const x = BigInt(Math.log2(Number(v)))
+    return result + x + (v >> x)
+}
+
+// const mathLog2 = v => BigInt(Math.log2(Number(v)))
+
 const log = document.getElementById('log')
 
 const benchmark = (s, f) => {
@@ -68,8 +106,16 @@ const benchmark = (s, f) => {
     let e = 1_048_575n
     let c = 1n << e
     for (let i = 0n; i < 1_000; ++i) {
-        const x = f(c)
-        if (x !== e) { throw x }
+        //
+        {
+            const x = f(c)
+            if (x !== e) { throw [e, x] }
+        }
+        {
+            const x = f(c - 1n)
+            if (x !== e - 1n) { throw [e, x] }
+        }
+        //
         c >>= 1n
         --e
     }
@@ -82,5 +128,7 @@ const benchmark = (s, f) => {
 
 benchmark('stringLog2', stringLog2)
 benchmark('stringHexLog2', stringHexLog2)
+benchmark('string32Log2', string32Log2)
 benchmark('oldLog2', oldLog2)
 benchmark('log2', log2)
+benchmark('mathLog2', mathLog2)
