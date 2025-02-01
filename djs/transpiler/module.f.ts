18543da2e3766b5fdc9d675aa415f83a60a0f99e
Shandar Denys <msden.ua@gmail.com>
2025-02-01 21:22:26 +0200

    transpile

--- djs/transpiler/module.f.ts
+++ djs/transpiler/module.f.ts
@@ -1,6 +1,6 @@
 import type * as djs from '../module.f.ts'
-import { type Result, error } from '../../types/result/module.f.ts'
-import { fold, drop, map as listMap, type List, find, last } from '../../types/list/module.f.ts'
+import { type Result, error, ok } from '../../types/result/module.f.ts'
+import { fold, drop, map as listMap, type List, find, toArray } from '../../types/list/module.f.ts'
 import type * as Operator from '../../types/function/operator/module.f.ts'
 import { tokenize } from '../tokenizer/module.f.ts'
 import { setReplace, at, type Map } from '../../types/map/module.f.ts'
@@ -8,23 +8,33 @@ import type { Fs } from '../io/module.f.ts'
 import { stringToList } from '../../text/utf16/module.f.ts'
 import { concat as pathConcat } from '../../path/module.f.ts'
 import { parseFromTokens } from '../parser/module.f.ts'
-import type { AstModule } from '../shared/module.f.ts'
-import { todo } from '../../dev/module.f.ts'
+import { run, type AstModule } from '../shared/module.f.ts'
 
 export type ParseContext = {    
     readonly fs: Fs    
-    readonly complete: Map<Result<djs.Unknown, string>>
+    readonly complete: Map<djs.Unknown>
     readonly stack: List<string>
+    readonly error: string | null
 }
+
+const mapDjs
+    : (context: ParseContext) => (path: string) => djs.Unknown
+    = context => path => {
+        return at(path)(context.complete)
+    }
 
 const transpileWithImports
     : (path: string) => (parseModuleResult: Result<AstModule, string>) => (context: ParseContext) => ParseContext
     = path => parseModuleResult => context => {
         if (parseModuleResult[0] === 'ok') {
             const pathsCombine = listMap(pathConcat(path))(parseModuleResult[1][0])
-            const contextAfterImports = fold(foldNextModuleOp)({ ... context, stack: { first: path, tail: context.stack } })(pathsCombine)
-            todo()
-            return { ... contextAfterImports, stack: drop(1)(contextAfterImports.stack) }            
+            const contextWithImports = fold(foldNextModuleOp)({ ... context, stack: { first: path, tail: context.stack } })(pathsCombine)
+            if (contextWithImports.error !== null) {
+                return contextWithImports
+            }
+            const args = toArray(listMap(mapDjs(contextWithImports))(pathsCombine))
+            const djs = run(parseModuleResult[1][1])(args)
+            return { ... contextWithImports, stack: drop(1)(contextWithImports.stack), complete: setReplace(path)(djs)(contextWithImports.complete) }
         }
         return context
 }
@@ -70,10 +80,10 @@ const foldNextModuleOp
 
 export const transpile: (fs: Fs) => (path: string) => Result<djs.Unknown, string>
  = fs => path => {
-    const context = foldNextModuleOp(path)({fs, stack: null, complete: null})
-    const result = at(path)(context.complete)
-    if (result === null) {
-        return error('error')
+    const context = foldNextModuleOp(path)({fs, stack: null, complete: null, error: null})
+    if (context.error !== null) {
+        return error(context.error)
     }
-    return result
+    const result = at(path)(context.complete)
+    return ok(result)
  }
