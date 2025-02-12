f3b781485d6c1fc0290dce76403de3837de998f5
Sasha Gil <alexgil@microsoft.com>
2025-02-12 15:29:44 -0800

    Replace unwrap() with ?

--- nanvm-lib/tests/test.rs
+++ nanvm-lib/tests/test.rs
@@ -439,31 +439,32 @@ fn big_int_add<A: Any>() {
     assert_eq!((n0.clone() + n0.clone()).unwrap(), n0);
 }
 
-fn big_int_mul<A: Any>() {
+fn big_int_mul<A: Any>() -> Result<(), WAny<A>> {
     let n0: WAny<A> = WAny::big_int(Sign::Positive, []);
     let n1: WAny<A> = WAny::big_int(Sign::Positive, [1]);
-    assert_eq!((n1.clone() * n0.clone()).unwrap(), n0);
-    assert_eq!((n0.clone() * n1.clone()).unwrap(), n0);
+    assert_eq!((n1.clone() * n0.clone())?, n0);
+    assert_eq!((n0.clone() * n1.clone())?, n0);
 
     let n_minus1: WAny<A> = WAny::big_int(Sign::Negative, [1]);
-    assert_eq!((n_minus1.clone() * n0.clone()).unwrap(), n0);
-    assert_eq!((n0.clone() * n_minus1.clone()).unwrap(), n0);
-    assert_eq!((n_minus1.clone() * n_minus1.clone()).unwrap(), n1);
+    assert_eq!((n_minus1.clone() * n0.clone())?, n0);
+    assert_eq!((n0.clone() * n_minus1.clone())?, n0);
+    assert_eq!((n_minus1.clone() * n_minus1.clone())?, n1);
 
     let a: WAny<A> = WAny::big_int(Sign::Positive, [1, 2, 3, 4]);
     let b: WAny<A> = WAny::big_int(Sign::Positive, [5, 6, 7]);
     let expected: WAny<A> = WAny::big_int(Sign::Positive, [5, 16, 34, 52, 45, 28]);
-    assert_eq!((a.clone() * b.clone()).unwrap(), expected);
-    assert_eq!((b.clone() * a.clone()).unwrap(), expected);
+    assert_eq!((a.clone() * b.clone())?, expected);
+    assert_eq!((b.clone() * a.clone())?, expected);
 
     let a: WAny<A> = WAny::big_int(Sign::Negative, [u64::MAX]);
     let expected: WAny<A> = WAny::big_int(Sign::Positive, [1, u64::MAX - 1]);
-    assert_eq!((a.clone() * a.clone()).unwrap(), expected);
+    assert_eq!((a.clone() * a.clone())?, expected);
 
     let b: WAny<A> = WAny::big_int(Sign::Negative, [u64::MAX, u64::MAX, u64::MAX]);
     let expected: WAny<A> = WAny::big_int(Sign::Positive, [1, u64::MAX, u64::MAX, u64::MAX - 1]);
-    assert_eq!((a.clone() * b.clone()).unwrap(), expected);
-    assert_eq!((b.clone() * a.clone()).unwrap(), expected);
+    assert_eq!((a.clone() * b.clone())?, expected);
+    assert_eq!((b.clone() * a.clone())?, expected);
+    Ok(())
 }
 
 fn test_vm<A: Any>() {
@@ -472,7 +473,7 @@ fn test_vm<A: Any>() {
     unary_minus::<A>();
     multiply::<A>();
     big_int_add::<A>();
-    big_int_mul::<A>();
+    big_int_mul::<A>().unwrap();
 }
 
 #[test]
