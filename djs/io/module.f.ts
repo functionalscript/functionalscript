9239dee5d54a8c8df0fad167adfd71de53a8dbcf
Shandar Denys <msden.ua@gmail.com>
2025-02-16 21:13:12 +0200

    io module (#386)
    * virtual fs
    
    * io
    
    * virtual fs
    
    * fix
    
    * index

--- djs/io/module.f.ts
+++
@@ -1,4 +0,0 @@
-export type Fs = {
-   readonly readFileSync: (name: string) => string | null
-   readonly writeFileSync: (name: string) => (content: string) => Fs
-}
---
+++ io/module.f.ts
@@ -0,0 +1,15 @@
+export type BufferEncoding = 'utf8'
+
+export type Fs = {
+   readonly writeFileSync: (file: string, data: string) => void
+   readonly readFileSync: (path: string, options: BufferEncoding) => string | null
+}
+
+export type Console = {
+   readonly log: (...d: unknown[]) => void
+}
+
+export type Io = {
+   readonly console: Console,
+   readonly fs: Fs
+}
