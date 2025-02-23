44b476c2a8597c99b59f1eab3575952ca2577af3
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2025-02-22 18:59:10 -0800

    ok

--- text/sgr/module.f.ts
+++ text/sgr/module.f.ts
@@ -1,16 +1,20 @@
 type End = 'm'
 
 /**
+ * Control Sequence Introducer (CSI) escape sequence.
  * https://en.wikipedia.org/wiki/ANSI_escape_code#Control_Sequence_Introducer_commands
+ *
+ * @param end - The final character that indicates the type of sequence.
+ * @returns A function that takes a code (number or string) and returns the complete ANSI escape sequence.
  */
-export const csi = (c: number, end: End): string => `\x1b[${c.toString()}${end}`
+export const csi = (end: End) => (code: number | string): string =>
+    `\x1b[${code.toString()}${end}`
 
 /**
- * Select Graphic Rendition (SGR) parameters.
- *
+ * Specialization of CSI for Select Graphic Rendition (SGR) sequences.
  * https://en.wikipedia.org/wiki/ANSI_escape_code#SGR
  */
-export const sgr = (c: number): string => csi(c, 'm')
+export const sgr = csi('m')
 
 export const reset: string = sgr(0)
 export const bold: string = sgr(1)
