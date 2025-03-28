fa3790ee3f0f1404260819c02edc99dc557c6376
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2025-03-27 22:09:00 -0700

    hren

--- io/module.f.ts
+++ io/module.f.ts
@@ -18,4 +18,32 @@ export type Io = {
 
 export type Process = {
    readonly argv: string[]
+   readonly env: Env
+   readonly exit: (code: number) => never
+}
+
+/**
+ * The environment variables.
+ */
+export type Env = {
+   readonly [k: string]: string|undefined
+}
+
+export type Run = (f: (io: Io) => Promise<number>) => Promise<never>
+
+/**
+ * Runs a function and exits the process with the returned code
+ * Handles errors by exiting with code 1
+ */
+export const run = (io: Io): Run => {
+   const { process: { exit }, console: { error } } = io
+   const code = ([x, b]: Result<number, unknown>) => {
+      if (x === 'error') {
+         error(x[1])
+         return 1
+      } else {
+         return b
+      }
+   }
+   return async f => exit(code(await io.asyncTryCatch(() => f(io))))
 }
