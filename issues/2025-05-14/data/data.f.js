4626549bcdd8ddcddc8ee835cc7231666571a3c3
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2025-05-14 14:31:26 -0700

    readme

--- issues/2025-05-14/data/data.f.js
+++
@@ -1,7 +0,0 @@
-import shared from "./shared.f.js"
-const s = "Hello, world!"
-export default {
-  "a": shared,
-  "b": shared,
-  "s": s
-}
--- issues/2025-05-14/data/shared.f.js
+++
@@ -1,1 +0,0 @@
-export default { "name": "shared object" }
