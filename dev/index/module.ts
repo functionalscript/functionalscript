2e2e9d4ffc4007438352bc6b1901d8f75fbab53c
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2025-05-21 09:55:23 -0700

    bin

--- dev/index/module.ts
+++
@@ -1,4 +0,0 @@
-import { index } from "../module.f.ts"
-import node from '../../io/module.js'
-
-await node(index)
---
+++ issues/demo/data/new.f.js
@@ -0,0 +1,6 @@
+import data from "./data.f.js"
+
+export default {
+    "c": 54n,
+    "d": data
+}
