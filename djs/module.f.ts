6e0907b7e85b1ce4f143c009788b01e0529f94ec
Shandar Denys <msden.ua@gmail.com>
2025-06-01 22:49:42 +0300

    Tokens with metadata (#481)
    * remove unused
    
    * tokenize with position
    
    * minor
    
    * minor
    
    * js token with position
    
    * fix codestyle
    
    * js tken with metadata
    
    * test
    
    * minor
    
    * djs token with metadata
    
    * add token metadata to parser error
    
    * test with metadata
    
    * fsc - output metadata in error message
    
    * fix
    
    ---------
    
    Co-authored-by: Sergey Shandar <sergey-shandar@users.noreply.github.com>

--- djs/module.f.ts
+++ djs/module.f.ts
@@ -37,7 +37,7 @@ export const compile = ({ console: { error }, fs, process: { argv } }: Io): Prom
             break
         }
         case 'error': {
-            error(`Parse error: ${result[1]}`)
+            error(`Parse error: ${JSON.stringify(result[1])}`)
             break
         }
     }
