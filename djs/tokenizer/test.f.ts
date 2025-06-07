24dc531a7e4843c26df0c09bf4b1fd71f820667d
Shandar Denys <msden.ua@gmail.com>
2025-06-07 17:53:24 +0300

    clickable link to file in error log

--- djs/tokenizer/test.f.ts
+++ djs/tokenizer/test.f.ts
@@ -10,10 +10,6 @@ const tokenizeString
     : (s: string) => readonly tokenizer.DjsToken[]
     = s => toArray(list.map(withoutMetada)(tokenizer.tokenize(encoding.stringToList(s))('')))
 
-const tokenizeStringWithMetadata
-    : (s: string) => readonly tokenizer.DjsTokenWithMetadata[]
-     = s => toArray(tokenizer.tokenize(encoding.stringToList(s))(''))
-
 const stringify = serializer.stringifyAsTree(sort)
 
 const withoutMetada
