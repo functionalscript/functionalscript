118ecae86dc9aa9ac3d6b7b5a1072acf5724446e
Shandar Denys <msden.ua@gmail.com>
2025-06-14 20:47:42 +0300

    tests

--- bnf/data/test.f.ts
+++ bnf/data/test.f.ts
@@ -1,11 +1,26 @@
+import { stringify } from '../../json/module.f.ts'
+import { sort } from '../../types/object/module.f.ts'
+import { range } from '../module.f.ts'
 import { classic, deterministic } from '../testlib.f.ts'
 import { toData } from './module.f.ts'
 
 export default {
-    toData: () => {
-        const c = toData(classic())
-        const d = toData(deterministic())
-    },
+    toData: [
+        () => {
+            const c = toData(classic())
+            const d = toData(deterministic())
+        },
+        () => {
+            const stringRule = 'true'
+            const result = stringify(sort)(toData(stringRule))
+            if (result != '[{"":["0","1","2","3"],"0":116,"1":114,"2":117,"3":101},""]') { throw result }
+        },
+        () => {
+            const terminalRangeRule = range('AF')
+            const result = stringify(sort)(toData(terminalRangeRule))
+            if (result != '[{"":1090519110},""]') { throw result } //1090519110 = 65 * 2^24 + 70
+        }
+    ],
     example: () => {
         const grammar = {
             space: 0x000020_000020,
