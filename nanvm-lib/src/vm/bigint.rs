1bbf26fa9c839ec31289545cef4ac9c1d600a789
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2025-06-21 12:57:51 -0700

    ok

--- nanvm-lib/src/vm/bigint.rs
+++ nanvm-lib/src/vm/bigint.rs
@@ -16,6 +16,12 @@ impl<A: IInternalAny> Default for BigInt<A> {
     }
 }
 
+impl<A: IInternalAny> BigInt<A> {
+    fn is_zero(&self) -> bool {
+        self.0.is_empty()
+    }
+}
+
 impl<A: IInternalAny> PartialEq for BigInt<A> {
     fn eq(&self, other: &Self) -> bool {
         self.0.deep_eq(&other.0)
@@ -35,15 +41,14 @@ impl<A: IInternalAny> TryFrom<Any<A>> for BigInt<A> {
 
 impl<A: IInternalAny> Debug for BigInt<A> {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        let len = self.0.len();
-        if len == 0 {
+        if self.is_zero() {
             return f.write_str("0n");
         }
         if *self.0.header() == Sign::Negative {
             f.write_char('-')?;
         }
         f.write_str("0x")?;
-        let last = len - 1;
+        let last = self.0.len() - 1;
         write!(f, "{:X}", self.0.at(last))?;
         for i in (0..last).rev() {
             write!(f, "_{:016X}", self.0.at(i))?;
