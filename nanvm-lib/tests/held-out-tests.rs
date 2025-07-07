73293e623b23017f88629b05e4e2022219cfcd31
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2025-07-07 12:34:22 -0700

    serialize and deserialize

--- nanvm-lib/tests/held-out-tests.rs
+++ nanvm-lib/tests/held-out-tests.rs
@@ -7,7 +7,7 @@ use nanvm_lib::{
     simple::Simple,
 };
 
-fn eq_value<A: Any>(a: &A, b: &A) -> bool {
+fn deep_eq<A: Any>(a: &A, b: &A) -> bool {
     match (a.clone().unpack(), b.clone().unpack()) {
         (Unpacked::Nullish(a), Unpacked::Nullish(b)) => a == b,
         (Unpacked::Bool(a), Unpacked::Bool(b)) => a == b,
@@ -27,7 +27,7 @@ fn eq_value<A: Any>(a: &A, b: &A) -> bool {
         (Unpacked::Array(a), Unpacked::Array(b)) => {
             a.header() == b.header()
                 && a.items().len() == b.items().len()
-                && a.items().iter().zip(b.items()).all(|(x, y)| eq_value(x, y))
+                && a.items().iter().zip(b.items()).all(|(x, y)| deep_eq(x, y))
         }
         (Unpacked::Object(a), Unpacked::Object(b)) => {
             a.header() == b.header()
@@ -35,16 +35,26 @@ fn eq_value<A: Any>(a: &A, b: &A) -> bool {
                 && a.items()
                     .iter()
                     .zip(b.items())
-                    .all(|((k1, v1), (k2, v2))| k1.items() == k2.items() && eq_value(v1, v2))
+                    .all(|((k1, v1), (k2, v2))| k1.items() == k2.items() && deep_eq(v1, v2))
         }
         _ => false,
     }
 }
 
-fn serialization<A: Any>() {
+fn serialization_test<A: Any>(values: &[A]) {
     use std::io::Cursor;
 
-    let values: Vec<A> = vec![
+    for value in values.into_iter() {
+        let mut buf = Vec::new();
+        value.clone().serialize(&mut buf).unwrap();
+        let mut cursor = Cursor::new(buf);
+        let result = A::deserialize(&mut cursor).unwrap();
+        assert!(deep_eq(value, &result));
+    }
+}
+
+fn serialization<A: Any>() {
+    serialization_test(&[
         Simple::Nullish(Nullish::Null).to_unknown(),
         Simple::Nullish(Nullish::Undefined).to_unknown(),
         Simple::Boolean(true).to_unknown(),
@@ -59,18 +69,70 @@ fn serialization<A: Any>() {
             ("b".to_string16::<A>(), "c".to_unknown()),
         ]
         .to_object_unknown(),
-    ];
-
-    for value in values.into_iter() {
-        let mut buf = Vec::new();
-        value.clone().serialize(&mut buf).unwrap();
-        let mut cursor = Cursor::new(buf);
-        let result = A::deserialize(&mut cursor).unwrap();
-        assert!(eq_value(&value, &result));
-    }
+    ]);
 }
 
 #[cfg(test)]
-fn held_out_test() {
-    serialization::<naive::Any>();
+fn test_nullish() {
+    serialization_test::<naive::Any>(&[
+        Simple::Nullish(Nullish::Null).to_unknown(),
+        Simple::Nullish(Nullish::Undefined).to_unknown(),
+    ]);
+}
+
+#[cfg(test)]
+fn test_boolean() {
+    serialization_test::<naive::Any>(&[
+        Simple::Boolean(true).to_unknown(),
+        Simple::Boolean(false).to_unknown(),
+    ]);
+}
+
+#[cfg(test)]
+fn test_number() {
+    use std::f64::{INFINITY, NAN};
+
+    serialization_test::<naive::Any>(&[
+        Simple::Number(2.3).to_unknown(),
+        Simple::Number(-INFINITY).to_unknown(),
+        Simple::Number(NAN).to_unknown(),
+    ]);
+}
+
+#[cfg(test)]
+fn test_string() {
+    serialization_test::<naive::Any>(&[
+        "Hello".to_unknown(),
+        "".to_unknown(),
+    ]);
+}
+
+#[cfg(test)]
+fn test_bigint() {
+    use nanvm_lib::naive::BigInt;
+
+    serialization_test::<naive::Any>(&[
+        BigInt::new(Sign::Positive, [12]).to_unknown(),
+        BigInt::new(Sign::Negative, [13, 14]).to_unknown(),
+    ]);
+}
+
+#[cfg(test)]
+fn test_array() {
+    serialization_test::<naive::Any>(&[
+        [].to_array_unknown(),
+        [Simple::Number(7.0).to_unknown()].to_array_unknown(),
+    ]);
+}
+
+#[cfg(test)]
+fn test_object() {
+    serialization_test::<naive::Any>(&[
+        [].to_object_unknown(),
+        [
+            ("a".to_string16::<naive::Any>(), Simple::Number(1.0).to_unknown()),
+            ("b".to_string16::<naive::Any>(), "c".to_unknown()),
+        ]
+        .to_object_unknown(),
+    ]);
 }
