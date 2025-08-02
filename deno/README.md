076da51d2f7159a7be518df534aac6203a93c0c3
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2025-08-02 13:53:42 -0700

    dev

--- deno/README.md
+++ deno/README.md
@@ -3,13 +3,11 @@
 To enable Deno testing in your project, you can add a simple test runner file `tets.ts` like this:
 
 ```ts
-import run from 'functionalscript/deno/module.js'
-
-await run()
+import {} from 'functionalscript/deno/test.run.js'
 ```
 
 And then run it as `deno test --allow-env --allow-read`.
 
-Another option is to run the [./test.ts](./test.ts) directly, for example
+Another option is to run the [./test.run.ts](./test.run.ts) directly, for example
 
-`deno test --allow-env --allow-read node_modules/functionalscript/deno/test.js`
+`deno test --allow-env --allow-read node_modules/functionalscript/deno/test.run.js`
--- deno/test.ts
+++ deno/test.ts
@@ -1,3 +1,59 @@
-import run from './module.ts'
+import { io } from '../io/module.ts'
+import { loadModuleMap } from '../dev/module.f.ts'
+import { isTest, parseTestSet } from '../dev/tf/module.f.ts'
+
+type DenoTestStep = {
+    readonly step: (name: string, f: () => void | Promise<void>) => Promise<void>
+}
+
+type DenoFunc = (t: DenoTestStep) => void | Promise<void>
+
+type DenoArg = readonly[string, DenoFunc]
+
+export type DenoTest = (...arg: DenoArg) => void
+
+type Test = readonly[string, unknown]
+
+declare namespace Deno {
+    const test: DenoTest
+}
+
+const parse = parseTestSet(io.tryCatch)
+
+const denoTest = (x: Test) => async(t: DenoTestStep) => {
+    let subTests = [x]
+    while (true) {
+        const [first, ...rest] = subTests
+        if (first === undefined) {
+            break
+        }
+        subTests = rest
+        //
+        const [name, value] = first
+        const set = parse(value)
+        if (typeof set === 'function') {
+            const g = () => {
+                const r = set()
+                subTests = [...subTests, [`${name}()`, r]]
+            }
+            await t.step(name, g)
+        } else {
+            for (const [j, y] of set) {
+                const pr = `${name}/${j}`
+                subTests = [...subTests, [pr, y]]
+            }
+        }
+    }
+}
+
+const run = async(): Promise<void> => {
+    const x = await loadModuleMap(io)
+
+    for (const [i, v] of Object.entries(x)) {
+        if (isTest(i)) {
+            Deno.test(i, denoTest(['', v.default]))
+        }
+    }
+}
 
 await run()
