348b3fbe26240cc5930de909040ce561f5b257b8
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2025-10-08 05:57:32 -0700

    indent

--- nanvm-lib/src/vm/object.rs
+++ nanvm-lib/src/vm/object.rs
@@ -14,13 +14,13 @@ pub type Property<A> = (String16<A>, Any<A>);
 /// ```
 /// use nanvm_lib::vm::{Object, ToObject, IVm, Array, Any, ToAny, naive::Naive};
 /// fn object_test<A: IVm>() {
-///   let b: Object<A> = [("a".into(), true.to_any())].to_object();
-///   let b1: Object<A> = [("a".into(), true.to_any())].to_object();
-///   assert_eq!(b, b);
-///   assert_ne!(b, b1);
-///   let ac: Any<A> = b.clone().to_any();
-///   let d: Object<A> = ac.try_into().unwrap();
-///   assert_eq!(d, b);
+///     let b: Object<A> = [("a".into(), true.to_any())].to_object();
+///     let b1: Object<A> = [("a".into(), true.to_any())].to_object();
+///     assert_eq!(b, b);
+///     assert_ne!(b, b1);
+///     let ac: Any<A> = b.clone().to_any();
+///     let d: Object<A> = ac.try_into().unwrap();
+///     assert_eq!(d, b);
 /// }
 ///
 /// object_test::<Naive>();
