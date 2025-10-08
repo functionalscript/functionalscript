2e9f16a9fa0980db9ae9ce9f9787f58453e296c7
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2025-10-08 16:06:28 -0700

    minor

--- nanvm-lib/src/vm/array.rs
+++ nanvm-lib/src/vm/array.rs
@@ -51,6 +51,8 @@ impl<A: IVm> IntoIterator for Array<A> {
 
 impl<A: IVm> Index<u32> for Array<A> {
     type Output = Any<A>;
+    /// Currently panics if out of bounds.
+    /// TODO: Future versions may change to return `Nullish::Undefined`.
     fn index(&self, index: u32) -> &Self::Output {
         self.0.items().index(index as usize)
     }
--- nanvm-lib/src/vm/string16.rs
+++ nanvm-lib/src/vm/string16.rs
@@ -131,6 +131,9 @@ impl<A: IVm> IntoIterator for String16<A> {
 
 impl<A: IVm> Index<u32> for String16<A> {
     type Output = u16;
+    /// Currently panics if out of bounds.
+    /// TODO: Future versions may change to return `Option<u16>`.
+    /// Also we can implement `Index<Any<A>>`, `Index<f64>` and `Index<String16>`.
     fn index(&self, index: u32) -> &Self::Output {
         self.0.items().index(index as usize)
     }
