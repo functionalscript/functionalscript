ba21b1c4b95caee0afc5cb9c0ae2a23ce7d96bea
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2025-10-08 00:27:12 -0700

    ok

--- nanvm-lib/src/vm/array.rs
+++ nanvm-lib/src/vm/array.rs
@@ -12,7 +12,7 @@ use crate::{
 /// ```
 /// use nanvm_lib::{
 ///     vm::{ToArray, IVm, Array, Any, ToAny, naive::Naive},
-///     common::array::SizedIndex,
+///     common::{array::SizedIndex, default::default},
 /// };
 /// fn array_test<A: IVm>() {
 ///     let b: Array<A> = [1.0.to_any(), true.to_any()].to_array();
@@ -25,6 +25,9 @@ use crate::{
 ///     let ac: Any<A> = b.clone().to_any();
 ///     let d: Array<A> = ac.try_into().unwrap();
 ///     assert_eq!(d, b);
+///     let e0: Array<A> = default();
+///     let e1: Array<A> = [].to_array();
+///     assert_ne!(e0, e1);
 /// }
 ///
 /// array_test::<Naive>();
@@ -34,7 +37,7 @@ pub struct Array<A: IVm>(A::InternalArray);
 
 impl<A: IVm> Default for Array<A> {
     fn default() -> Self {
-        Array(A::InternalArray::new_empty(()))
+        [].to_array()
     }
 }
 
