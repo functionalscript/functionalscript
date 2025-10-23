e2b86695b9242591498d0b457d6afda348f666b5
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2025-10-22 22:52:07 -0700

    ok

--- types/bit_vec/module.f.ts
+++ types/bit_vec/module.f.ts
@@ -21,19 +21,19 @@ import { as_base, as_nominal, type Nominal } from '../nominal/module.f.ts'
  */
 export type Vec = Nominal<'bit_vec_0cef502e4a951e6e42f421c62abd79e7e9b07bee3e63549638676ec2d8ed98e3', bigint>
 
-export const unsafe_vec: (u: bigint) => Vec = as_nominal
+export const unsafeVec: (u: bigint) => Vec = as_nominal
 
-export const unsafe_bigint: (v: Vec) => bigint = as_base
+export const unsafeBigint: (v: Vec) => bigint = as_base
 
 /**
  * An empty vector of bits.
  */
-export const empty: Vec = unsafe_vec(1n)
+export const empty: Vec = unsafeVec(1n)
 
 /**
  * Calculates the length of the given vector of bits.
  */
-export const length = (v: Vec): bigint => log2(unsafe_bigint(v))
+export const length = (v: Vec): bigint => log2(unsafeBigint(v))
 
 const lazyEmpty = () => empty
 
@@ -52,7 +52,7 @@ export const vec = (len: bigint): (ui: bigint) => Vec => {
     if (len <= 0n) { return lazyEmpty }
     const stop = 1n << len
     const mask = stop - 1n
-    return data => unsafe_vec(stop | (data & mask))
+    return data => unsafeVec(stop | (data & mask))
 }
 
 /**
@@ -70,9 +70,7 @@ export const vec8: (u: bigint) => Vec = vec(8n)
  * const result = uint(vector); // result is 0x5n
  * ```
  */
-export const uint = (v: Vec): bigint => {
-    return unsafe_bigint(v) ^ (1n << length(v))
-}
+export const uint = (v: Vec): bigint => unsafeBigint(v) ^ (1n << length(v))
 
 /**
  * Represents operations for handling bit vectors with a specific bit order.
@@ -168,27 +166,27 @@ export const lsb: BitOrder = {
     front: len => {
         const m = mask(len)
         return v => {
-            const u = unsafe_bigint(v)
+            const u = unsafeBigint(v)
             const result = u & m
             return result === u ? uint(v) : result
         }
     },
     removeFront: len => v => {
-        const r = unsafe_bigint(v) >> len
-        return r === 0n ? empty : unsafe_vec(r)
+        const r = unsafeBigint(v) >> len
+        return r === 0n ? empty : unsafeVec(r)
     },
     popFront: len => {
         const m = mask(len)
         return v => {
-            const u = unsafe_bigint(v)
+            const u = unsafeBigint(v)
             const result = u & m
-            return result === u ? [uint(v), empty] : [result, unsafe_vec(u >> len)]
+            return result === u ? [uint(v), empty] : [result, unsafeVec(u >> len)]
         }
     },
     concat: a => {
         const aLen = length(a)
         const m = mask(aLen)
-        return b => unsafe_vec((unsafe_bigint(b) << aLen) | (unsafe_bigint(a) & m))
+        return b => unsafeVec((unsafeBigint(b) << aLen) | (unsafeBigint(a) & m))
     },
 }
 
@@ -202,13 +200,13 @@ export const lsb: BitOrder = {
 export const msb: BitOrder = {
     front: len => {
         const m = mask(len)
-        return v => (unsafe_bigint(v) >> (length(v) - len)) & m
+        return v => (unsafeBigint(v) >> (length(v) - len)) & m
     },
-    removeFront: len => v => vec(length(v) - len)(unsafe_bigint(v)),
+    removeFront: len => v => vec(length(v) - len)(unsafeBigint(v)),
     popFront: len => {
         const m = mask(len)
         return v => {
-            const u = unsafe_bigint(v)
+            const u = unsafeBigint(v)
             const d = length(v) - len
             return [(u >> d) & m, vec(d)(u)]
         }
@@ -245,4 +243,4 @@ export const u8List = ({ popFront }: BitOrder): (v: Vec) => Thunk<number> => {
     return f
 }
 
-export const listToVec = (bo: BitOrder): (list: List<Vec>) => Vec => fold(bo.concat)(empty)
+export const listToVec = ({ concat }: BitOrder): (list: List<Vec>) => Vec => fold(concat)(empty)
