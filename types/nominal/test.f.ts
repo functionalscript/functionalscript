67993e84aa861933ab7e28cfff8779c8f4517f95
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2025-11-01 08:21:22 -0700

    compilation error

--- types/nominal/test.f.ts
+++ types/nominal/test.f.ts
@@ -1,53 +1,71 @@
-import { asNominal, type Nominal } from "./module.f.ts"
+import { asBase, asNominal, type Nominal } from "./module.f.ts"
 
-declare const noCompareBrand: unique symbol;
+declare const noCompareBrand: unique symbol
 
-declare const brand: unique symbol;
+declare const brand: unique symbol
 
-export default () => {
-    type Str = Nominal<'utf8', bigint>
-    const strA: Str = asNominal(0b1_11000010_10100010_11000010_10100011n) // "¢£"
-    const strB: Str = asNominal(0b1_11000010_10100010_11000010_10100100n) // "¢¤"
-    if (strA === strB) { throw [strA, strB] }
-    // // TypeScript compilation error.
-    // const x1 = strA > strB
+export default {
+    pre: () => {
+        type Str = Nominal<'utf8', bigint>
+        const strA: Str = asNominal(0b1_11000010_10100010_11000010_10100011n) // "¢£"
+        const strB: Str = asNominal(0b1_11000010_10100010_11000010_10100100n) // "¢¤"
+        if (strA === strB) { throw [strA, strB] }
+        // // TypeScript compilation error.
+        // const x1 = strA > strB
 
-    //
-    {
-        type ForbiddenCompare<T extends object = object> = T & { __noCompare__: never }
+        //
+        {
+            type ForbiddenCompare<T extends object = object> = T & { __noCompare__: never }
 
-        type SafeId = ForbiddenCompare<{ value: number }>
+            type SafeId = ForbiddenCompare<{ value: number }>
 
-        const a: SafeId = { value: 1 } as SafeId
-        const b: SafeId = { value: 2 } as SafeId
+            const a: SafeId = { value: 1 } as SafeId
+            const b: SafeId = { value: 2 } as SafeId
 
-        // No Compile-time error
-        if (a < b) { }
-    }
+            // No Compile-time error
+            if (a < b) { }
+        }
 
-    {
-        interface NoCompare { _brand: 'NoCompare' }
+        {
+            interface NoCompare { _brand: 'NoCompare' }
 
-        const x: NoCompare = { _brand: 'NoCompare' }
-        // No Error
-        if (x < x) { }
-    }
-    {
-        type NoCompare = { [noCompareBrand]: void }
+            const x: NoCompare = { _brand: 'NoCompare' }
+            // No Error
+            if (x < x) { }
+        }
+        {
+            type NoCompare = { [noCompareBrand]: void }
 
-        const a = {} as NoCompare
-        const b = {} as NoCompare
+            const a = {} as NoCompare
+            const b = {} as NoCompare
 
-        // No Error
-        a < b
-    }
-    {
+            // No Error
+            a < b
+        }
+        {
 
-        type SafeId = symbol & { [brand]: 'SafeId' }
+            type SafeId = symbol & { [brand]: 'SafeId' }
 
-        const a = undefined as any as SafeId
-        const b = undefined as any as SafeId
+            const a = undefined as any as SafeId
+            const b = undefined as any as SafeId
 
-        // a < b; // TS2469: Operator '<' cannot be applied to type 'symbol'.
+            // a < b; // TS2469: Operator '<' cannot be applied to type 'symbol'.
+        }
+    },
+    nominal: () => {
+        type UserId = Nominal<'UserId', number>
+        type UserId2 = Nominal<'UserId2', number>
+        const userIdA: UserId = asNominal(123)
+        const userIdB: UserId = asNominal(456)
+        if (userIdA === userIdB) { throw [userIdA, userIdB] }
+        const to: (_: UserId) => number = asBase
+        const to2: (_: UserId2) => number = asBase
+        const userId2A: UserId2 = asNominal(123)
+        const userId2B: UserId2 = asNominal(456)
+        if (userId2A === userId2B) { throw [userId2A, userId2B] }
+        // if (userIdA === userId2A) { throw [userIdA, userId2A] } // compilation error
+        const n1: number = to(userIdA)
+        const n2: number = to2(userId2A)
+        // const x = to(userId2A) // compilation error
     }
 }
