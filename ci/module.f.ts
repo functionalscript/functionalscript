dce30a062c06e38ddab00762d08893c822a98b5b
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2025-11-03 13:04:55 -0800

    ok

--- ci/module.f.ts
+++ ci/module.f.ts
@@ -1,23 +1,27 @@
 import type { Io } from '../io/module.f.ts'
 
+const os = ['ubuntu', 'macos', 'windows'] as const
+
+type Os = typeof os[number]
+
 type GitHubAction = {
     on: {}
     jobs: {
         [jobs: string]: {
-            'runs-on': string
+            'runs-on': Os
             steps: readonly string[]
         }
     }
 }
 
+type Architecture = 'intel' | 'arm'
+
 const gha: GitHubAction = {
     on: {},
-    jobs: {
-        main: {
-            'runs-on': 'ubuntu-latest',
-            steps: []
-        },
-    },
+    jobs: Object.fromEntries(os.map(v => [v, {
+        'runs-on': v,
+        steps: [],
+    }])),
 }
 
 export default async(io: Io): Promise<number> => {
