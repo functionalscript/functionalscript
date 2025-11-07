346ba29e28d3a0d2a7be94963215f89b40fd5527
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2025-11-07 02:38:45 -0800

    ok

--- json/test.f.ts
+++ json/test.f.ts
@@ -1,4 +1,4 @@
-import * as json from './module.f.ts'
+import { setProperty, stringify } from './module.f.ts'
 import * as o from '../types/object/module.f.ts'
 const { sort } = o
 import * as f from '../types/function/module.f.ts'
@@ -6,41 +6,41 @@ const { identity } = f
 
 export default {
     setProperty: () => {
-        if (json.setProperty("Hello")([])({}) !== "Hello") { throw 'error' }
+        if (setProperty("Hello")([])({}) !== "Hello") { throw 'error' }
     },
     stringify: [
         {
             sort: () => {
-                const r = json.setProperty("Hello")(['a'])({})
-                const x = json.stringify(sort)(r)
+                const r = setProperty("Hello")(['a'])({})
+                const x = stringify(sort)(r)
                 if (x !== '{"a":"Hello"}') { throw x }
             },
             identity: () => {
-                const x = json.stringify(identity)(json.setProperty("Hello")(['a'])({}))
+                const x = stringify(identity)(setProperty("Hello")(['a'])({}))
                 if (x !== '{"a":"Hello"}') { throw x }
             },
         },
         {
             sort: () => {
-                const x = json.stringify(sort)(json.setProperty("Hello")(['a'])({ c: [], b: 12 }))
+                const x = stringify(sort)(setProperty("Hello")(['a'])({ c: [], b: 12 }))
                 if (x !== '{"a":"Hello","b":12,"c":[]}') { throw x }
             },
             identity: () => {
-                const x = json.stringify(identity)(json.setProperty("Hello")(['a'])({ c: [], b: 12 }))
+                const x = stringify(identity)(setProperty("Hello")(['a'])({ c: [], b: 12 }))
                 if (x !== '{"c":[],"b":12,"a":"Hello"}') { throw x }
             },
         },
         {
             sort: () => {
                 const _0 = { a: { y: [24] }, c: [], b: 12 }
-                const _1 = json.setProperty("Hello")(['a', 'x'])(_0)
-                const _2 = json.stringify(sort)(_1)
+                const _1 = setProperty("Hello")(['a', 'x'])(_0)
+                const _2 = stringify(sort)(_1)
                 if (_2 !== '{"a":{"x":"Hello","y":[24]},"b":12,"c":[]}') { throw _2 }
             },
             identity: () => {
                 const _0 = { a: { y: [24] }, c: [], b: 12 }
-                const _1 = json.setProperty("Hello")(['a', 'x'])(_0)
-                const _2 = json.stringify(identity)(_1)
+                const _1 = setProperty("Hello")(['a', 'x'])(_0)
+                const _2 = stringify(identity)(_1)
                 if (_2 !== '{"a":{"y":[24],"x":"Hello"},"c":[],"b":12}') { throw _2 }
             }
         }
