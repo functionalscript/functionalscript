87c3bef3df1722013ffe80ac3efc93a639267033
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2025-11-13 11:52:24 -0800

    to_result

--- nanvm-lib/src/vm/string_coercion.rs
+++ nanvm-lib/src/vm/string_coercion.rs
@@ -20,27 +20,25 @@ impl<A: IVm> Dispatch<A> for StringCoercion {
     type Result = Result<String16<A>, Any<A>>;
 
     fn nullish(self, v: Nullish) -> Self::Result {
-        match v {
+        to_result(match v {
             Nullish::Null => "null",
             Nullish::Undefined => "undefined",
-        }
-        .to_string16_result()
+        })
     }
 
     fn bool(self, v: bool) -> Self::Result {
-        match v {
+        to_result(match v {
             true => "true",
             false => "false",
-        }
-        .to_string16_result()
+        })
     }
 
     fn number(self, v: f64) -> Self::Result {
         match v {
-            f64::INFINITY => "Infinity".to_string16_result(),
-            f64::NEG_INFINITY => "-Infinity".to_string16_result(),
-            -0.0 => "0".to_string16_result(),
-            v => v.to_string().to_string16_result(),
+            f64::INFINITY => to_result("Infinity"),
+            f64::NEG_INFINITY => to_result("-Infinity"),
+            -0.0 => to_result("0"),
+            v => to_result(&v.to_string()),
         }
     }
 
@@ -50,12 +48,12 @@ impl<A: IVm> Dispatch<A> for StringCoercion {
 
     fn bigint(self, v: BigInt<A>) -> Self::Result {
         // TODO: we should use different algorithm for large numbers.
-        format!("{v:?}").to_string16_result()
+        to_result(&format!("{v:?}"))
     }
 
     fn object(self, _: Object<A>) -> Self::Result {
         // TODO: invoke user-defined methods Symbol.toPrimitive, toString, valueOf.
-        "[object Object]".to_string16_result()
+        to_result("[object Object]")
     }
 
     fn array(self, v: Array<A>) -> Self::Result {
@@ -64,16 +62,10 @@ impl<A: IVm> Dispatch<A> for StringCoercion {
 
     fn function(self, _: Function<A>) -> Self::Result {
         // TODO: invoke user-defined methods Symbol.toPrimitive, toString, valueOf.
-        "[object Function]".to_string16_result()
+        to_result("[object Function]")
     }
 }
 
-trait ToString16Result<A: IVm> {
-    fn to_string16_result(self) -> Result<String16<A>, Any<A>>;
-}
-
-impl<A: IVm> ToString16Result<A> for &str {
-    fn to_string16_result(self) -> Result<String16<A>, Any<A>> {
-        Ok(self.into())
-    }
+fn to_result<A: IVm>(s: &str) -> Result<String16<A>, Any<A>> {
+    Ok(s.into())
 }
