3566bac887b44e21647e13e9ebb88de106a82abd
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2025-11-22 01:07:03 -0800

    ok

--- crypto/rfc6979/test.f.ts
+++ crypto/rfc6979/test.f.ts
@@ -1,12 +1,15 @@
+import { utf8 } from "../../text/module.f.ts"
 import { vec } from "../../types/bit_vec/module.f.ts"
-import { all } from "./module.f.ts"
+import { curve, secp192r1 } from "../secp/module.f.ts"
+import { computeSync, sha224, sha256 } from "../sha2/module.f.ts"
+import { all, fromCurve, k } from "./module.f.ts"
 
-export default [
-    () => {
+export default {
+    bits2int: () => {
         if (all(7n).bits2int(vec(5n)(0b10100n)) !== 0b101n) { throw new Error("fail") }
         if (all(17n).bits2int(vec(3n)(0b101n)) !== 0b101n) { throw new Error("fail") }
     },
-    () => {
+    int2octets: () => {
         // 3 bit prime
         if (all(5n).int2octets(0b101n) !== vec(8n)(0b0000_0101n)) { throw new Error("fail") }
         // 5 bit prime
@@ -14,7 +17,29 @@ export default [
         // 15 bit prime
         if (all(16387n).int2octets(0x13n) !== vec(16n)(0x13n)) { throw new Error("fail") }
     },
-    () => {
+    bit2octets: () => {
         if (all(11n).bits2octets(vec(4n)(0b1101n)) !== vec(8n)(0b0000_0010n)) { throw new Error("fail") }
+    },
+    k: () => {
+        const q = 0x4000000000000000000020108A2E0CC0D99F8A5EFn
+        const { qlen, int2octets, bits2octets } = all(q)
+        if (qlen !== 163n) { throw qlen }
+        const x = 0x09A4D6792295A7F730FC3F2B49CBC0F62E862272Fn
+        const m = utf8("sample")
+        const h1 = computeSync(sha256)([m])
+        if (h1 !== vec(256n)(0xAF2BDBE1AA9B6EC1E2ADE1D694F41FC71A831D0268E9891562113D8A62ADD1BFn)) { throw h1 }
+        const xi2o = int2octets(x)
+        if (xi2o !== vec(168n)(0x009A4D6792295A7F730FC3F2B49CBC0F62E862272Fn)) { throw xi2o }
+        const h1b2o = bits2octets(h1)
+        if (h1b2o !== vec(168n)(0x01795EDF0D54DB760F156D0DAC04C0322B3A204224n)) { throw h1b2o }
     }
-]
+    /*
+    k: () => {
+        const a = fromCurve(curve(secp192r1))
+        const x = 0x6FAB034934E4C0FC9AE67F5B5659A9D7D1FEFD187EE09FD4n
+        const m = utf8("sample")
+        const kk = k(a)(sha224)(x)(m)
+        if (kk !== 0x4381526B3FC1E7128F202E194505592F01D5FF4C5AF015D8n) { throw kk }
+    }
+    */
+}
