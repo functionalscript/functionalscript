9aa3e3f4d1d060060c2de5f0b27175b355406117
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2026-01-08 17:59:14 -0800

    Update compile args handling

--- fjs/module.f.ts
+++ fjs/module.f.ts
@@ -2,24 +2,6 @@ import type { Io } from '../io/module.f.ts'
 import { compile } from '../djs/module.f.ts'
 import { main as testMain } from '../dev/tf/module.f.ts'
 
-const withArgv = (io: Io) => (argv: readonly string[]): Io => ({
-    ...io,
-    process: {
-        ...io.process,
-        argv: [...argv],
-    },
-})
-
-const runCompile = (io: Io) => (rest: readonly string[]): Promise<number> => {
-    const [nodePath, scriptPath] = io.process.argv
-    return compile(withArgv(io)([nodePath, scriptPath, ...rest]))
-}
-
-const runCasStub = (io: Io): Promise<number> => {
-    io.console.error('cas command is not implemented yet')
-    return Promise.resolve(1)
-}
-
 export const main = (io: Io): Promise<number> => {
     const [command, ...rest] = io.process.argv.slice(2)
     switch (command) {
@@ -28,10 +10,11 @@ export const main = (io: Io): Promise<number> => {
             return testMain(io)
         case 'compile':
         case 'c':
-            return runCompile(io)(rest)
+            return compile(io)(rest)
         case 'cas':
         case 's':
-            return runCasStub(io)
+            io.console.error('cas command is not implemented yet')
+            return Promise.resolve(1)
         case undefined:
             io.console.error('Error: command is required')
             return Promise.resolve(1)
