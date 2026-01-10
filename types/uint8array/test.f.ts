6e2d6608ab53ac3dd9b2f2a4f8365fecb7d0c8e1
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2026-01-10 03:38:00 -0800

    fix Codex merge bug

--- types/uint8array/test.f.ts
+++ types/uint8array/test.f.ts
@@ -1,12 +1,7 @@
 import { vec } from '../bit_vec/module.f.ts'
 import { toVec, fromVec } from './module.f.ts'
 
-    },
-    unalignedLength: () => {
-        const input = vec(4n)(0xFn)
-        const output = fromVec(input)
-        assertArrayEq(output, Uint8Array.from([0xF0]))
-const assertEq = (a: number, b: number) => {
+const assertEq = <T>(a: T, b: T) => {
     if (a !== b) { throw [a, b] }
 }
 
@@ -18,15 +13,21 @@ const assertArrayEq = (a: Uint8Array, b: Uint8Array) => {
 }
 
 export default {
-    roundTrip: () => {
-        const input = Uint8Array.from([0, 1, 2, 250, 255])
+    empty: () => {
+        const input = new Uint8Array()
         const vec = toVec(input)
         const output = fromVec(vec)
         assertArrayEq(output, input)
     },
-    empty: () => {
-        const input = new Uint8Array()
-        const output = fromVec(toVec(input))
+    roundTrip: () => {
+        const input = Uint8Array.from([0, 1, 2, 3, 255])
+        const vec = toVec(input)
+        const output = fromVec(vec)
         assertArrayEq(output, input)
+    },
+    unalignedLength: () => {
+        const input = vec(4n)(0xFn)
+        const output = fromVec(input)
+        assertArrayEq(output, Uint8Array.from([0xF0]))
     }
 }
