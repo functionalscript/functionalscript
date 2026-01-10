e579162501fafdaefbaf3044502c5cf9c59dc762
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2026-01-10 01:35:36 -0800

    test

--- types/nullable/test.f.ts
+++ types/nullable/test.f.ts
@@ -1,9 +1,17 @@
-import { map } from './module.f.ts'
+import { map, toOption } from './module.f.ts'
 
-export default () => {
-    const optionSq = map((v: number) => v * v)
-    const sq3 = optionSq(3)
-    if (sq3 !== 9) { throw sq3 }
-    const sqNull = optionSq(null)
-    if (sqNull !== null) { throw sqNull }
-}
+export default [
+    () => {
+        const optionSq = map((v: number) => v * v)
+        const sq3 = optionSq(3)
+        if (sq3 !== 9) { throw sq3 }
+        const sqNull = optionSq(null)
+        if (sqNull !== null) { throw sqNull }
+    },
+    () => {
+        const opt1 = toOption(5)
+        if (opt1.length !== 1 || opt1[0] !== 5) { throw opt1 }
+        const opt2 = toOption(null)
+        if (opt2.length !== 0) { throw opt2 }
+    }
+]
