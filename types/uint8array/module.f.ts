04835a4d0790ce2894d191e534e4e811483d1148
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2026-01-10 02:39:17 -0800

    Avoid temp arrays in uint8array conversions

--- types/uint8array/module.f.ts
+++ types/uint8array/module.f.ts
@@ -4,19 +4,27 @@
  * @module
  */
 import { msb, u8List, u8ListToVec, type Vec } from "../bit_vec/module.f.ts"
-import { toArray } from "../list/module.f.ts"
+import { iterable, type List } from "../list/module.f.ts"
 
 const u8ListToVecMsb = u8ListToVec(msb)
 const u8ListMsb = u8List(msb)
+
+const toList = (input: Uint8Array): List<number> => {
+    const step = (index: number): List<number> =>
+        index < input.length
+            ? { first: input[index], tail: () => step(index + 1) }
+            : null
+    return step(0)
+}
 
 /**
  * Converts a Uint8Array into an MSB-first bit vector.
  */
 export const toVec = (input: Uint8Array): Vec =>
-    u8ListToVecMsb(Array.from(input))
+    u8ListToVecMsb(toList(input))
 
 /**
  * Converts an MSB-first bit vector into a Uint8Array.
  */
 export const fromVec = (input: Vec): Uint8Array =>
-    Uint8Array.from(toArray(u8ListMsb(input)))
+    Uint8Array.from(iterable(u8ListMsb(input)))
