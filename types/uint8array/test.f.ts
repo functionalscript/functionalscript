d17b01371718c4ecc4e999923b4442f4f686b7de
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2026-01-10 07:48:34 -0800

    Add utf8 encode/decode tests

--- types/uint8array/test.f.ts
+++ types/uint8array/test.f.ts
@@ -1,5 +1,5 @@
 import { vec } from '../bit_vec/module.f.ts'
-import { toVec, fromVec } from './module.f.ts'
+import { toVec, fromVec, decodeUtf8, encodeUtf8 } from './module.f.ts'
 
 const assertEq = <T>(a: T, b: T) => {
     if (a !== b) { throw [a, b] }
@@ -29,5 +29,30 @@ export default {
         const input = vec(4n)(0xFn)
         const output = fromVec(input)
         assertArrayEq(output, Uint8Array.from([0xF0]))
+    },
+    encodeUtf8Empty: () => {
+        const output = encodeUtf8('')
+        assertArrayEq(output, new Uint8Array())
+    },
+    encodeUtf8Ascii: () => {
+        const output = encodeUtf8('Hi!')
+        assertArrayEq(output, Uint8Array.from([72, 105, 33]))
+    },
+    encodeUtf8Multibyte: () => {
+        const output = encodeUtf8('✓')
+        assertArrayEq(output, Uint8Array.from([0xE2, 0x9C, 0x93]))
+    },
+    decodeUtf8Ascii: () => {
+        const output = decodeUtf8(Uint8Array.from([102, 115]))
+        assertEq(output, 'fs')
+    },
+    decodeUtf8Multibyte: () => {
+        const output = decodeUtf8(Uint8Array.from([0xE2, 0x9C, 0x93]))
+        assertEq(output, '✓')
+    },
+    utf8RoundTrip: () => {
+        const input = 'FunctionalScript 🐝'
+        const output = decodeUtf8(encodeUtf8(input))
+        assertEq(output, input)
     }
 }
