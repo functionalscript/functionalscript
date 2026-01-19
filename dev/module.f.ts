5088dcdd8529f0cbc68289af6cd4cbdbfe7f80b3
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2026-01-19 01:21:59 -0600

    fix dir

--- dev/module.f.ts
+++ dev/module.f.ts
@@ -59,6 +59,7 @@ export const loadModuleMap = async (io: Io): Promise<ModuleMap> => {
     } = io
     let map: ModuleArray = []
     const initCwd = env(io)('INIT_CWD')
+    console.log(`INIT_CWD=${initCwd}`)
     const s = initCwd === undefined ? '.' : `${initCwd.replaceAll('\\', '/')}`
     for (const f of await allFiles(io)(s)) {
         if (f.endsWith('.f.js') ||
