52d9482ae054ca8b1a7e36bf84293750e5942fb7
Sasha Gil <16183908+sasha-gil@users.noreply.github.com>
2026-01-21 15:56:57 -0800

    Complete BigInt's Mul implementation.

--- nanvm-lib/src/vm/any/mul.rs
+++ nanvm-lib/src/vm/any/mul.rs
@@ -1,7 +1,7 @@
 use super::{Any, IVm};
 use std::ops::Mul;
 
-impl<A: IVm> Mul for Any<A> where <A as IVm>::InternalBigInt: AsRef<[u64]> {
+impl<A: IVm> Mul for Any<A> {
     type Output = Result<Any<A>, Any<A>>;
 
     fn mul(self, rhs: Self) -> Self::Output {
--- nanvm-lib/src/vm/bigint/mul.rs
+++ nanvm-lib/src/vm/bigint/mul.rs
@@ -1,40 +1,57 @@
-use crate::{/*sign::Sign,*/ vm::{BigInt, IVm}};
+use crate::{
+    common::{default::default, sized_index::SizedIndex},
+    sign::Sign,
+    vm::{BigInt, IContainer, IVm},
+};
 
 use std::ops::Mul;
 
-impl<A: IVm> Mul for BigInt<A>
-where
-    A::InternalBigInt: AsRef<[u64]>,
-{
+pub fn new_resize<T: Default + Clone>(size: usize) -> Vec<T> {
+    let mut vec = Vec::with_capacity(size);
+    vec.resize(size, default());
+    vec
+}
+
+pub fn normalize(vec: &mut Vec<u64>) {
+    while let Some(&0) = vec.last() {
+        vec.pop();
+    }
+}
+
+impl<A: IVm> Mul for BigInt<A> {
     type Output = Self;
 
-    fn mul(self, _rhs: Self) -> Self {
-        todo!()
-        // if self.is_zero() || _rhs.is_zero() {
-        //     return Self::new(Sign::Positive, vec![]);
-        // }
+    fn mul(self, rhs: Self) -> Self {
+        if self.is_zero() || rhs.is_zero() {
+            return Self::new(Sign::Positive, []);
+        }
 
-        // // use slices of the inner representation for multiplication
-        // let a = self.0.as_ref();
-        // let b = _rhs.0.as_ref();
+        let lhs_max = self.length() - 1;
+        let rhs_max = rhs.length() - 1;
 
-        // let lhs_max = a.len() - 1;
-        // let rhs_max = b.len() - 1;
-        // let total_max = a.len() + b.len() - 1;
-        // let mut value = new_resize(total_max + 1);
-        // let mut i: usize = 0;
-        // while i < total_max {
-        //     let mut j = i.saturating_sub(rhs_max);
-        //     let max = if i < lhs_max { i } else { lhs_max };
-        //     while j <= max {
-        //         value = add_to_vec(value, i, a[j] as u128 * b[i - j] as u128);
-        //         j += 1;
-        //     }
-        //     i += 1;
-        // }
+        // TODO: 1. Implement Karatsuba multiplication for large numbers.
+        //       2. For 'value', reate mutable BigInt instead of Vec<u64>.
+        //       3. Ask self, _rhs for mutable access and operate in-place on one of them (if available).
+        let total_max = lhs_max + rhs_max + 1;
+        let mut value = new_resize((total_max + 1) as usize);
+        let mut i: u32 = 0;
+        while i < total_max {
+            let mut j = i.saturating_sub(rhs_max);
+            let max = if i < lhs_max { i } else { lhs_max };
+            while j <= max {
+                value = Self::add_to_vec(value, i, self[j] as u128 * rhs[i - j] as u128);
+                j += 1;
+            }
+            i += 1;
+        }
+        normalize(&mut value);
 
-        // let mut result = Self(value);
-        // result.normalize();
-        // result
+        let sign = if self.0.header() == rhs.0.header() {
+            Sign::Positive
+        } else {
+            Sign::Negative
+        };
+
+        Self::new(sign, value)
     }
 }
