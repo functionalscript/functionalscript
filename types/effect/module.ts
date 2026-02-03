ceb4ae421ef1027a3e3a3febc92545e45e3b0dea
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2026-02-02 21:17:42 -0800

    tsc

--- types/effect/module.ts
+++ types/effect/module.ts
@@ -1,9 +1,21 @@
 import { promises } from 'fs'
-import { type NodeOperationMap } from './module.f.ts'
+import { type NodeEffect, type NodeOperationMap } from './module.f.ts'
 
 const { readFile } = promises
 
 const nodeOperationMap: NodeOperationMap = {
     'console.log': async (message: string): Promise<void> => console.log(message),
     'fs.promises.readFile': readFile,
+}
+
+export const run = async<T>(effect: NodeEffect<T>): Promise<T> => {
+    while (true) {
+        if (effect[0] === 'pure') {
+            return effect[1]
+        }
+        const [, command, payload, continuation] = effect
+        const operation = nodeOperationMap[command]
+        const result = await operation(payload)
+        effect = continuation(result as any)
+    }
 }
