9e9612e6c9fc3bef169f94a24b094a853b77260c
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2026-02-03 22:37:41 -0800

    readFile and writeFile are using Vec instead of Uint8Array (#651)
    * website in deno
    
    * readFile, writeFile

--- types/effect/node/module.ts
+++ types/effect/node/module.ts
@@ -2,16 +2,14 @@ import { promises } from 'fs'
 import type { NodeEffect, NodeOperationMap } from './module.f.ts'
 import { run } from '../module.f.ts'
 import type { Vec } from '../../bit_vec/module.f.ts'
-import { toVec } from '../../uint8array/module.f.ts'
+import { fromVec, toVec } from '../../uint8array/module.f.ts'
 
-const { readFile } = promises
+const { readFile, writeFile } = promises
 
 const nodeOperationMap: NodeOperationMap = {
     log: async (message: string): Promise<void> => console.log(message),
-    readFile: async(path: string): Promise<Vec> => {
-        const buffer = await readFile(path)
-        return toVec(buffer)
-    }
+    readFile: async(path: string): Promise<Vec> => toVec(await readFile(path)),
+    writeFile: ([path, data]: readonly[string, Vec]): Promise<void> => writeFile(path, fromVec(data))
 }
 
 export type NodeRun = <T>(effect: NodeEffect<T>) => Promise<T>
