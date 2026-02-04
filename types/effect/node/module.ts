b07a86fa95f82148f975522e6551dad5ff750d0b
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2026-02-04 04:47:12 -0800

    nodeRun type

--- types/effect/node/module.ts
+++ types/effect/node/module.ts
@@ -1,5 +1,5 @@
 import { promises } from 'fs'
-import type { NodeOperationMap } from './module.f.ts'
+import type { NodeEffect, NodeOperationMap } from './module.f.ts'
 import type { Vec } from '../../bit_vec/module.f.ts'
 import { fromVec, toVec } from '../../uint8array/module.f.ts'
 import { asyncRun } from '../module.ts'
@@ -12,4 +12,4 @@ const nodeOperationMap: NodeOperationMap = {
     writeFile: ([path, data]: readonly[string, Vec]): Promise<void> => writeFile(path, fromVec(data))
 }
 
-export const nodeRun = asyncRun(nodeOperationMap)
+export const nodeRun: (effect: NodeEffect<number>) => Promise<number> = asyncRun(nodeOperationMap)
