4c1d143db0975e7ba1dad3b4be11ab738c877a57
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2026-02-03 20:46:09 -0800

    a

--- website/module.f.ts
+++ website/module.f.ts
@@ -3,7 +3,9 @@ import { encodeUtf8 } from '../types/uint8array/module.f.ts'
 import { htmlToString, type Element } from '../html/module.f.ts'
 
 export const run = async({ fs: { promises: { writeFile }} }: Io): Promise<number> => {
-    const html: Element = ['body',['h1', 'FunctionalScript']]
+    const html: Element = ['body',
+        ['a', { href: 'https://github.com/functionalscript/functionalscript' }, 'GitHub Repository']
+    ]
     await writeFile('index.html', encodeUtf8(htmlToString(html)))
     return 0
 }
