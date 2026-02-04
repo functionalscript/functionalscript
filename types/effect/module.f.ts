2b43d1cfba7f5490f7e0a4ec43a3ded16cd57881
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2026-02-04 05:38:01 -0800

    ok

--- types/effect/module.f.ts
+++ types/effect/module.f.ts
@@ -6,6 +6,8 @@ export type Effect<O extends Operations, T> = Pure<T> | Do<O, T>
 
 export type Pure<T> = readonly['pure', T]
 
+export const pure = <T>(value: T): Pure<T> => ['pure', value]
+
 export type DoOperation<O extends Operations, T, K extends keyof O> =
     readonly['do', K, O[K][0], (input: O[K][1]) => Effect<O, T>]
 
--- types/effect/node/module.f.ts
+++ types/effect/node/module.f.ts
@@ -1,5 +1,5 @@
 import type { Vec } from "../../bit_vec/module.f.ts"
-import type { Effect, ToAsyncOperationMap } from "../module.f.ts"
+import type { Do, Effect, ToAsyncOperationMap } from "../module.f.ts"
 
 export type NodeOperations = {
     readonly log: readonly[string, void]
@@ -9,6 +9,17 @@ export type NodeOperations = {
 
 export type NodeEffect<T> = Effect<NodeOperations, T>
 
+export type NodeDo<T> = Do<NodeOperations, T>
+
 export type NodeOperationMap = ToAsyncOperationMap<NodeOperations>
 
 export type NodeProgram = (argv: readonly string[]) => NodeEffect<number>
+
+export const log = (message: string) => <T>(c: () => NodeEffect<T>): NodeDo<T> =>
+    ['do', 'log', message, c]
+
+export const readFile = (path: string) => <T>(c: (data: Vec) => NodeEffect<T>): NodeDo<T> =>
+    ['do', 'readFile', path, c]
+
+export const writeFile = (path: string, data: Vec) => <T>(c: () => NodeEffect<T>): NodeDo<T> =>
+    ['do', 'writeFile', [path, data], c]
