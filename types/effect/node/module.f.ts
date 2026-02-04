8607679376851d513c04bfb38e15a1bd1977db89
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2026-02-04 13:56:39 -0800

    ok

--- types/effect/node/module.f.ts
+++ types/effect/node/module.f.ts
@@ -1,11 +1,21 @@
 import type { Vec } from "../../bit_vec/module.f.ts"
+import type { Result } from "../../result/module.f.ts"
 import type { Effect, ToAsyncOperationMap } from "../module.f.ts"
 
-export type NodeOperations = {
-    readonly log: readonly[string, void]
-    readonly readFile: readonly[string, Vec]
-    readonly writeFile: readonly[readonly[string, Vec], void]
+export type IoResult<T> = Result<T, unknown>
+
+export type FileOperations = {
+    readonly readFile: readonly[string, IoResult<Vec>]
+    readonly writeFile: readonly[readonly[string, Vec], IoResult<void>]
 }
+
+export type ConsoleOperations = {
+    readonly log: readonly[string, void]
+}
+
+export type NodeOperations =
+    & ConsoleOperations
+    & FileOperations
 
 export type NodeEffect<T> = Effect<NodeOperations, T>
 
--- types/effect/node/module.ts
+++ types/effect/node/module.ts
@@ -1,16 +1,28 @@
 import { promises } from 'node:fs'
 import { argv } from 'node:process'
-import type { NodeOperationMap, NodeProgram } from './module.f.ts'
+import type { IoResult, NodeOperationMap, NodeProgram } from './module.f.ts'
 import type { Vec } from '../../bit_vec/module.f.ts'
 import { fromVec, toVec } from '../../uint8array/module.f.ts'
 import { asyncRun } from '../module.ts'
 
 const { readFile, writeFile } = promises
 
+const tc = async<T>(f: () => Promise<T>): Promise<IoResult<T>> => {
+    try {
+        const r = await f()
+        return ['ok', r]
+    } catch (e) {
+        return ['error', e]
+    }
+}
+
 const nodeOperationMap: NodeOperationMap = {
-    log: async (message: string): Promise<void> => console.log(message),
-    readFile: async(path: string): Promise<Vec> => toVec(await readFile(path)),
-    writeFile: ([path, data]: readonly[string, Vec]): Promise<void> => writeFile(path, fromVec(data))
+    log: async (message: string): Promise<void> =>
+        console.log(message),
+    readFile: (path: string): Promise<IoResult<Vec>> =>
+        tc(async() => toVec(await readFile(path))),
+    writeFile: ([path, data]: readonly[string, Vec]): Promise<IoResult<void>> =>
+        tc(() => writeFile(path, fromVec(data)))
 }
 
 const nr = asyncRun(nodeOperationMap)
