b96bf702c1b72308566d9b6f3df75b1215a730d9
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2026-02-05 13:05:51 -0800

    Effect: Bind (#656)
    * bind
    
    * bind
    
    * changelog
    
    * narrow the type
    
    * narrow
    
    * fmt

--- types/effect/module.f.ts
+++ types/effect/module.f.ts
@@ -1,27 +1,48 @@
 export type Operations = {
-    readonly[command in string]: readonly[input: unknown, output: unknown]
+    readonly [command in string]: readonly [input: unknown, output: unknown]
 }
 
 export type Effect<O extends Operations, T> = Pure<T> | Do<O, T>
 
-export type Pure<T> = readonly['pure', T]
+export type Pure<T> = readonly ['pure', T]
 
 export const pure = <T>(value: T): Pure<T> => ['pure', value]
 
 export type One<O extends Operations, T, K extends keyof O & string> =
-    readonly['do', K, O[K][0], (input: O[K][1]) => Effect<O, T>]
+    readonly ['do', K, O[K][0], (input: O[K][1]) => Effect<O, T>]
 
 export type Do<O extends Operations, T> = {
-    readonly[K in keyof O & string]: One<O, T, K>
+    readonly [K in keyof O & string]: One<O, T, K>
 }[keyof O & string]
 
-export const do_ = <O extends Operations, T, K extends keyof O & string>(
+export const do_ = <O extends Operations, K extends keyof O & string>(
     cmd: K,
-    payload: O[K][0],
-    k: (input: O[K][1]) => Effect<O, T>
-): Do<O, T> =>
-    ['do', cmd, payload, k]
+    payload: O[K][0]
+): Do<O, O[K][1]> =>
+    ['do', cmd, payload, pure]
 
 export type ToAsyncOperationMap<O extends Operations> = {
-    readonly[K in keyof O]: (payload: O[K][0]) => Promise<O[K][1]>
+    readonly [K in keyof O]: (payload: O[K][0]) => Promise<O[K][1]>
+}
+
+export const bind = <O extends Operations, M, R>(
+    a: Effect<O, M>,
+    f: (x: M) => Effect<O, R>
+): Effect<O, R> => {
+    if (f === pure) {
+        return a as Effect<O, R> // M == R
+    }
+    if (a[0] === 'pure') {
+        return f(a[1])
+    }
+    const [, cmd, payload, cont] = a
+    type T = O[typeof cmd][1]
+    return [
+        'do',
+        cmd,
+        payload,
+        cont === pure
+            ? f as (x: T) => Effect<O, R> // M == T
+            : x => bind(cont(x), f)
+    ]
 }
