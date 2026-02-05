9a2ba227fd80ea73bdb9759cc9f5f330a186b802
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2026-02-05 13:32:55 -0800

    doFull

--- types/effect/module.f.ts
+++ types/effect/module.f.ts
@@ -17,11 +17,18 @@ export type Do<O extends Operations, T> = {
     readonly do: { readonly [K in keyof O & string]: One<O, T, K> }[keyof O & string]
 }
 
+const doFull = <O extends Operations, K extends keyof O & string, T>(
+    cmd: K,
+    payload: O[K][0],
+    f: (input: O[K][1]) => Effect<O, T>
+): Do<O, O[K][1]> =>
+    ({ do:  [cmd, payload, f] })
+
 export const do_ = <O extends Operations, K extends keyof O & string>(
     cmd: K,
     payload: O[K][0]
 ): Do<O, O[K][1]> =>
-    ({ do:  [cmd, payload, pure] })
+    doFull(cmd, payload, pure)
 
 export const bind = <O extends Operations, M, R>(
     a: Effect<O, M>,
@@ -45,47 +52,6 @@ export const bind = <O extends Operations, M, R>(
         ]
     }
 }
-
-/*
-export type Pure<T> = readonly ['pure', T]
-
-export const pure = <T>(value: T): Pure<T> => ['pure', value]
-
-export type One<O extends Operations, T, K extends keyof O & string> =
-    readonly ['do', K, O[K][0], (input: O[K][1]) => Effect<O, T>]
-
-export type Do<O extends Operations, T> = {
-    readonly [K in keyof O & string]: One<O, T, K>
-}[keyof O & string]
-
-export const do_ = <O extends Operations, K extends keyof O & string>(
-    cmd: K,
-    payload: O[K][0]
-): Do<O, O[K][1]> =>
-    ['do', cmd, payload, pure]
-
-export const bind = <O extends Operations, M, R>(
-    a: Effect<O, M>,
-    f: (x: M) => Effect<O, R>
-): Effect<O, R> => {
-    if (f === pure) {
-        return a as Effect<O, R> // M == R
-    }
-    if (a[0] === 'pure') {
-        return f(a[1])
-    }
-    const [, cmd, payload, cont] = a
-    type T = O[typeof cmd][1]
-    return [
-        'do',
-        cmd,
-        payload,
-        cont === pure
-            ? f as (x: T) => Effect<O, R> // M == T
-            : x => bind(cont(x), f)
-    ]
-}
-*/
 
 export type ToAsyncOperationMap<O extends Operations> = {
     readonly [K in keyof O]: (payload: O[K][0]) => Promise<O[K][1]>
