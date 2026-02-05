8e9452f793694fc1b3babe7408ca5377138a3fb7
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2026-02-05 12:19:18 -0800

    narrow

--- types/effect/module.f.ts
+++ types/effect/module.f.ts
@@ -1,18 +1,18 @@
 export type Operations = {
-    readonly[command in string]: readonly[input: unknown, output: unknown]
+    readonly [command in string]: readonly [input: unknown, output: unknown]
 }
 
 export type Effect<O extends Operations, T> = Pure<T> | Do<O, T>
 
-export type Pure<T> = readonly['pure', T]
+export type Pure<T> = readonly ['pure', T]
 
 export const pure = <T>(value: T): Pure<T> => ['pure', value]
 
 export type One<O extends Operations, T, K extends keyof O & string> =
-    readonly['do', K, O[K][0], (input: O[K][1]) => Effect<O, T>]
+    readonly ['do', K, O[K][0], (input: O[K][1]) => Effect<O, T>]
 
 export type Do<O extends Operations, T> = {
-    readonly[K in keyof O & string]: One<O, T, K>
+    readonly [K in keyof O & string]: One<O, T, K>
 }[keyof O & string]
 
 export const do_ = <O extends Operations, K extends keyof O & string>(
@@ -22,7 +22,7 @@ export const do_ = <O extends Operations, K extends keyof O & string>(
     ['do', cmd, payload, pure]
 
 export type ToAsyncOperationMap<O extends Operations> = {
-    readonly[K in keyof O]: (payload: O[K][0]) => Promise<O[K][1]>
+    readonly [K in keyof O]: (payload: O[K][0]) => Promise<O[K][1]>
 }
 
 export const bind = <O extends Operations, M, R>(a: Effect<O, M>, f: (x: M) => Effect<O, R>): Effect<O, R> => {
@@ -40,5 +40,6 @@ export const bind = <O extends Operations, M, R>(a: Effect<O, M>, f: (x: M) => E
         payload,
         cont === pure
             ? f as (x: T) => Effect<O, R> // M == T
-            : x => bind(cont(x), f)]
+            : x => bind(cont(x), f)
+    ]
 }
