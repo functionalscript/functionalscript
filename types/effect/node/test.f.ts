52163d3547a986721268b83823a6b429e9ac6423
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2026-02-05 19:11:19 -0800

    test

--- types/effect/node/test.f.ts
+++ types/effect/node/test.f.ts
@@ -1,7 +1,23 @@
+import { uint, vec8 } from "../../bit_vec/module.f.ts"
 import { readFile } from "./module.f.ts"
 
 export default {
     map: () => {
-        const e = readFile('hello').map(a => a)
+        let e = readFile('hello').map(([k, v]) => {
+            if (k === 'error') { throw v }
+            return uint(v) * 2n
+        })
+        //
+        while (true) {
+            if ('pure' in e) {
+                const result = e.pure
+                if (result !== 0x2An) { throw result }
+                return
+            }
+            const [cmd, p, cont] = e.do
+            if (cmd !== 'readFile') { throw cmd }
+            if (p !== 'hello') { throw p }
+            e = cont(['ok', vec8(0x15n)])
+        }
     }
 }
