0f0d171f3ac38f91da6b0be081ac9504c9965ba6
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2026-02-13 02:01:56 -0800

    Minor clean-up (#667)
    * minor
    
    * clean modules
    
    * program

--- website/module.f.ts
+++ website/module.f.ts
@@ -1,12 +1,12 @@
 import { htmlToString, type Element } from '../html/module.f.ts'
-import { type NodeOperations, writeFile, type NodeProgram } from '../types/effect/node/module.f.ts'
+import { type NodeEffect, type NodeOperations, writeFile } from '../types/effect/node/module.f.ts'
 import { utf8 } from '../text/module.f.ts'
 
 const html: Element = ['body',
     ['a', { href: 'https://github.com/functionalscript/functionalscript' }, 'GitHub Repository']
 ]
 
-const program = writeFile<NodeOperations>('index.html', utf8(htmlToString(html)))
+const program: NodeEffect<number> = writeFile<NodeOperations>('index.html', utf8(htmlToString(html)))
     .map(() => 0)
 
 export default () => program
