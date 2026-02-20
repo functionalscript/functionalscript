63579b7d4fd187da310761e65aa4ae6b00b875c1
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2026-02-19 20:21:14 -0800

    ok

--- types/asn.1/test.f.ts
+++ types/asn.1/test.f.ts
@@ -17,19 +17,36 @@ export default {
         if (intLen !== 8n) { throw intLen }
         if (uint !== 0x13n) { throw uint }
     },
-    encodeBig: () => {
-        const v = vec(0x7Fn << 3n)(0x1234n)
+    encode7F: () => {
+        const valueLen = 0x7Fn << 3n
+        const value = 0x1234n
+        const v = vec(valueLen)(value)
         const x = encode(integer, v)
         const lx = length(x)
-        if (lx !== 129n << 3n) { throw lx }
-        /*
+        if (lx !== 0x81n << 3n) { throw lx }
         const [tag, x1] = pop8(x)
         if (tag !== BigInt(integer)) { throw tag }
         const [len, x2] = pop8(x1)
-        if (len !== 1n) { throw len }
+        if (len !== 0x7Fn) { throw len }
         const { length: intLen, uint } = unpack(x2)
-        if (intLen !== 8n) { throw intLen }
-        if (uint !== 0x13n) { throw uint }
-        */
-    }
+        if (intLen !== valueLen) { throw intLen }
+        if (uint !== value) { throw uint }
+    },
+    encode80: () => {
+        const valueLen = 0x80n << 3n
+        const value = 0x123456n
+        const v = vec(valueLen)(value)
+        const x = encode(integer, v)
+        const lx = length(x)
+        if (lx !== 0x83n << 3n) { throw lx }
+        const [tag, x1] = pop8(x)
+        if (tag !== BigInt(integer)) { throw tag }
+        const [lenLen, x2] = pop8(x1)
+        if (lenLen !== 0x81n) { throw lenLen }
+        const [len, x3] = pop8(x2)
+        if (len !== 0x80n) { throw len }
+        const { length: intLen, uint } = unpack(x3)
+        if (intLen !== valueLen) { throw intLen }
+        if (uint !== value) { throw uint }
+    },
 }
