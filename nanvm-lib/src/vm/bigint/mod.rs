d226fc16db29e39d722ca83a1301411cce99cbe4
Sasha Gil <alexgil@microsoft.com>
2026-03-03 18:21:25 -0800

    Use Copilot implementations

--- nanvm-lib/src/vm/bigint/mod.rs
+++ nanvm-lib/src/vm/bigint/mod.rs
@@ -58,14 +58,98 @@ impl<A: IVm> BigInt<A> {
     // NOTE: use .index_iter in abs_* helpers.
 
     fn abs_cmp_vec(self, _rhs: Self) -> Ordering {
-        todo!();
+        let a: Vec<u64> = self.index_iter().collect();
+        let b: Vec<u64> = _rhs.index_iter().collect();
+        let la = a.len();
+        let lb = b.len();
+        if la != lb {
+            return la.cmp(&lb);
+        }
+        for i in (0..la).rev() {
+            if a[i] != b[i] {
+                return if a[i] > b[i] {
+                    Ordering::Greater
+                } else {
+                    Ordering::Less
+                };
+            }
+        }
+        Ordering::Equal
     }
 
     fn abs_add_vec(self, _rhs: Self) -> Vec<u64> {
-        todo!();
+        let mut iter_a = self.index_iter();
+        let mut iter_b = _rhs.index_iter();
+        let mut carry: u128 = 0;
+        let mut out: Vec<u64> = Vec::new();
+        loop {
+            match (iter_a.next(), iter_b.next()) {
+                (Some(a), Some(b)) => {
+                    let sum = a as u128 + b as u128 + carry;
+                    out.push(sum as u64);
+                    carry = sum >> 64;
+                }
+                (Some(a), None) => {
+                    let mut s = a as u128 + carry;
+                    out.push(s as u64);
+                    carry = s >> 64;
+                    for aa in iter_a {
+                        s = aa as u128 + carry;
+                        out.push(s as u64);
+                        carry = s >> 64;
+                    }
+                    break;
+                }
+                (None, Some(b)) => {
+                    let mut s = b as u128 + carry;
+                    out.push(s as u64);
+                    carry = s >> 64;
+                    for bb in iter_b {
+                        s = bb as u128 + carry;
+                        out.push(s as u64);
+                        carry = s >> 64;
+                    }
+                    break;
+                }
+                (None, None) => break,
+            }
+        }
+        if carry > 0 {
+            out.push(carry as u64);
+        }
+        out
     }
 
     fn abs_sub_vec(self, _rhs: Self) -> Vec<u64> {
-        todo!();
+        let mut iter_a = self.index_iter();
+        let mut iter_b = _rhs.index_iter();
+        let mut borrow: u128 = 0;
+        let mut out: Vec<u64> = Vec::new();
+        loop {
+            let a_opt = iter_a.next();
+            let b_opt = iter_b.next();
+            if a_opt.is_none() && b_opt.is_none() {
+                break;
+            }
+            let a = a_opt.unwrap_or(0u64);
+            let b = b_opt.unwrap_or(0u64);
+            let subtrahend = b as u128 + borrow;
+            if (a as u128) >= subtrahend {
+                out.push((a as u128 - subtrahend) as u64);
+                borrow = 0;
+            } else {
+                out.push(((1u128 << 64) + a as u128 - subtrahend) as u64);
+                borrow = 1;
+            }
+        }
+        // Trim leading zeros (most-significant words)
+        while let Some(&last) = out.last() {
+            if last == 0 {
+                out.pop();
+            } else {
+                break;
+            }
+        }
+        out
     }
 }
