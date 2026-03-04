4d6fe3fbfc13e1acfa135cda996e35ef998c43a2
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2026-03-03 20:51:03 -0800

    Re-add module documentation comment

--- bnf/module.f.ts
+++ bnf/module.f.ts
@@ -1,9 +1,10 @@
 /**
- * @module
  * Core BNF grammar primitives and helpers for describing parser rules.
  *
  * The module provides terminal-range encoding utilities, rule composition
  * types, and set/range helpers used by FunctionalScript grammar definitions.
+ *
+ * @module
  */
 import { codePointListToString, stringToCodePointList } from '../text/utf16/module.f.ts'
 import { type Array2, isArray2 } from '../types/array/module.f.ts'
