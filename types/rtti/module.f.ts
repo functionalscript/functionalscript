de53e5f38857346cbac560d5bd48328f3dd72216
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2026-03-19 19:01:06 +0000

    edit

--- types/rtti/module.f.ts
+++ types/rtti/module.f.ts
@@ -21,7 +21,23 @@ export type LazyType = () => Type
 
 export type Type = BaseType | ObjectType | LazyType
 
+type BaseTs<T extends BaseType> =
+    T extends 'undefined' ? undefined :
+    T extends 'boolean' ? boolean :
+    T extends 'string' ? string :
+    T extends 'number' ? number :
+    T extends 'bigint' ? bigint :
+    T extends 'null' ? null :
+    T extends 'array' ? readonly unknown[] :
+    T extends 'object' ? object :
+    T extends 'function' ? Function :
+    never
+
 /**
  * Converts to TypeScript type.
  */
 export type Ts<T extends Type> =
+    T extends BaseType ? BaseTs<T> :
+    T extends LazyType ? Ts<ReturnType<T>> :
+    T extends ObjectType ? { readonly [K in keyof T]: Ts<T[K]> } :
+    never
