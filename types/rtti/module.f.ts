9adb98be77cdcfa045f10ac67e2b91cccebcab42
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2026-03-19 20:01:29 +0000

    ok

--- types/rtti/module.f.ts
+++ types/rtti/module.f.ts
@@ -1,4 +1,5 @@
-import type { Result } from "../result/module.f.ts"
+import { todo } from '../../dev/module.f.ts'
+import { ok, error, type Result } from "../result/module.f.ts"
 
 export type BaseType =
     | 'undefined'
@@ -40,4 +41,17 @@ export type Ts<T extends Type> =
     T extends RecordType ? { readonly [K in keyof T]: Ts<T[K]> } :
     never
 
-export const validate = <T extends Type>(rtti: T) => (value: unknown): Result<Ts<T>, string> =>
+const baseValidate = <T extends BaseType>(rtti: T) => (value: unknown): Result<BaseTs<T>, string> => {
+
+}
+
+export const validate: <T extends Type>(rtti: T) => (value: unknown) => Result<Ts<T>, string> = rtti => {
+    switch (typeof rtti) {
+        case 'function':
+            return validate(rtti()) as any // TS goes into infinte loop
+        case 'string':
+            return baseValidate(rtti)
+        default:
+            return todo()
+    }
+}
