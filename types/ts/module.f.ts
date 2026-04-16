a6898713b56346b583e42b4ac898ae8284bfcffb
sergey-shandar <sergey-shandar@users.noreply.github.com>
2026-04-15 21:31:13 -0700

    complex

--- types/ts/module.f.ts
+++ types/ts/module.f.ts
@@ -5,23 +5,31 @@ export type Equal<A, B> =
 
 export type Assert<T extends true> = T
 
-export const tuple = (types: readonly string[]): string => `readonly[${types.join(',')}]`
+const complex = (open: string, close: string) => (i: readonly string[]) =>
+    `${open}${i.join(',')}${close}`
+
+export const tuple: (types: readonly string[]) => string =
+    complex('readonly[', ']')
+
+const structX = complex('{', '}')
 
 export const struct = (fields: readonly (readonly[string, string])[]): string =>
-    `{${fields.map(([k, v]) => `readonly${JSON.stringify(k)}:${v}`).join(',')}}`
+    structX(fields.map(([k, v]) => `readonly${JSON.stringify(k)}:${v}`))
 
-export const array = (type: string): string => `readonly(${type})[]`
+export const array = (type: string): string =>
+    `readonly(${type})[]`
 
-export const record = (type: string): string => `{readonly[k in string]:${type}}`
+export const record = (type: string): string =>
+    structX([`readonly[k in string]:${type}`])
 
 export const primitive = (c: bigint|string|undefined|boolean|number|null): string => {
     if (c === null) { return 'null' }
     switch (typeof c) {
         case 'bigint': return `${c}n`
         case 'string': return JSON.stringify(c)
+        case 'number': return isFinite(c) ? String(c) : 'number'
         case 'undefined':
         case 'boolean': return String(c)
-        case 'number': return isFinite(c) ? String(c) : 'number'
     }
 }
 
