c9a4678cecc9c2783beba1a8a878a9f3750490e3
sergey-shandar <sergey-shandar@users.noreply.github.com>
2026-04-30 05:37:17 -0700

    ok

--- types/mpt/module.f.ts
+++ types/mpt/module.f.ts
@@ -1,6 +1,5 @@
-import { todo } from "../../dev/module.f.ts"
-
-export type State = undefined
+import { todo } from '../../dev/module.f.ts'
+import type { StateScan } from '../function/operator/module.f.ts'
 
 /**
  * [left, right, hash]
@@ -9,6 +8,14 @@ export type Node = readonly[bigint, bigint, bigint]
 
 export type Compress = (a: bigint, b: bigint) => bigint
 
-const mpt = (compress: Compress) => (state: State) => (u: bigint): readonly[readonly Node[], State] => {
-    return todo()
-}
+export type Candidate = readonly[bigint, bigint]
+
+export type State = readonly Candidate[]
+
+/**
+ * Builds a left-fold Merkle chain over a stream of symbols.
+ * Each symbol after the first emits one completed {@link Node}.
+ * The final `State` is the root hash of the whole sequence.
+ */
+export const mpt = (compress: Compress): StateScan<bigint, State, readonly Node[]> =>
+    todo()
