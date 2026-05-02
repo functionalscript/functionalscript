fa3cccc652404740f1383a0804d4ed4fbe87b348
sergey-shandar <sergey-shandar@users.noreply.github.com>
2026-05-01 17:27:50 -0700

    encode

--- types/sul/level/hash/module.f.ts
+++ types/sul/level/hash/module.f.ts
@@ -1,10 +1,8 @@
-import type { StateScan } from '../../../function/operator/module.f.ts'
+import { todo } from '../../../../dev/module.f.ts'
 import type { State } from '../../../patricia_trie/module.f.ts'
 
-/*
-export type EncodeState = State<bigint>
+export type EncodeState<S> = State<S, bigint>
 
-export const encode = <S>(storage: S): StateScan<bigint, EncodeState, > => {
-
+export const encode = <S>(symbol: bigint, state: EncodeState<S>): readonly[bigint|undefined, EncodeState<S>] => {
+    return todo()
 }
-*/
