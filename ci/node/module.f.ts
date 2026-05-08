478a9e9891da39c6d2b90a1b6f2544dbabb6eba1
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2026-05-08 16:41:23 +0000

    cache

--- ci/node/module.f.ts
+++ ci/node/module.f.ts
@@ -1,12 +1,12 @@
-import { node, playwright } from '../config/module.f.ts'
-import { type Job, type Jobs, type MetaStep, type Os, clean, install, test, ubuntu } from '../common/module.f.ts'
+import { node } from '../config/module.f.ts'
+import { type Jobs, type MetaStep, type Os, clean, install, test, ubuntu } from '../common/module.f.ts'
 
 export const major = (v: string) => v.split('.')[0]
 
 const installNode = (version: string) =>
     ({ uses: 'actions/setup-node@v6', with: { 'node-version': version } })
 
-const basicNode = (version: string) => (extra: readonly MetaStep[]): readonly MetaStep[] => clean([
+export const basicNode = (version: string) => (extra: readonly MetaStep[]): readonly MetaStep[] => clean([
     install(installNode(version)),
     test({ run: 'npm ci' }),
     ...extra,
@@ -19,8 +19,6 @@ export const nodeTests = (version: string) => (extra: readonly MetaStep[]): read
 ])
 
 const findTgz = (v: Os) => v === 'windows' ? '(Get-ChildItem *.tgz).FullName' : './*.tgz'
-
-const playwrightAndVersion = `playwright@${playwright}`
 
 const nodeTest = (v: string) => major(v) === '20' ? 'run test20' : 'test'
 
@@ -35,15 +33,6 @@ export const nodeVersions: Jobs = Object.fromEntries(node.others.map(v => [
     ubuntu(nodeSteps(v))
 ]))
 
-// Playwright installation is stuck on Node 26 (May 7 2026) so we use Node 24.
-export const playwrightJob: Job = ubuntu(basicNode(node.others.at(-1)!)([
-    install({ run: `npm install -g ${playwrightAndVersion}` }),
-    install({ run: 'playwright install --with-deps' }),
-    // we have to use `npx` to make sure that we respect `@playwright/test` version from
-    // the `package.json`.
-    ...['chromium', 'firefox', 'webkit'].map(browser =>
-        test({ run: `npx playwright test --browser=${browser}` })),
-]))
 
 export const nodeMainSteps = (v: Os): readonly MetaStep[] => nodeTests(node.default)([
     // TypeScript Preview
