7d562db2dcb69bde653ceab306e8326cc916127a
sergey-shandar <sergey-shandar@users.noreply.github.com>
2026-05-10 20:17:44 -0700

    cas

--- fjs/module.f.ts
+++ fjs/module.f.ts
@@ -6,7 +6,7 @@
 import { fromIo, type Io } from '../io/module.f.ts'
 import { compile } from '../fs/djs/module.f.ts'
 import { main as testMain } from '../dev/tf/module.f.ts'
-import { main as casMain } from '../cas/module.f.ts'
+import { main as casMain } from '../fs/cas/module.f.ts'
 import type { NodeProgram } from '../fs/types/effects/node/module.f.ts'
 
 export const main = async(io: Io): Promise<number> => {
