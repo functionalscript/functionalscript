c4aa6fd9360c9d2c0770440f61eae015d1794c93
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2026-05-13 17:06:19 +0000

    0.14.1. New the new `ci` function.

--- fs/ci/module.f.ts
+++ fs/ci/module.f.ts
@@ -14,11 +14,11 @@ import { playwrightJob } from './playwright/module.f.ts'
 import { bunSteps } from './bun/module.f.ts'
 import { denoSteps } from './deno/module.f.ts'
 
-const job = (o: Os) => (a: Architecture): readonly [string, Job] => {
+const job = (rust: boolean) => (o: Os) => (a: Architecture): readonly [string, Job] => {
     const id = `${o}-${a}`
     const image = images[o][a]
     const result = [
-        ...rustSteps(o, a),
+        ...(rust ? rustSteps(o, a) : []),
         ...nodeMainSteps(o),
         ...denoSteps,
         ...bunSteps(o, a),
@@ -26,20 +26,22 @@ const job = (o: Os) => (a: Architecture): readonly [string, Job] => {
     return [id, { 'runs-on': image, steps: toSteps(result) }]
 }
 
-const jobs: Jobs = {
-    ...Object.fromEntries(os.flatMap(o => architecture.map(job(o)))),
-    ...nodeVersions,
-    playwright: playwrightJob,
+export const ci = (rust: boolean): Effect<NodeOp, number> => {
+    const jobs: Jobs = {
+        ...Object.fromEntries(os.flatMap(o => architecture.map(job(rust)(o)))),
+        ...nodeVersions,
+        playwright: playwrightJob,
+    }
+    const gha: GitHubAction = {
+        name: 'CI',
+        on: { pull_request: {} },
+        jobs,
+    }
+    return begin
+        .step(() => writeFile('.github/workflows/ci.yml', utf8(JSON.stringify(gha, null, '  '))))
+        .step(() => pure(0))
 }
 
-const gha: GitHubAction = {
-    name: 'CI',
-    on: { pull_request: {} },
-    jobs,
-}
+const defaultEffect: Effect<NodeOp, number> = ci(true)
 
-export const effect: Effect<NodeOp, number> = begin
-    .step(() => writeFile('.github/workflows/ci.yml', utf8(JSON.stringify(gha, null, '  '))))
-    .step(() => pure(0))
-
-export default () => effect
+export default () => defaultEffect
