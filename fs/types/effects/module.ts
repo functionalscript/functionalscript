d30dcb7eecbdf8de7d8dbcb2b2a97d5d3e585eb3
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2026-05-26 11:48:28 +0000

    c

--- fs/types/effects/module.ts
+++ fs/types/effects/module.ts
@@ -1,7 +1,5 @@
 import type { Effect, Operation, ToAsyncOperationMap } from "./module.f.ts"
 
-let i = 0
-
 export const asyncRun =
     <O extends Operation>(map: ToAsyncOperationMap<O>) =>
     async<T, E extends Effect<O, T>>(effect: Effect<O, T>): Promise<T> =>
@@ -13,10 +11,7 @@ export const asyncRun =
         }
         const [command, payload, continuation] = value
         const operation = map[command]
-        const x = ++i
-        console.log('Running operation #', x, ':', command, payload)
         const result = await operation(...payload)
-        console.log('Result of operation #', x, ':', result)
         effect = continuation(result)
     }
 }
