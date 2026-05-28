e09e66ff6cc862052d3940931f010dc1fd247cbc
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2026-05-28 19:24:51 +0000

    Ok

--- fs/dev/tf/module.ts
+++ fs/dev/tf/module.ts
@@ -2,7 +2,6 @@ import { io } from '../../io/module.ts'
 import { loadModuleMap } from '../module.f.ts'
 import { type RegisterTest, registerModuleMap } from './module.f.ts'
 import * as nodeTest from 'node:test'
-import { asyncImport } from '../../io/module.ts'
 import { fromIo } from '../../io/module.f.ts'
 import { type ToAsyncOperationMap } from '../../types/effects/module.f.ts'
 import { asyncRun } from '../../types/effects/module.ts'
@@ -10,29 +9,26 @@ import { type All } from '../../types/effects/node/module.f.ts'
 
 //
 
-const isPlaywright = typeof process !== 'undefined' && process?.env?.PLAYWRIGHT_TEST !== undefined
+type TestContext = {
+    test: TestFn
+}
 
-//
+type TestFn = (
+    name: string,
+    options: { readonly expectFailure: boolean },
+    fn: (t: TestContext) => void | Promise<void>
+) => void
 
-type TestFn = (name: string, options: { readonly expectFailure: boolean }, fn: () => void | Promise<void>) => void
-
-const makeMap = (testFn: TestFn): ToAsyncOperationMap<RegisterTest | All> => {
+const makeMap = (testFn: TestContext): ToAsyncOperationMap<RegisterTest | All> => {
     const m: ToAsyncOperationMap<RegisterTest | All> = {
         all: async (...effects) => Promise.all(effects.map(asyncRun(m))),
         registerTest: async (name, expectFailure, test) =>
-            testFn(name, { expectFailure }, async () => asyncRun(m)(test())),
+            testFn.test(name, { expectFailure }, async t => asyncRun(makeMap(t))(test())),
     }
     return m
 }
 
-const createPlaywrightMap = async (): Promise<ToAsyncOperationMap<RegisterTest | All>> => {
-    const pwTest = (await asyncImport('@playwright/test') as any).test
-    return makeMap((name, _opts, fn) => pwTest(name, fn))
-}
-
-const map: ToAsyncOperationMap<RegisterTest | All> = isPlaywright
-    ? await createPlaywrightMap()
-    : makeMap((name, opts, fn) => nodeTest.test(name, opts, fn))
+const map: ToAsyncOperationMap<RegisterTest | All> = makeMap(nodeTest)
 
 export const run = async (): Promise<void> => {
     const moduleMap = await fromIo(io)(loadModuleMap(io.process.env))
