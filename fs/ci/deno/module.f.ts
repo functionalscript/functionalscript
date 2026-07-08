9ae2a47b5bb0d92f7f79f11d8d4cb38a4b8fe5e0
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2026-07-08 08:42:17 -0700

    remove Deno restrictions

--- fs/ci/deno/module.f.ts
+++ fs/ci/deno/module.f.ts
@@ -11,7 +11,10 @@ const denoTest = 'deno test --allow-read --allow-env --allow-sys' as const
 
 export const denoSteps = (version: string): readonly MetaStep[] => clean([
     install(uses('denoland/setup-deno', { 'deno-version': deno })),
-    install({ run: `deno install -g -A npm:functionalscript@${version}` }),
+    // we need --minimum-dependency-age=0 for functionalscript because we would like to use
+    // the latest version of the package even if it is not yet 24 hours old,
+    // which is the default minimum dependency age for Deno installs
+    install({ run: `deno install -g -A npm:functionalscript@${version} --minimum-dependency-age=0` }),
     test({ run: `deno run -A npm:functionalscript@${version} t` }),
     test({ run: 'deno install --frozen' }),
     test({ run: `${denoTest} --coverage && deno coverage --include='.*module\\.f\\.ts'` }),
