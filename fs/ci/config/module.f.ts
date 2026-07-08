cd9d3a38a0b9c0f449651486909f1ce8a87887a0
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2026-07-08 10:33:53 -0700

    Upgrade to TypeScript 7 (#1242)
    * TypeScript 7
    
    * forgot about ci.yml

--- fs/ci/config/module.f.ts
+++ fs/ci/config/module.f.ts
@@ -49,9 +49,6 @@ export const wasmtime = '46.0.1'
 // https://github.com/wasmerio/wasmer/releases
 export const wasmer = '7.2.0'
 
-// https://www.npmjs.com/package/@typescript/native-preview?activeTab=versions
-export const tsgo = '7.0.0-dev.20260707.2'
-
 // GitHub Action versions used by CI step builders. The key is the action
 // `owner/name`; call sites compose the full ref as
 // `` `${name}@${actions[name]}` ``.
