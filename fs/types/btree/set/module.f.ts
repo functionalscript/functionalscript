309e89f4d9dde0f10a19b11e43b55d9cf19e0653
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2026-07-12 09:17:40 -0700

    more

--- fs/types/btree/set/module.f.ts
+++ fs/types/btree/set/module.f.ts
@@ -89,10 +89,10 @@ const nodeSet
             case 3: {
                 // replace
                 // TODO: remove after TSGO fix the regression.
-                const _xl: 2|5 = x.length
+                // const _xl: 2|5 = x.length
                 switch (x.length) {
                     case 2: { return [[x[0], g(x[1])]] }
-                    case 5: { return [[x[0], x[1], x[2], g(x[3]), x[4]]]}
+                    case 5: { return [[x[0], x[1], x[2], g(x[3]), x[4]]] }
                 }
                 // TODO: remove after TSGO fix the regression.
                 throw 'unreachable'
@@ -110,3 +110,71 @@ const nodeSet
 export const set
     : <T>(c: Compare<T>) => (f: (value: T|null) => T) => (tree: Tree<T>) => TNode<T>
     = c => f => tree => tree === null ? [f(null)] : nodeSet(c)(f)(tree)
+
+const nodeSet2
+    = <T>(c: Compare<T>) => (g: (value: T | null) => T) => (node: TNode<T>): undefined => {
+    // export type Result<T> = {
+    //    readonly first: First<T>,
+    //    readonly tail: Path<T>
+    //}
+    const { first, tail }: Result<T> = find(c)(node)
+    // export type Index2 = 0 | 1
+    // export type Index3 = Index2 | 2 // 0 | 1 | 2
+    // export type Index4 = Index3 | 3 // 0 | 1 | 2 | 3
+    // export type Index5 = Index4 | 4 // 0 | 1 | 2 | 3 | 4
+    // type FirstLeaf1<T> = readonly[Index3, Leaf1<T>]
+    // type FirstBranch3<T> = readonly[1, Branch3<T>]
+    // type FirstLeaf2<T> = readonly[Index5, Leaf2<T>]
+    // type FirstBranch5<T> = readonly[1|3, Branch5<T>]
+    // export type First<T> = FirstLeaf1<T> | FirstBranch3<T> | FirstLeaf2<T> | FirstBranch5<T>
+    const [i, x]: First<T> = first;
+    const f = (): undefined => {
+        switch (i) {
+            case 0: {
+                // insert
+                const value = g(null)
+                switch (x.length) {
+                    case 1:
+                    case 2: { return undefined }
+                }
+            }
+            case 1: {
+                // replace
+                switch (x.length) {
+                    case 1:
+                    case 2:
+                    case 3:
+                    case 5: { return undefined }
+                }
+            }
+            case 2: {
+                // insert
+                const value = g(null)
+                // TODO: remove after TSGO fix the regression.
+                // const _xl: 1|2 = x.length
+                switch (x.length) {
+                    case 1:
+                    case 2: { return undefined }
+                }
+                // TODO: remove after TSGO fix the regression.
+                throw 'unreachable'
+            }
+            case 3: {
+                // replace
+                // TODO: remove after TSGO fix the regression.
+                //const _xl: 2|5 = x.length
+                switch (x.length) {
+                    case 2:
+                    case 5: { return undefined}
+                }
+                // TODO: remove after TSGO fix the regression.
+                // throw 'unreachable'
+            }
+            case 4: {
+                // insert
+                return undefined
+            }
+        }
+    }
+    return f()
+}
