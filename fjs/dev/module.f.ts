4e697d7bc4e5c533941b71b83b255d23c7581bb8
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2026-07-24 00:23:18 -0700

    no eff

--- fjs/dev/module.f.ts
+++ fjs/dev/module.f.ts
@@ -16,8 +16,7 @@ import {
 import { cmp as strCmp } from '../types/string/module.f.ts'
 import type { StringMap } from '../types/object/module.f.ts'
 import { unwrap } from '../types/result/module.f.ts'
-import { pure, type Effect } from '../effects/module.f.ts'
-import { eff } from '../effects/eff/module.f.ts'
+import { pure, step, type Effect } from '../effects/module.f.ts'
 import { join, relativize, toPosix } from '../path/module.f.ts'
 import { assert } from '../asserts/module.f.ts'
 
@@ -51,32 +50,37 @@ const allFiles = (
     s: string,
     predicate: (path: string) => boolean,
 ): Effect<Readdir | All, readonly string[]> => {
-    const load = (p: string): Effect<Readdir | All, readonly string[]> =>
-        eff(readdir(p, {}))
-        .step(d => {
-            let result: readonly Effect<Readdir | All, readonly string[]>[] = []
-            for (const i of unwrap(d)) {
-                const { name } = i
-                if (name.startsWith('.')) { continue }
-                const file = join(p, name)
-                if (!i.isFile) {
-                    if (name === 'node_modules') { continue }
-                    result = [...result, load(file)]
-                    continue
+    const load = (p: string): Effect<Readdir | All, readonly string[]> => {
+        const x0 = step(
+            readdir(p, {}),
+            d => {
+                let result: readonly Effect<Readdir | All, readonly string[]>[] = []
+                for (const i of unwrap(d)) {
+                    const { name } = i
+                    if (name.startsWith('.')) { continue }
+                    const file = join(p, name)
+                    if (!i.isFile) {
+                        if (name === 'node_modules') { continue }
+                        result = [...result, load(file)]
+                        continue
+                    }
+                    if (predicate(file)) {
+                        result = [...result, pure([file])]
+                    }
                 }
-                if (predicate(file)) {
-                    result = [...result, pure([file])]
-                }
-            }
-            return all(...result)
-        })
-        .step(v => pure(v.flat()))
-        .value
+                return all(...result)
+            })
+        return step(
+            x0,
+            v => pure(v.flat()))
+    }
     return load(s)
 }
 
 const loadFile = (f: string): Effect<Access | Import, readonly (readonly[string, Module])[]> =>
-    eff(import_(f)).step(r => pure([[f, unwrap(r)] as const])).value
+    step(
+        import_(f),
+        r => pure([[f, unwrap(r)] as const]))
 
 /** The effect operations required to discover and load a module map. */
 export type LoadModuleOperations = Access | Import | All | Readdir
@@ -105,15 +109,17 @@ export const loadModuleMap = (env: Env): Effect<LoadModuleOperations, ModuleMap>
     //       we should consider optimize them by ALIQ technique or something similar.
     //       For example, we should be able to write it like `allFiles(s).flatMap(loadFile)`,
     //       then an effect runner can batch all file loading operations together.
-    return eff(allFiles(s, shouldLoad))
-        .step(files => all(...files.map(loadFile)))
-        .step(entries => pure(fromEntries(
+    const x0 = step(
+        allFiles(s, shouldLoad),
+        files => all(...files.map(loadFile)))
+    return step(
+        x0,
+        entries => pure(fromEntries(
             entries
                 .flat()
                 .map(([k, v]) => [relativize(prefix, k), v] as const)
                 .toSorted(([a], [b]) => strCmp(a)(b))
         )))
-        .value
 }
 
 export const proof = {
