82facb247b8e87a96d1f958b60027bc18b7cd236
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2026-07-23 22:53:57 -0700

    remove eff

--- fjs/effects/node/module.f.ts
+++ fjs/effects/node/module.f.ts
@@ -18,7 +18,6 @@ import type { Nominal } from '../../types/nominal/module.f.ts'
 import { ok, error as resultError, mapOk, type Result } from '../../types/result/module.f.ts'
 import type { StringMap } from '../../types/object/module.f.ts'
 import { type Effect, type Func, type Operation, type ToAsyncOperationMap, do_, okStep, pure, step } from '../module.f.ts'
-// import { eff } from '../eff/module.f.ts'
 import type { List } from '../list/module.f.ts'
 
 export type IoResult<T> = Result<T, unknown>
