148a0e990f8f29faefc3172133c607755823800f
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2026-08-18 08:15:00 -0700

    strange 1

---
+++ fjs/effects2/types.ts
@@ -0,0 +1,63 @@
+import type { Assert } from "../asserts/types.ts"
+import type { Result } from "../types/result/types.ts"
+import type { Equal } from "../types/ts/types.ts"
+
+export type Operation = {
+    readonly name: string,
+    readonly func: (...args: readonly never[]) => {
+        readonly ok: unknown
+        readonly error: unknown
+    }
+}
+
+export type Effect<O extends Operation, T, E = unknown> = Pure<T, E> | Do<O, T, E>
+
+export type Pure<T, E> =
+    () => Result<T, E>
+
+export type Do<out O extends Operation, T, E> = {
+    readonly name: O['name']
+    readonly payload: Info<O, O['name']>['args']
+    readonly continuation: Cont<O, T, E>
+}
+
+export type Info<O extends Operation, K extends O['name']> =
+    O extends {
+        readonly name: K,
+        readonly func: (...args: infer P) => infer R
+    }
+    ? {
+        readonly args: P
+        readonly result: R
+    }
+    : never
+
+export type Cont<out O extends Operation, T, E> =
+    (_: Info<O, O['name']>['result']) => Effect<O, T, E>
+
+export type CommandSet<O extends Operation> =
+    Readonly<Record<O['name'], null>>
+
+export type Commands<O extends Operation> =
+    readonly O['name'][]
+
+export type SelfInfo<O extends Operation> = Info<O, O['name']>
+
+export type Func<O extends Operation> =
+    (..._: Args<O>) => Effect<O, Return<O>['ok'], Return<O>['error']>
+
+export type Args<O extends Operation> = SelfInfo<O>['args']
+
+export type Return<O extends Operation> = SelfInfo<O>['result']
+
+type TestOp = {
+    readonly name: 'hello',
+    readonly func: () => {
+        readonly ok: number
+        readonly error: string
+    }
+}
+
+type _X = Return<TestOp>['ok']
+
+type _0 = Assert<Equal<Return<TestOp>['ok'], number>>
