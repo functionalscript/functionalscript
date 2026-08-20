dc42c63d3b28a3b8bb82d46cc4a2ed641e92093c
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2026-08-20 09:33:12 -0700

    optional-array-check

--- fjs/types/rtti/parse/proof.f.mjs
+++ fjs/types/rtti/parse/proof.f.mjs
@@ -359,4 +359,12 @@ export const proof = {
             assertError(v({ a: 42 }))
         },
     },
+    arrayOptional: () => {
+        const a = /** @type {const} */([number, option(string)])
+        const v = parse(a)
+        assertOk(v([5]))
+        assertError(v(["n"]))
+        assertOk(v([6, "3"]))
+        assertError(v([6, 9]))
+    }
 }
