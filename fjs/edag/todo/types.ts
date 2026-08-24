f03f997f60ea0b9beb9e5a61681a902893daa507
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2026-08-23 22:23:03 -0700

    ok

--- fjs/edag/todo/types.ts
+++ fjs/edag/todo/types.ts
@@ -2,16 +2,18 @@ import type { Tuple } from '../../types/array/types.ts'
 import type { Exp } from '../types.ts'
 
 type ExpOp = Extract<Exp, readonly unknown[]>
-
-type StaticKey = Exclude<ExpOp[0], 'frame' | 'args'>
+type Context = {
+    readonly frame: unknown,
+    readonly args: readonly unknown[],
+}
 
 type Next<T extends ExpOp, K> =
-    T extends readonly [infer Op, ...infer R]
+    T extends readonly [infer Op, ...readonly unknown[]]
         ? K extends Op
-            ? (...r: Tuple<R['length'], any>) => unknown
+            ? (c: Context, r: T) => unknown
             : never
         : never
 
 export type StaticMap = {
-    readonly[K in StaticKey]: Next<ExpOp, K>
+    readonly[K in ExpOp[0]]: Next<ExpOp, K>
 }
