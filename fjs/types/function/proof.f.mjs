653bf77d9d75102a4dd74420fdd580879f37ef23
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2026-08-26 10:16:56 -0700

    types/function: prove iterate semantics

--- fjs/types/function/proof.f.mjs
+++ fjs/types/function/proof.f.mjs
@@ -1,4 +1,4 @@
-import { fn } from './module.f.mjs'
+import { fn, iterate } from './module.f.mjs'
 import { assertEq } from '../../asserts/module.f.mjs'
 
 export const proof = () => {
@@ -13,4 +13,10 @@ export const proof = () => {
 
     const result = r('hello')
     assertEq(result, 5, r)
+
+    /** @type {(x: number) => number} */
+    const increment = x => x + 1
+    assertEq(iterate(0n)(increment)(5), 5, iterate)
+    assertEq(iterate(3n)(increment)(5), 8, iterate)
+    assertEq(iterate(-1n)(increment)(5), 5, iterate)
 }
