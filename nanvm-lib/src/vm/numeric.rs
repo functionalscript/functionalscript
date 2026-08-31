2a6b8bff7c6d418ea685b60f82be63678f388be5
sasha-gil <sasha-gil@users.noreply.github.com>
2026-08-30 20:37:54 -0700

    nanvm: implement subtraction

--- nanvm-lib/src/vm/numeric.rs
+++ nanvm-lib/src/vm/numeric.rs
@@ -1,6 +1,6 @@
-use std::ops::Mul;
+use std::ops::{Mul, Neg, Sub};
 
-use crate::vm::{Any, BigInt, IVm, ToAny};
+use crate::vm::{Any, BigInt, IVm, Unpacked};
 
 /// <https://tc39.es/ecma262/#sec-tonumeric>
 /// Represents ECMAScript numeric types, i.e. `Number` or `BigInt`, as defined by ToNumeric.
@@ -11,14 +11,46 @@ pub enum Numeric<A: IVm> {
     BigInt(BigInt<A>),
 }
 
+impl<A: IVm> From<Numeric<A>> for Unpacked<A> {
+    fn from(value: Numeric<A>) -> Self {
+        match value {
+            Numeric::Number(value) => Unpacked::Number(value),
+            Numeric::BigInt(value) => Unpacked::BigInt(value),
+        }
+    }
+}
+
+impl<A: IVm> Neg for Numeric<A> {
+    type Output = Self;
+
+    fn neg(self) -> Self::Output {
+        match self {
+            Numeric::Number(value) => Numeric::Number(-value),
+            Numeric::BigInt(value) => Numeric::BigInt(-value),
+        }
+    }
+}
+
 impl<A: IVm> Mul for Numeric<A> {
-    type Output = Result<Any<A>, Any<A>>;
+    type Output = Result<Self, Any<A>>;
 
     fn mul(self, rhs: Self) -> Self::Output {
         Ok(match (self, rhs) {
-            (Numeric::Number(a), Numeric::Number(b)) => (a * b).to_any(),
-            (Numeric::BigInt(a), Numeric::BigInt(b)) => (a * b).to_any(),
+            (Numeric::Number(a), Numeric::Number(b)) => Numeric::Number(a * b),
+            (Numeric::BigInt(a), Numeric::BigInt(b)) => Numeric::BigInt(a * b),
             _ => return Err("TODO: Cannot multiply Number and BigInt".into()),
+        })
+    }
+}
+
+impl<A: IVm> Sub for Numeric<A> {
+    type Output = Result<Self, Any<A>>;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Ok(match (self, rhs) {
+            (Numeric::Number(a), Numeric::Number(b)) => Numeric::Number(a - b),
+            (Numeric::BigInt(a), Numeric::BigInt(b)) => Numeric::BigInt(a - b),
+            _ => return Err("TODO: Cannot subtract Number and BigInt".into()),
         })
     }
 }
