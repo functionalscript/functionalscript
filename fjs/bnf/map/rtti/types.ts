aa4d3966b8061c4e46101f7d3ca4ca1ceeffcda1
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2026-08-31 22:04:33 -0700

    ok

--- fjs/bnf/map/rtti/types.ts
+++ fjs/bnf/map/rtti/types.ts
@@ -1,14 +1,21 @@
-import type { Ts } from "../../../rtti/ts/types.ts"
+import type { Ts, TupleTs } from "../../../rtti/ts/types.ts"
 import type { Type } from "../../../rtti/types.ts"
-import type { SequenceMap, TerminalMap } from "../types.ts"
+import type { RequiredMap } from "../../../types/object/types.ts"
+import type { SequenceMap, TerminalMap, VariantMap } from "../types.ts"
 
-export type TerminalMapInfo<MI, MO, RO extends Type> = {
+export type TerminalInfo<MI, MO, RO extends Type> = {
     readonly ro: RO
     readonly map: TerminalMap<MI, MO, Ts<RO>>
 }
 
-export type SequenceMapInfo<MI, RI extends readonly Type[], MO, RO extends Type> = {
+export type SequenceInfo<MI, RI extends readonly Type[], MO, RO extends Type> = {
     readonly ri: RI
     readonly ro: RO
-    readonly map: SequenceMap<MI, Ts<RI>, MO, Ts<RO>>
+    readonly map: SequenceMap<MI, TupleTs<RI>, MO, Ts<RO>>
+}
+
+export type VariantInfo<MI, RI extends RequiredMap<string, Type>, MO, RO extends Type> = {
+    readonly ri: RI
+    readonly ro: RO
+    readonly map: VariantMap<MI, Ts<RI>, MO, Ts<RO>>
 }
