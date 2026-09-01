b14fcbe4146f449e4cbe1d33ab7dfb8f92397427
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2026-08-31 21:49:59 -0700

    broken

--- fjs/bnf/map/rtti/types.ts
+++ fjs/bnf/map/rtti/types.ts
@@ -1,3 +1,14 @@
-export type RttiWrap<MI, RI, MO, RO> = {
+import type { Ts } from "../../../rtti/ts/types.ts"
+import type { Type } from "../../../rtti/types.ts"
+import type { SequenceMap, TerminalMap } from "../types.ts"
+
+export type TerminalMapInfo<MI, MO, RO extends Type> = {
+    readonly ro: RO
+    readonly map: TerminalMap<MI, MO, Ts<RO>>
+}
+
+export type SequenceMapInfo<MI, RI extends readonly Type[], MO, RO extends Type> = {
     readonly ri: RI
+    readonly ro: RO
+    readonly map: SequenceMap<MI, Ts<RI>, MO, Ts<RO>>
 }
