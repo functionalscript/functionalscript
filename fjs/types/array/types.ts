a0df8272e9a8b01c88f8bef4a286f1da9730c9aa
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2026-09-02 22:23:13 -0700

    ok

--- fjs/types/array/types.ts
+++ fjs/types/array/types.ts
@@ -31,7 +31,40 @@ type _X3 = Assert<Equal<Tuple<number, true>, readonly true[]>>
 type _X4 = Assert<Equal<Tuple<2, true>, readonly [true, true]>>
 // type _X5 = Assert<Equal<Tuple<-1, true>, readonly [true, true]>>
 
-export type Array1_5<T> =
-    Tuple<1, T> | Tuple<2, T> | Tuple<3, T> | Tuple<4, T> | Tuple<5, T>
+type _Option<X extends readonly unknown[]> = { readonly [K in keyof X]?: X[K] }
+
+export type OptionTuple<N extends number, T> = _Option<Tuple<N, T>>
+
+type _Tail<
+    Max extends number,
+    T,
+    R extends readonly unknown[],
+    O extends readonly T[] = readonly [],
+> =
+    number extends Max ? readonly T[] :
+    R['length'] extends Max
+        ? _Option<O>
+        : _Tail<Max, T, readonly [...R, unknown], readonly [...O, T]>
+
+export type Array<
+    Min extends number,
+    Max extends number,
+    T,
+> =
+    Tuple<Min, T> extends infer R extends readonly T[]
+        ? readonly [...R, ..._Tail<Max, T, R>]
+        : never
+
+type _X00 = Assert<Equal< Array<0, 0, true>, readonly []>>
+type _X01 = Assert<Equal<Array<0, 1, true>, readonly [true?]>>
+type _X02 = Assert<Equal<Array<0, 2, true>, readonly [true?, true?]>>
+type _X11 = Assert<Equal<Array<1, 1, true>, readonly [true]>>
+type _X12 = Assert<Equal<Array<1, 2, true>, readonly [true, true?]>>
+type _X13 = Assert<Equal<Array<1, 3, true>, readonly [true, true?, true?]>>
+type _X22 = Assert<Equal<Array<2, 2, true>, readonly [true, true]>>
+type _X2_ = Assert<Equal<
+    Array<2, number, true>,
+    readonly [true, true, ...readonly true[]]>
+>
 
 export type Includes<I, T extends readonly I[]> = (v: I) => v is T[number]
