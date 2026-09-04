964670f7e015d401a74ae31c13837b74752f2a29
Sergey Shandar <sergey-shandar@users.noreply.github.com>
2026-09-03 17:04:13 -0700

    ok

--- fjs/ebnf/module.f.mjs
+++ fjs/ebnf/module.f.mjs
@@ -1,4 +1,5 @@
 /**
+ * @import { RangeSet } from '../types/range_set/types.ts'
  * @import { SetInfo, Rule, Infinity, RepeatInfo } from './types.ts'
  */
 
@@ -6,6 +7,7 @@ import { assert } from "../asserts/module.f.mjs"
 import { stringToCodePointList } from "../text/utf16/module.f.mjs"
 import { isFixedArray } from "../types/array/module.f.mjs"
 import { toArray } from "../types/list/module.f.mjs"
+import { complement, intersection } from "../types/range_set/module.f.mjs"
 
 const isFixedArray2 =
     isFixedArray(2)
@@ -25,13 +27,27 @@ export const range = ab => {
 }
 
 /**
- * @type {<const A extends number, const B extends number>(a: A, b: B) =>
+ * @type {(a: number, b: number) =>
  *  SetInfo<readonly[number, number]>}
  */
 export const rangeEncode = (a, b) => (
     assert(a <= b),
     () => ['set', a, b + 1]
 )
+
+/** @type {(a: SetInfo<readonly number[]>) => RangeSet} */
+const getSet = a => {
+    const [, ...r] = a()
+    return r
+}
+
+/**
+ * @type {(a: SetInfo<readonly[number, number]>) =>
+ *  (b: SetInfo<readonly[number, number]>) =>
+ *  SetInfo<readonly number[]>}
+ */
+export const remove =
+    a => b => () => ['set', ...intersection(getSet(a))(complement(getSet(b)))]
 
 /**
  * @type {<const A extends number, const B extends number>(a: A, b: B) =>
